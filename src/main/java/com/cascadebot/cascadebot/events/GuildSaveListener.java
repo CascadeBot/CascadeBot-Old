cb95fecde05e1b3270b49cc658f3cc0c042ec0f5
William Oldham <wegg7250@gmail.com>
2018-09-21 20:15:27 +0100

    Add guild saving

---
+++ src/main/java/com/cascadebot/cascadebot/events/GuildSaveListener.java
@@ -0,0 +1,17 @@
+package com.cascadebot.cascadebot.events;
+
+import com.cascadebot.cascadebot.objects.GuildData;
+import com.github.benmanes.caffeine.cache.RemovalCause;
+import com.github.benmanes.caffeine.cache.RemovalListener;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+public class GuildSaveListener implements RemovalListener<Long, GuildData> {
+
+    @Override
+    public void onRemoval(@Nullable Long aLong, @Nullable GuildData data, @Nonnull RemovalCause removalCause) {
+
+    }
+
+}
