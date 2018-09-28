5af2e1e4c98418874658de7662a9c2bf6f7f60ce
Unknown <theweeryan17@gmail.com>
2018-09-27 21:01:13 -0500

    Started work on eval command

--- src/main/java/com/cascadebot/cascadebot/commands/CommandContext.java
+++ src/main/java/com/cascadebot/cascadebot/commands/CommandContext.java
@@ -3,14 +3,21 @@ package com.cascadebot.cascadebot.commands;
 import com.cascadebot.cascadebot.objects.GuildData;
 import net.dv8tion.jda.core.entities.TextChannel;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CommandContext {
 
     private final TextChannel channel;
     private final  GuildData data;
+    private String[] args;
 
     public CommandContext(TextChannel channel, GuildData data, String[] args) {
         this.channel = channel;
         this.data = data;
+        this.args = args;
     }
 
     public TextChannel getChannel() {
@@ -19,5 +26,18 @@ public class CommandContext {
 
     public GuildData getData() {
         return data;
+    }
+
+    public String[] getArgs() {
+        return args;
+    }
+
+    public String getMessageFromArgs(int start) {
+        return getMessageFromArgs(start, args.length - 1);
+    }
+
+    public String getMessageFromArgs(int start, int end) {
+        List<String> messageParts = Arrays.asList(Arrays.copyOfRange(args, start, end));
+        return messageParts.stream().collect(Collectors.joining(" "));
     }
 }
