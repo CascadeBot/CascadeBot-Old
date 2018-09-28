85b4871ef7506718cfefbafbb33817e126dc409d
William Oldham <wegg7250@gmail.com>
2018-09-28 21:19:20 +0100

    Refine command context

--- src/main/java/com/cascadebot/cascadebot/commands/CommandContext.java
+++ src/main/java/com/cascadebot/cascadebot/commands/CommandContext.java
@@ -1,6 +1,7 @@
 package com.cascadebot.cascadebot.commands;
 
 import com.cascadebot.cascadebot.objects.GuildData;
+import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.TextChannel;
 
 import java.util.Arrays;
@@ -10,14 +11,12 @@ import java.util.stream.Collectors;
 
 public class CommandContext {
 
-    private final TextChannel channel;
-    private final  GuildData data;
+    private TextChannel channel;
+    private GuildData data;
     private String[] args;
+    private Member member;
 
-    public CommandContext(TextChannel channel, GuildData data, String[] args) {
-        this.channel = channel;
-        this.data = data;
-        this.args = args;
+    public CommandContext() {
     }
 
     public TextChannel getChannel() {
@@ -40,4 +39,25 @@ public class CommandContext {
         List<String> messageParts = Arrays.asList(Arrays.copyOfRange(args, start, end));
         return messageParts.stream().collect(Collectors.joining(" "));
     }
+
+    public void setChannel(TextChannel channel) {
+        this.channel = channel;
+    }
+
+    public void setGuildData(GuildData data) {
+        this.data = data;
+    }
+
+    public void setArgs(String[] args) {
+        this.args = args;
+    }
+
+    public void setMember(Member member) {
+        this.member = member;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
 }
