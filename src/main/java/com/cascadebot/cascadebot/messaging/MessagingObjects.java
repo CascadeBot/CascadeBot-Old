e631fce1b5e7264dbfdf7a119e72d15122a5030a
William Oldham <wegg7250@gmail.com>
2018-10-06 19:11:10 +0100

    Message objects for stuff :eyes:

---
+++ src/main/java/com/cascadebot/cascadebot/messaging/MessagingObjects.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2018 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.messaging;
+
+import com.cascadebot.cascadebot.Constants;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.MessageBuilder;
+
+public class MessagingObjects {
+
+    private static ThreadLocal<MessageBuilder> threadLocalMessageBuilder = ThreadLocal.withInitial(MessageBuilder::new);
+    private static ThreadLocal<EmbedBuilder> threadLocalEmbedBuilder = ThreadLocal.withInitial(EmbedBuilder::new);
+
+    public static MessageBuilder getClearThreadLocalMessageBuilder() {
+        return threadLocalMessageBuilder.get().clear();
+    }
+
+    public static EmbedBuilder getClearThreadLocalEmbedBuilder() {
+        return threadLocalEmbedBuilder.get()
+                .clearFields()
+                .setTitle(null)
+                .setDescription(null)
+                .setTimestamp(null)
+                .setColor(null)
+                .setThumbnail(null)
+                .setAuthor(null, null, null)
+                .setFooter(null, null)
+                .setImage(null);
+    }
+
+    public static EmbedBuilder getColoredEmbedBuilder() {
+        return getClearThreadLocalEmbedBuilder()
+                .setColor(Constants.CASCADE_COLOR);
+    }
+
+
+}
