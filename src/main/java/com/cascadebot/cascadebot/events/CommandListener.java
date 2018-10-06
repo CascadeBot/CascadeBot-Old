e15597d3e4a326f955f3c3981669e33d8bbc44e1
William Oldham <wegg7250@gmail.com>
2018-10-06 12:15:56 +0100

    Refactor and add logging for command execution

--- src/main/java/com/cascadebot/cascadebot/events/CommandListener.java
+++ src/main/java/com/cascadebot/cascadebot/events/CommandListener.java
@@ -2,16 +2,17 @@ package com.cascadebot.cascadebot.events;
 
 import com.cascadebot.cascadebot.CascadeBot;
 import com.cascadebot.cascadebot.Config;
-import com.cascadebot.cascadebot.commands.Command;
+import com.cascadebot.cascadebot.Constants;
+import com.cascadebot.cascadebot.commands.ICommand;
 import com.cascadebot.cascadebot.commands.CommandContext;
 import com.cascadebot.cascadebot.objects.GuildData;
 import net.dv8tion.jda.core.events.message.guild.GuildMessageReceivedEvent;
 import net.dv8tion.jda.core.hooks.ListenerAdapter;
 import org.apache.commons.lang3.ArrayUtils;
 
+import java.util.Arrays;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.regex.Pattern;
 
 public class CommandListener extends ListenerAdapter {
 
@@ -19,12 +20,11 @@ public class CommandListener extends ListenerAdapter {
     private static final ExecutorService COMMAND_POOL = Executors.newFixedThreadPool(5, r ->
             new Thread(COMMAND_THREADS, r, "Command Pool-" + COMMAND_THREADS.activeCount()));
 
-    private final Pattern multiSpace = Pattern.compile(" {2,}");
 
     @Override
     public void onGuildMessageReceived(GuildMessageReceivedEvent event) {
         if (event.getAuthor().isBot()) return;
-        String message = multiSpace.matcher(event.getMessage().getContentRaw()).replaceAll(" ");
+        String message = Constants.MULTISPACE_REGEX.matcher(event.getMessage().getContentRaw()).replaceAll(" ");
         String prefix = Config.VALUES.defaultPrefix; //TODO: Add guild data prefix here
         GuildData guildData = new GuildData(event.getGuild().getIdLong());
         if (message.startsWith(prefix)) {
@@ -38,15 +38,20 @@ public class CommandListener extends ListenerAdapter {
             context.setGuildData(guildData);
             context.setMember(event.getMember());
 
-            Command cmd = CascadeBot.instance().getCommandManager().getCommand(commandString, event.getAuthor(), guildData);
+            ICommand cmd = CascadeBot.instance().getCommandManager().getCommand(commandString, event.getAuthor(), guildData);
             if (cmd != null) {
+                context.setOriginalCommand(commandString);
                 dispatchCommand(cmd, context);
             }
         }
     }
 
-    private void dispatchCommand(final Command command, final CommandContext context) {
+    private void dispatchCommand(final ICommand command, final CommandContext context) {
         COMMAND_POOL.submit(() -> {
+            CascadeBot.LOGGER.info("ICommand {} executed by {} with args: {}",
+                    command.defaultCommand() + (command.defaultCommand().equalsIgnoreCase(context.getOriginalCommand()) ? "" : context.getOriginalCommand()),
+                    context.getUser().getName() + "#" + context.getUser().getDiscriminator(), // TODO: Util this
+                    Arrays.toString(context.getArgs()));
             command.onCommand(context.getMember(), context);
         });
     }
