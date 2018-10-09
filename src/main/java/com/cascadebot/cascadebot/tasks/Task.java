8c7a9fc4b9ddaf5b137348b2cdc63dd3155bf9da
William Oldham <wegg7250@gmail.com>
2018-10-09 08:04:14 +0100

    Tada :)

--- src/main/java/com/cascadebot/cascadebot/tasks/Task.java
+++ src/main/java/com/cascadebot/cascadebot/tasks/Task.java
@@ -1,40 +1,102 @@
 package com.cascadebot.cascadebot.tasks;
 
-import com.cascadebot.cascadebot.CascadeBot;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import java.util.concurrent.ExecutorService;
+import java.util.Collections;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
 
-public interface Task {
 
-    ThreadGroup taskThreadGroup = new ThreadGroup("Task Thread Poll");
-    ScheduledExecutorService delayService = Executors.newScheduledThreadPool(10, r -> new Thread(taskThreadGroup, r,
-            taskThreadGroup.getName() + taskThreadGroup.activeCount()));
+// Slightly modified version of FredBoat's "FredBoatAgent"
+public abstract class Task implements Runnable {
 
-    /**
-     * This is the thing you run to start the task.
-     * DO NOT OVERRIDE THIS IN YOUR TASK!
-     */
-    default void run() {
-        if(repeat() < 0) {
-            delayService.schedule(this::execute, delay(), TimeUnit.MILLISECONDS);
-        } else {
-            delayService.scheduleAtFixedRate(this::execute, delay(), repeat(), TimeUnit.MILLISECONDS);
+    private static final String IDLE_NAME = "idle agent worker thread";
+    private static final String RUNNING_NAME = "%s agent worker thread";
+
+    private static final Map<Class<? extends Task>, Long> LAST_RUN_TIME = new ConcurrentHashMap<>();
+
+    private static final ThreadGroup TASK_THREADS = new ThreadGroup("Task Thread Poll");
+    private static final ScheduledExecutorService AGENTS = Executors.newScheduledThreadPool(2, runnable -> {
+        Thread thread = new Thread(TASK_THREADS, runnable, IDLE_NAME);
+        thread.setPriority(4);
+        return thread;
+    });
+    public static ScheduledExecutorService getScheduler() {
+        return AGENTS;
+    }
+
+
+    //only one of each agent, non-static is fine
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
+    private final String name;
+    private final long timeToSleepInMillis;
+    private final long delayInMillis;
+
+    public Task(String name, long timeToSleepInMillis) {
+        this.name = name;
+        this.timeToSleepInMillis = timeToSleepInMillis;
+        this.delayInMillis = 0L;
+    }
+
+    public Task(String name, long timeToSleep, TimeUnit unit) {
+        this.name = name;
+        this.timeToSleepInMillis = unit.toMillis(timeToSleep);
+        this.delayInMillis = 0L;
+    }
+
+    public Task(String name, long timeToSleepInMillis, long delayInMillis) {
+        this.name = name;
+        this.timeToSleepInMillis = timeToSleepInMillis;
+        this.delayInMillis = delayInMillis;
+    }
+
+    public Task(String name, long timeToSleep, long delay, TimeUnit unit) {
+        this.name = name;
+        this.timeToSleepInMillis = unit.toMillis(timeToSleep);
+        this.delayInMillis = unit.toMillis(delay);
+    }
+
+    @Override
+    public final void run() {
+        LAST_RUN_TIME.put(this.getClass(), System.currentTimeMillis());
+        try {
+            Thread.currentThread().setName(name);
+            execute();
+        } catch (Throwable t) {
+            log.warn("Whoa! Unhandled throwable!", t);
+        } finally {
+            Thread.currentThread().setName(IDLE_NAME);
         }
     }
 
     /**
      * This is where your tasks code actually runs!
      */
-    void execute();
+    protected abstract void execute();
 
-    default long delay() {
-        return 0;
+    public static void start(Task agent) {
+        LAST_RUN_TIME.put(agent.getClass(), 0L);
+        AGENTS.scheduleAtFixedRate(agent, agent.delayInMillis, agent.timeToSleepInMillis, TimeUnit.MILLISECONDS);
     }
 
-    default long repeat() {
-        return -1;
+    //start the agent without a delay
+    public static void startNow(Task agent) {
+        LAST_RUN_TIME.put(agent.getClass(), 0L);
+        AGENTS.scheduleAtFixedRate(agent, 0L, agent.timeToSleepInMillis, TimeUnit.MILLISECONDS);
     }
+
+    public static Map<Class<? extends Task>, Long> getLastRunTimes() {
+        return Collections.unmodifiableMap(LAST_RUN_TIME);
+    }
+
+    public static void shutdown() {
+        AGENTS.shutdown();
+    }
+
+
 }
