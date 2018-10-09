856b66a959d3408a29198a922108e5c97bff85c1
Unknown <theweeryan17@gmail.com>
2018-10-08 22:01:47 -0500

    Dms wooo!
    I might switch all of these over to just accept Message object later, or at the very least switch the base methods to the message object.

--- src/main/java/com/cascadebot/cascadebot/messaging/MessageContext.java
+++ src/main/java/com/cascadebot/cascadebot/messaging/MessageContext.java
@@ -6,6 +6,7 @@
 package com.cascadebot.cascadebot.messaging;
 
 import com.cascadebot.cascadebot.tasks.AutoDeleteMessageTask;
+import net.dv8tion.jda.core.MessageBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.Message;
@@ -63,5 +64,29 @@ public class MessageContext {
 
     public void sendAutoDeleteEmbedMessage(MessageEmbed embed, long delay) {
         channel.sendMessage(embed).queue(deleteMessage -> new AutoDeleteMessageTask(deleteMessage, delay).run());
+    }
+
+    public void sendDm(String message) {
+       sendDm(message, false);
+    }
+
+    public void sendDm(String message, boolean allowChannel) {
+        member.getUser().openPrivateChannel().queue(channel -> channel.sendMessage(message).queue(), exception -> {
+            if(allowChannel) {
+                sendAutoDeleteMessage(message);
+            }
+        });
+    }
+
+    public void sendEmbedDm(MessageEmbed embed) {
+        sendEmbedDm(embed, false);
+    }
+
+    public void sendEmbedDm(MessageEmbed embed, boolean allowChannel) {
+        member.getUser().openPrivateChannel().queue(channel -> channel.sendMessage(embed).queue(), exception -> {
+            if(allowChannel) {
+                sendAutoDeleteEmbedMessage(embed);
+            }
+        });
     }
 }
