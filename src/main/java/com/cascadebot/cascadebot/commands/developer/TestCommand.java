a72dc093175b641d8a5ee613caecf6d4af87d22b
William Oldham <wegg7250@gmail.com>
2018-10-09 17:52:53 +0100

    This tests the Task functionality good

--- src/main/java/com/cascadebot/cascadebot/commands/developer/TestCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/developer/TestCommand.java
@@ -3,12 +3,25 @@ package com.cascadebot.cascadebot.commands.developer;
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.CommandType;
 import com.cascadebot.cascadebot.commandmeta.ICommandRestricted;
+import com.cascadebot.cascadebot.tasks.Task;
 import net.dv8tion.jda.core.entities.Member;
 
 public class TestCommand implements ICommandRestricted {
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        context.sendAutoDeleteMessage("Test 1");
+
+        new Task("Hello", 2000, 5000) {
+
+            private int i = 0;
+
+            @Override
+            protected void execute() {
+                if (++i == 5) {
+                    cancel();
+                }
+                context.sendDm("hi");
+            }
+        }.start();
     }
 
     @Override
