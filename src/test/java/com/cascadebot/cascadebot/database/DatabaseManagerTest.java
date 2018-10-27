af90a79ae428b27d9ace5af63f4b64ce923f8a00
William Oldham <wegg7250@gmail.com>
2018-10-27 20:47:36 +0100

    Array of strings for memory saving

--- src/test/java/com/cascadebot/cascadebot/database/DatabaseManagerTest.java
+++ src/test/java/com/cascadebot/cascadebot/database/DatabaseManagerTest.java
@@ -17,13 +17,13 @@ public class DatabaseManagerTest {
 
     @Test
     public void testBuildStandardConnectionString() {
-        assertEquals(DatabaseManager.buildStandardConnectionString("", "", Arrays.asList("localhost"), "", ""), "mongodb://localhost/"); // Single host test
-        assertEquals(DatabaseManager.buildStandardConnectionString("", "", Arrays.asList("localhost", "localhost2"), "", ""), "mongodb://localhost,localhost2/"); // Multiple host test
-        assertEquals(DatabaseManager.buildStandardConnectionString("admin", "", Arrays.asList("localhost"), "", ""), "mongodb://admin@localhost/"); // Username test
-        assertEquals(DatabaseManager.buildStandardConnectionString("admin", "hello", Arrays.asList("localhost"), "", ""), "mongodb://admin:hello@localhost/"); // Username and password test
-        assertEquals(DatabaseManager.buildStandardConnectionString("", "", Arrays.asList("localhost"), "test", ""), "mongodb://localhost/test"); // Database test
-        assertEquals(DatabaseManager.buildStandardConnectionString("", "", Arrays.asList("localhost"), "", "test=2"), "mongodb://localhost/?test=2"); // Options test
-        assertThrows(IllegalArgumentException.class, () -> DatabaseManager.buildStandardConnectionString("", "", new ArrayList<>(), "", "")); // Not allowed to pass empty list of hosts
+        assertEquals(DatabaseManager.buildStandardConnectionString("", "", new String[]{"localhost"}, "", ""), "mongodb://localhost/"); // Single host test
+        assertEquals(DatabaseManager.buildStandardConnectionString("", "", new String[]{"localhost", "localhost2"}, "", ""), "mongodb://localhost,localhost2/"); // Multiple host test
+        assertEquals(DatabaseManager.buildStandardConnectionString("admin", "", new String[]{"localhost"}, "", ""), "mongodb://admin@localhost/"); // Username test
+        assertEquals(DatabaseManager.buildStandardConnectionString("admin", "hello", new String[]{"localhost"}, "", ""), "mongodb://admin:hello@localhost/"); // Username and password test
+        assertEquals(DatabaseManager.buildStandardConnectionString("", "", new String[]{"localhost"}, "test", ""), "mongodb://localhost/test"); // Database test
+        assertEquals(DatabaseManager.buildStandardConnectionString("", "", new String[]{"localhost"}, "", "test=2"), "mongodb://localhost/?test=2"); // Options test
+        assertThrows(IllegalArgumentException.class, () -> DatabaseManager.buildStandardConnectionString("", "", new String[]{}, "", "")); // Not allowed to pass empty list of hosts
     }
 
     @Test
