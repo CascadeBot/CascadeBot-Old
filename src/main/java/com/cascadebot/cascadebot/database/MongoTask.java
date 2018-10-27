da16cf6a9896425a9f6c3da242bade4169bd5241
William Oldham <wegg7250@gmail.com>
2018-10-27 12:16:55 +0100

    Start implementing Mongo DB
    Include Async and Sync driver to have support for both types of operations

---
+++ src/main/java/com/cascadebot/cascadebot/database/MongoTask.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2018 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.database;
+
+@FunctionalInterface
+public interface MongoTask {
+
+    public void run();
+
+
+}
