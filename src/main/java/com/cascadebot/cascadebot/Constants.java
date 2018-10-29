e7d834081cc48b13676e8987329a2065e3e63991
William Oldham <wegg7250@gmail.com>
2018-10-29 15:12:52 +0000

    Store config version here, final because we will only change with a change of code

--- src/main/java/com/cascadebot/cascadebot/Constants.java
+++ src/main/java/com/cascadebot/cascadebot/Constants.java
@@ -10,6 +10,8 @@ import java.util.regex.Pattern;
 
 public class Constants {
 
+    public static final String CONFIG_VERSION = "1.0.0";
+
     public static final Pattern INTEGER_REGEX = Pattern.compile("-?[0-9]+");
     public static final Pattern DECIMAL_REGEX = Pattern.compile("-?[0-9]*([.,])[0-9]+");
     public static final Pattern MULTISPACE_REGEX = Pattern.compile(" {2,}");
