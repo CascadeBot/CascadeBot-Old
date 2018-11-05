103461143b7356ced31442e944d2b3772c27a411
weeryan17 <theweeryan17@gmail.com>
2018-11-05 13:36:57 -0600

    Start track logic

--- src/main/java/com/cascadebot/cascadebot/music/CascadePlayer.java
+++ src/main/java/com/cascadebot/cascadebot/music/CascadePlayer.java
@@ -5,13 +5,18 @@
 
 package com.cascadebot.cascadebot.music;
 
+import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import lavalink.client.player.IPlayer;
 import lavalink.client.player.LavaplayerPlayerWrapper;
 
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Map;
+import java.util.Queue;
 
 public class CascadePlayer {
+
+    private Queue<AudioTrack> tracks = new LinkedList<>();
 
     private static Map<Long, CascadePlayer> playerMap = new HashMap<>();
 
@@ -21,11 +26,27 @@ public class CascadePlayer {
         player = MusicHandler.isLavalinkEnabled() ?
                 MusicHandler.getLavaLink().getLink(guildId.toString()).getPlayer() :
                 new LavaplayerPlayerWrapper(MusicHandler.createLavaLinkPlayer());
+        player.addListener(new PlayerListener(this));
     }
 
     public static CascadePlayer getCascadePlayer(Long guildId) {
         return playerMap.computeIfAbsent(guildId, CascadePlayer::new);
     }
 
+    public IPlayer getPlayer() {
+        return player;
+    }
+
+    public Queue<AudioTrack> getTracks() {
+        return tracks;
+    }
+
     //TODO implement player methods
+    public void addTrack(AudioTrack track) {
+        if(player.getPlayingTrack() != null) {
+            tracks.add(track);
+        } else {
+            player.playTrack(track);
+        }
+    }
 }
