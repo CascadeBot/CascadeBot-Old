7ba3367da012205ecc8ba8c3a8f176bfca88a43a
William Oldham <wegg7250@gmail.com>
2018-11-11 12:04:22 +0000

    Tests

---
+++ src/test/java/com/cascadebot/cascadebot/data/VersionTest.java
@@ -0,0 +1,35 @@
+/*
+ * Copyright (c) 2018 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.data;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class VersionTest {
+
+    @Test
+    void toStringTest() {
+        assertEquals(Version.of(1,0,0).toString(), "1.0.0");
+        assertEquals(Version.of(-1, -10, -20).toString(), "1.10.20");
+    }
+
+    @Test
+    void equalsTest() {
+        assertEquals(Version.of(1,0,0), Version.of(1,0,0));
+        assertNotEquals(Version.of(1, 0, 0), Version.of(1, 0, 1));
+    }
+
+    @Test
+    void hashCodeTest() {
+        assertEquals(Version.of(1,0,0).hashCode(), Version.of(1,0,0).hashCode());
+    }
+
+    @Test
+    void compareToTest() {
+    }
+
+}
