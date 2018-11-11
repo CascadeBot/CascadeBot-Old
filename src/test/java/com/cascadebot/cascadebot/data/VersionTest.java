fdd955d4ccb0703d68d5848a81b20d31426f0cae
William Oldham <wegg7250@gmail.com>
2018-11-11 12:10:01 +0000

    Add more tests

--- src/test/java/com/cascadebot/cascadebot/data/VersionTest.java
+++ src/test/java/com/cascadebot/cascadebot/data/VersionTest.java
@@ -13,23 +13,26 @@ class VersionTest {
 
     @Test
     void toStringTest() {
-        assertEquals(Version.of(1,0,0).toString(), "1.0.0");
+        assertEquals(Version.of(1, 0, 0).toString(), "1.0.0");
         assertEquals(Version.of(-1, -10, -20).toString(), "1.10.20");
     }
 
     @Test
     void equalsTest() {
-        assertEquals(Version.of(1,0,0), Version.of(1,0,0));
+        assertEquals(Version.of(1, 0, 0), Version.of(1, 0, 0));
         assertNotEquals(Version.of(1, 0, 0), Version.of(1, 0, 1));
     }
 
     @Test
     void hashCodeTest() {
-        assertEquals(Version.of(1,0,0).hashCode(), Version.of(1,0,0).hashCode());
+        assertEquals(Version.of(1, 0, 0).hashCode(), Version.of(1, 0, 0).hashCode());
+        assertNotEquals(Version.of(1, 0, 0).hashCode(), Version.of(1, 0, 1).hashCode());
     }
 
     @Test
     void compareToTest() {
+        assertEquals(-1, Version.of(1, 0, 0).compareTo(Version.of(1, 0, 1)));
+        assertEquals(0, Version.of(1, 0, 0).compareTo(Version.of(1, 0, 0)));
     }
 
 }
