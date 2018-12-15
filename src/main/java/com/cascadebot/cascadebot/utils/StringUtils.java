62c5aec3ff2422e487e9ef04b5412297bb71b7e6
William Oldham <wegg7250@gmail.com>
2018-12-15 21:30:12 +0000

    Docs!

--- src/main/java/com/cascadebot/cascadebot/utils/StringUtils.java
+++ src/main/java/com/cascadebot/cascadebot/utils/StringUtils.java
@@ -7,6 +7,13 @@ package com.cascadebot.cascadebot.utils;
 
 public class StringUtils {
 
+    /**
+     * Creates a progress bar using unicode bars and discord URL formatting
+     * Format: [▬▬▬▬▬▬](url)▬▬▬▬ 60%
+     *
+     * @param percentage The percentage to be represented by the progress bar
+     * @return The discord formatted progress bar
+     */
     public static String getProgressBar(float percentage) {
         int bars = NumberUtils.round(percentage) / 10;
         return "[" + org.apache.commons.lang3.StringUtils.repeat("▬", bars) +
