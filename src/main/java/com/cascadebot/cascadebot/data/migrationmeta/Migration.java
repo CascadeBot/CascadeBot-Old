59b2aefb70c405a257a076eff9c0e0da247279e6
William Oldham <wegg7250@gmail.com>
2018-12-15 20:19:49 +0000

    Remove old version and change expection to fit

--- src/main/java/com/cascadebot/cascadebot/data/migrationmeta/Migration.java
+++ src/main/java/com/cascadebot/cascadebot/data/migrationmeta/Migration.java
@@ -10,11 +10,9 @@ import org.bson.Document;
 
 public abstract class Migration {
 
-    private final Version oldVersion;
     private final Version newVersion;
 
     public Migration(Version oldVersion, Version newVersion) {
-        this.oldVersion = oldVersion;
         this.newVersion = newVersion;
     }
 
@@ -22,8 +20,20 @@ public abstract class Migration {
 
     Document runMigrationInternal(Document document) {
         Document newDocument = runMigration(document);
+        if (Version.parseVer(newDocument.getString("config_version")).compareTo(newVersion) < 0) {
+            throw new MigrationException(
+                    "Intermediate config does not have the expected version! Config has a larger version than the migration!" +
+                            "\nConfig version was expected to be less than {} but it wasn't ;(" +
+                            "\nCan you ask the migration to not change the version plskthnx :)"
+                    , newVersion
+            );
+        }
         newDocument.put("config_version", newVersion.toString());
         return newDocument;
+    }
+
+    public Version getNewVersion() {
+        return newVersion;
     }
 
 }
