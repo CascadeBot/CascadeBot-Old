9d96da79ed9b4221780154df909cc7c75e2ef9c7
William Oldham <wegg7250@gmail.com>
2018-12-16 11:14:48 +0000

    Add helper method to get security level

--- src/main/java/com/cascadebot/cascadebot/permissions/SecurityLevel.java
+++ src/main/java/com/cascadebot/cascadebot/permissions/SecurityLevel.java
@@ -11,7 +11,7 @@ import java.util.Set;
 
 /**
  * Security levels defined by Role IDs or User IDs, A level can be defined by an unlimited amount of roles
- * and users.
+ * and users. Security levels are declared in ascending order.
  */
 public enum SecurityLevel {
     STAFF,
@@ -24,6 +24,22 @@ public enum SecurityLevel {
 
     public boolean isAuthorised(SecurityLevel level) {
         return level.ordinal() <= this.ordinal();
+    }
+
+    /**
+     * Returns the highest security level a user has access to.
+     *
+     * @param userId The ID of the user to check.
+     * @param roleIds The list of role IDs to check against, this will almost always be the roles from the official server.
+     * @return The highest security level the user has access to or {@code null} if they do not have access to anything.
+     */
+    public static SecurityLevel getLevelById(long userId, Set<Long> roleIds) {
+        for (int i = SecurityLevel.values().length - 1; i >= 0; i--) {
+            SecurityLevel level = SecurityLevel.values()[i];
+            if (level.getIds().contains(userId)) return level;
+            if (roleIds.stream().anyMatch(id -> level.getIds().contains(id))) return level;
+        }
+        return null;
     }
 
 }
