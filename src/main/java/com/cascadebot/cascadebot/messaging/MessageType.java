4e40001492fe4be458a3dc468d78895a83a9b254
William Oldham <wegg7250@gmail.com>
2018-12-18 19:52:48 +0000

    Message Type

---
+++ src/main/java/com/cascadebot/cascadebot/messaging/MessageType.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2018 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.messaging;
+
+import com.cascadebot.shared.SharedConstants;
+
+import java.awt.*;
+
+public enum MessageType {
+
+    INFO(Color.CYAN, "\u2139"), // ℹ
+    SUCCESS(Color.GREEN, "\u2705"), // ✅
+    WARNING(Color.YELLOW, "\u26A0"), // ⚠
+    MODERATION(Color.WHITE, "\uD83D\uDC6E"), // 👮
+    ERROR(Color.RED, "\u274C"), // ❌
+    NEUTRAL(SharedConstants.CASCADE_COLOR, "");
+
+    private final Color color;
+    private final String emoji;
+
+    MessageType(Color color, String emoji) {
+        this.color = color;
+        this.emoji = emoji;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public String getEmoji() {
+        return emoji;
+    }
+}
