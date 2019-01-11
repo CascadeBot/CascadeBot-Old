6547d0a2f2519e2d7f600b1ca15cd20c14c3de0e
William Oldham <wegg7250@gmail.com>
2019-01-11 17:27:00 +0000

    Make this work with jars

--- src/main/java/com/cascadebot/cascadebot/utils/ReflectionUtils.java
+++ src/main/java/com/cascadebot/cascadebot/utils/ReflectionUtils.java
@@ -5,12 +5,14 @@
 
 package com.cascadebot.cascadebot.utils;
 
+import com.cascadebot.cascadebot.CascadeBot;
+import com.google.common.reflect.ClassPath;
+
 import java.io.File;
 import java.io.IOException;
-import java.net.URL;
 import java.util.ArrayList;
-import java.util.Enumeration;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ReflectionUtils {
 
@@ -22,48 +24,9 @@ public class ReflectionUtils {
      * @throws ClassNotFoundException Thrown if the loader cannot find the class.
      * @throws IOException If something goes badly.
      */
-    public static ArrayList<Class> getClasses(String packageName) throws ClassNotFoundException, IOException {
-        ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-        assert classLoader != null;
-        String path = packageName.replace('.', '/');
-        Enumeration<URL> resources = classLoader.getResources(path);
-
-        List<File> dirs = new ArrayList<>();
-        while (resources.hasMoreElements()) {
-            URL resource = resources.nextElement();
-            dirs.add(new File(resource.getFile()));
-        }
-
-        ArrayList<Class> classes = new ArrayList<>();
-        for (File directory : dirs)
-            classes.addAll(findClasses(directory, packageName));
-
-        return classes;
-    }
-
-    /**
-     * Recursive method used to find all classes in a given directory and subdirs.
-     *
-     * @param directory   The base directory
-     * @param packageName The package name for classes found inside the base directory
-     * @return The classes
-     * @throws ClassNotFoundException Thrown if the loader cannot find the class.
-     */
-    private static List<Class> findClasses(File directory, String packageName) throws ClassNotFoundException {
-        List<Class> classes = new ArrayList<>();
-        if (!directory.exists())
-            return classes;
-
-        File[] files = directory.listFiles();
-        assert files != null;
-        for (File file : files) {
-            if (file.isDirectory()) {
-                assert !file.getName().contains(".");
-                classes.addAll(findClasses(file, packageName + "." + file.getName()));
-            } else if (file.getName().endsWith(".class"))
-                classes.add(Class.forName(packageName + '.' + file.getName().substring(0, file.getName().length() - 6)));
-        }
-        return classes;
+    public static List<Class<?>> getClasses(String packageName) throws ClassNotFoundException, IOException {
+        List<ClassPath.ClassInfo> classInfos = ClassPath.from(CascadeBot.class.getClassLoader()).getTopLevelClassesRecursive("com.cascadebot.cascadebot.commands").asList();
+        return classInfos.stream().map(ClassPath.ClassInfo::load).collect(Collectors.toList());
     }
 
 }
