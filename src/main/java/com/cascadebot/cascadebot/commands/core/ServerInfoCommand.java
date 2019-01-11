b67d0c8792d346d009b0c67110675cb68f50bc2f
James <40138757+squeezeboxyt@users.noreply.github.com>
2019-01-11 17:06:08 +0000

    Push ServerInfoCommand.java, I've been working on this for a while so it should be gucci

---
+++ src/main/java/com/cascadebot/cascadebot/commands/core/ServerInfoCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2018 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.commands.core;
+
+import com.cascadebot.cascadebot.CascadeBot;
+import com.cascadebot.cascadebot.commandmeta.CommandContext;
+import com.cascadebot.cascadebot.commandmeta.CommandType;
+import com.cascadebot.cascadebot.commandmeta.ICommand;
+import com.cascadebot.cascadebot.utils.DiscordUtils;
+import com.cascadebot.cascadebot.utils.pagination.Page;
+import com.cascadebot.cascadebot.utils.pagination.PageObjects;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.*;
+import org.bukkit.Server;
+
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class ServerInfoCommand implements ICommand {
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+
+        List<Page> pageList = new ArrayList<>();
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setTitle(context.getGuild().getName());
+        builder.setThumbnail(context.getGuild().getIconUrl());
+        builder.addField("Creation Date", context.getGuild().getCreationTime().format(DateTimeFormatter.RFC_1123_DATE_TIME), true);
+        builder.addField("Guild Name", context.getGuild().getName(), true);
+        builder.addField("Owner", context.getGuild().getOwner().toString(), true);
+        builder.addField("Region", context.getGuild().getRegion().toString(), true);
+        builder.addField("Guild ID", context.getGuild().getId(), true);
+        builder.addField("Member Count", context.getGuild().getMembers().size() + "\n", true);
+        pageList.add(new PageObjects.EmbedPage(builder));
+
+        context.sendPagedMessage(pageList);
+    }
+
+    @Override
+    public String defaultCommand() {
+        return "serverinfo";
+    }
+
+    @Override
+    public CommandType getType() {
+        return null;
+    }
+}
