8022e2b0d0fb31d10e11af36791be90fa345ca2b
Squeezebox <jhawki2005@gmail.com>
2019-01-12 16:22:53 +0000

    feature/userinfocommand (#11)
    * Add variable guildForInfo and make all sections rely on that
    
    * Add userMessage variable
    
    * Remove unused imports
    
    * Make ;serverinfo work for any guild (hopefully)
    
    * changes from pr
    
    * proper fetching of the arg

--- src/main/java/com/cascadebot/cascadebot/commands/core/ServerInfoCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/core/ServerInfoCommand.java
@@ -5,28 +5,42 @@
 
 package com.cascadebot.cascadebot.commands.core;
 
+import com.cascadebot.cascadebot.CascadeBot;
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.CommandType;
 import com.cascadebot.cascadebot.commandmeta.ICommand;
-import com.cascadebot.cascadebot.messaging.MessagingObjects;
+import com.cascadebot.cascadebot.utils.DiscordUtils;
 import com.cascadebot.cascadebot.utils.FormatUtils;
+import net.dv8tion.jda.bot.sharding.ShardManager;
 import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
 import java.util.Set;
 
 public class ServerInfoCommand implements ICommand {
     @Override
     public void onCommand(Member sender, CommandContext context) {
+        Guild guildForInfo = context.getGuild();
+
+        if(context.getArgs().length > 0) {
+            guildForInfo = CascadeBot.INS.getShardManager().getGuildById(context.getArg(0));
+        }
+        if(guildForInfo == null) {
+            context.replyDanger("We couldn't find that guild!");
+            return;
+        }
+
         EmbedBuilder builder = new EmbedBuilder();
-        builder.setTitle(context.getGuild().getName());
-        builder.setThumbnail(context.getGuild().getIconUrl());
+        builder.setTitle(guildForInfo.getName());
+        builder.setThumbnail(guildForInfo.getIconUrl());
         builder.setDescription("Guild Information");
-        builder.addField("Creation Date", FormatUtils.formatDateTime(context.getGuild().getCreationTime()), true);
-        builder.addField("Guild Name", context.getGuild().getName(), true);
-        builder.addField("Owner", context.getGuild().getOwner().getUser().getAsTag(), true);
-        builder.addField("Region", context.getGuild().getRegion().toString(), true);
-        builder.addField("Member Count", String.valueOf(context.getGuild().getMembers().size()), true);
-        builder.setFooter("ID: " + context.getGuild().getId(), context.getGuild().getIconUrl());
+        builder.addField("Creation Date", FormatUtils.formatDateTime(guildForInfo.getCreationTime()), true);
+        builder.addField("Guild Name", guildForInfo.getName(), true);
+        builder.addField("Owner", guildForInfo.getOwner().getUser().getAsTag(), true);
+        builder.addField("Region", guildForInfo.getRegion().toString(), true);
+        builder.addField("Member Count", String.valueOf(guildForInfo.getMembers().size()), true);
+        builder.setFooter("ID: " + guildForInfo.getId(), guildForInfo.getIconUrl());
 
         context.replyInfo(builder);
     }
