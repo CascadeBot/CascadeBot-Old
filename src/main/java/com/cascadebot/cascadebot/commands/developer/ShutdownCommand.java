c144a487d3f1f407d44b23f1a238daf2453602ce
William Oldham <wegg7250@gmail.com>
2019-01-20 21:48:19 +0000

    Add confirmation to shutdown

--- src/main/java/com/cascadebot/cascadebot/commands/developer/ShutdownCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/developer/ShutdownCommand.java
@@ -11,13 +11,25 @@ import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.CommandType;
 import com.cascadebot.cascadebot.commandmeta.ICommandRestricted;
 import com.cascadebot.cascadebot.permissions.SecurityLevel;
+import com.cascadebot.cascadebot.tasks.Task;
+import com.cascadebot.cascadebot.utils.ConfirmUtils;
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
 
 public class ShutdownCommand implements ICommandRestricted {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        context.reply("Bot is shutting down!");
+        // A confirmation check to make sure we actually want to shut down on production
+        if (!CascadeBot.getVersion().getBuild().equalsIgnoreCase("dev") && !ConfirmUtils.hasConfirmedAction(sender.getUser().getIdLong(), "shutdown_bot")) {
+            context.replyDanger("It looks like the bot is running in production mode, **do you really want to do this?** \nIf so, simply repeat the command again. This confirmation will expire in one minute!");
+            ConfirmUtils.confirmAction(sender.getUser().getIdLong(), "shutdown_bot");
+            return;
+        }
+        EmbedBuilder builder = new EmbedBuilder();
+        builder.setFooter(sender.getUser().getAsTag(), sender.getUser().getEffectiveAvatarUrl());
+        builder.setDescription("Cascade bot shutting down!");
+        context.replyInfo(builder);
         CascadeBot.logger.info("Shutting down via command! Issuer: " + context.getUser().getAsTag());
         ShutdownHandler.stop();
     }
