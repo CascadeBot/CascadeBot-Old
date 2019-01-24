94abc7aa1aba07ebd4e85770e705c2069b11dc60
William Oldham <wegg7250@gmail.com>
2019-01-24 08:01:29 +0000

    Begin to add permissions objects

---
+++ src/main/java/com/cascadebot/cascadebot/permissions/objects/User.java
@@ -0,0 +1,29 @@
+package com.cascadebot.cascadebot.permissions.objects;
+
+import com.cascadebot.cascadebot.permissions.Permission;
+import com.google.common.collect.Sets;
+
+import java.util.Set;
+
+public class User {
+
+    private final Set<String> groups = Sets.newConcurrentHashSet();
+    private final Set<Permission> permissions = Sets.newConcurrentHashSet();
+
+    public boolean addGroup(Group group) {
+        return groups.add(group.getName());
+    }
+
+    public boolean removeGroup(Group group) {
+        return groups.remove(group.getName());
+    }
+
+
+    public Set<String> getGroups() {
+        return groups;
+    }
+
+    public Set<Permission> getPermissions() {
+        return permissions;
+    }
+}
