a8f2b0de2b7f19fbfa72d7d7e9ba543554965bb4
William Oldham <wegg7250@gmail.com>
2019-01-24 21:50:29 +0000

    Update missing copyright

--- src/main/java/com/cascadebot/cascadebot/commandmeta/IMainCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commandmeta/IMainCommand.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
 package com.cascadebot.cascadebot.commandmeta;
 
 import com.cascadebot.cascadebot.permissions.Permission;
