88ae7541c585bb1b0574ea2725d36ff7d39b1eb9
William Oldham <wegg7250@gmail.com>
2019-01-25 07:55:01 +0000

    Still changing things...

--- src/main/java/com/cascadebot/cascadebot/commands/core/InviteCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/core/InviteCommand.java
@@ -7,11 +7,11 @@ package com.cascadebot.cascadebot.commands.core;
 
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.CommandType;
-import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
+import com.cascadebot.cascadebot.commandmeta.IMainCommand;
 import com.cascadebot.cascadebot.permissions.Permission;
 import net.dv8tion.jda.core.entities.Member;
 
-public class InviteCommand implements ICommandExecutable {
+public class InviteCommand implements IMainCommand {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
@@ -19,7 +19,7 @@ public class InviteCommand implements ICommandExecutable {
     }
 
     @Override
-    public String defaultCommand() {
+    public String command() {
         return "invite";
     }
 
