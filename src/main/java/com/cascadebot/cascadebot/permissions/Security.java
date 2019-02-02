1b0967c45aab6a5e0a12f2ae93bf01dadd8c22d1
William Oldham <wegg7250@gmail.com>
2019-02-02 20:40:09 +0000

    Make security a static util for the shared security level

---
+++ src/main/java/com/cascadebot/cascadebot/permissions/Security.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.permissions;
+
+import com.cascadebot.cascadebot.data.Config;
+import com.cascadebot.shared.SecurityLevel;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+/**
+ * Security levels defined by Role IDs or User IDs, A level can be defined by an unlimited amount of roles
+ * and users. Security levels are declared in ascending order.
+ */
+public class Security {
+
+    public static Set<Long> getIds(SecurityLevel level) {
+        return Config.INS.getSecurityLevels().get(level);
+    }
+
+    public static boolean isAuthorised(SecurityLevel level, SecurityLevel comparingLevel) {
+        return comparingLevel.isAuthorised(level);
+    }
+
+    public static SecurityLevel getSecurityLevelById(long userId, Set<Long> roleIds){
+        if(getLevelById(userId, roleIds) == null) {
+            return null;
+        }
+        return getLevelById(userId, roleIds);
+    }
+
+    /**
+     * Returns the highest security level a user has access to.
+     *
+     * @param userId The ID of the user to check.
+     * @param roleIds The list of role IDs to check against, this will almost always be the roles from the official server.
+     * @return The highest security level the user has access to or {@code null} if they do not have access to anything.
+     */
+    public static SecurityLevel getLevelById(long userId, Set<Long> roleIds) {
+        for (int i = SecurityLevel.values().length - 1; i >= 0; i--) {
+            SecurityLevel level = SecurityLevel.values()[i];
+            if (Security.getIds(level).contains(userId)) return level;
+            if (roleIds.stream().anyMatch(id -> Security.getIds(level).contains(id))) return level;
+        }
+        return null;
+    }
+
+}
