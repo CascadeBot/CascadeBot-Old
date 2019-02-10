2769b0bdea8a3051d29ccedccb66c790d8418470
William Oldham <wegg7250@gmail.com>
2019-02-10 15:10:12 +0000

    Add getters and a map for module -> permission getting

--- src/main/java/com/cascadebot/cascadebot/permissions/PermissionsManager.java
+++ src/main/java/com/cascadebot/cascadebot/permissions/PermissionsManager.java
@@ -6,19 +6,22 @@
 package com.cascadebot.cascadebot.permissions;
 
 import com.cascadebot.cascadebot.commandmeta.CommandManager;
-import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
 import com.cascadebot.cascadebot.commandmeta.ICommandMain;
 import com.cascadebot.cascadebot.commandmeta.ICommandRestricted;
+import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.data.objects.GuildData;
 import com.cascadebot.cascadebot.utils.DiscordUtils;
 import com.cascadebot.shared.SecurityLevel;
 import com.github.benmanes.caffeine.cache.Caffeine;
 import com.github.benmanes.caffeine.cache.LoadingCache;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.TimeUnit;
@@ -37,6 +40,7 @@ public class PermissionsManager {
             .build(id -> Security.getLevelById(id, officialGuildRoleIDCache.get(id)));
 
     private ConcurrentHashMap<String, Permission> permissions = new ConcurrentHashMap<>();
+    private Map<Module, Permission> permissionModuleMap = Map.of();
 
     public void registerPermissions() {
         if (!permissions.isEmpty()) throw new IllegalStateException("Permissions have already been registered!");
@@ -51,17 +55,31 @@ public class PermissionsManager {
             }
         }
 
-        registerPermission(Permission.of("All permissions", "*"));
-
         registerPermission(Permission.of("Core Category", "module.core", true, Module.CORE));
         registerPermission(Permission.of("Info Category", "module.info", true, Module.INFORMATIONAL));
 
         LOGGER.info("{} permissions loaded in {}ms!", permissions.size(), System.currentTimeMillis() - startTime);
 
+        Map<Module, Permission> tempMap = new HashMap<>();
+        permissions
+                .entrySet()
+                .stream()
+                .filter(p -> p.getValue().getModule() != null)
+                .forEach((e) -> tempMap.put(e.getValue().getModule(), e.getValue()));
+        permissionModuleMap = Map.copyOf(tempMap);
+
     }
 
     private void registerPermission(Permission permission) {
         permissions.put(permission.getPermissionNode(), permission);
+    }
+
+    public Permission getPermission(String permission) {
+        return permissions.get(permission);
+    }
+
+    public Permission getPermissionFromModule(Module module) {
+        return permissionModuleMap.get(module);
     }
 
 
