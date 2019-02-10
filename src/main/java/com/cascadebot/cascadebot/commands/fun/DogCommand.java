9d511b18f8461028578976ab8c81c8da93969654
James Hawkins <jhawki2005@gmail.com>
2019-02-10 18:10:57 +0000

    Comply with wildcard regulations (#25)
    * Comply with the new regulations restricting wildcard imports.
    
    * Fix some spacing to comply
    
    * Add FUN category to PermissionsManager
    
    * Mistook onCommand for a class header what an idiot i am
    
    * Actually register the permissions
    
    * Remove force default
    
    * misunderstood instructions

--- src/main/java/com/cascadebot/cascadebot/commands/fun/DogCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/fun/DogCommand.java
@@ -9,10 +9,12 @@ import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.CommandType;
 import com.cascadebot.cascadebot.commandmeta.ICommandMain;
 import com.cascadebot.cascadebot.permissions.Permission;
-import com.google.gson.JsonObject;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
-import okhttp3.*;
+import okhttp3.OkHttpClient;
+import okhttp3.HttpUrl;
+import okhttp3.Request;
+import okhttp3.Response;
 import org.json.JSONArray;
 import org.json.JSONObject;
 import java.io.IOException;
@@ -23,17 +25,14 @@ public class DogCommand implements ICommandMain {
     @Override
     public void onCommand(Member sender, CommandContext context) {
         OkHttpClient client = new OkHttpClient();
-
         HttpUrl.Builder urlBuilder = HttpUrl.parse("https://api.thedogapi.com/v1/images/search").newBuilder();
         String url = urlBuilder.build().toString();
-
         Request request = new Request.Builder()
                 .url(url)
                 .build();
 
         try {
             Response response = client.newCall(request).execute();
-
             if (response != null) {
                 JSONArray jsonArray = new JSONArray(response.body().string());
                 JSONObject jsonObject = jsonArray.getJSONObject(0);
@@ -57,18 +56,13 @@ public class DogCommand implements ICommandMain {
     }
 
     @Override
-    public boolean forceDefault() {
-        return true;
-    }
-
-    @Override
     public CommandType getType() {
         return CommandType.FUN;
     }
 
     @Override
     public Permission getPermission() {
-        return null; // Cannot be restricted
+        return Permission.of("Dog command", "dog", true);
     }
 
 }
