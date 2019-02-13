15a3ce9c7a57c825c88b9f237059fa771fe679ee
weeryan17 <theweeryan17@gmail.com>
2019-02-13 13:41:40 -0600

    We don't use those methods

--- src/main/java/com/cascadebot/cascadebot/messaging/MessagingObjects.java
+++ src/main/java/com/cascadebot/cascadebot/messaging/MessagingObjects.java
@@ -34,56 +34,8 @@ public final class MessagingObjects {
         return getClearThreadLocalEmbedBuilder().setColor(messageType.getColor());
     }
 
-    public static EmbedBuilder getColoredEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.NEUTRAL);
-    }
-
-    public static EmbedBuilder getSuccessEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.SUCCESS);
-    }
-
-    public static EmbedBuilder getInfoEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.INFO);
-    }
-
-    public static EmbedBuilder getWarningEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.WARNING);
-    }
-
-    public static EmbedBuilder getDangerEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.DANGER);
-    }
-
-    public static EmbedBuilder getModerationEmbedBuilder() {
-        return getMessageTypeEmbedBuilder(MessageType.MODERATION);
-    }
-
     public static MessageBuilder getMessageTypeMessageBuilder(MessageType messageType) {
         return getClearThreadLocalMessageBuilder().append(messageType.getEmoji()).append(" ");
-    }
-
-    public static MessageBuilder getColoredMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.NEUTRAL);
-    }
-
-    public static MessageBuilder getSuccessMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.SUCCESS);
-    }
-
-    public static MessageBuilder getInfoMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.INFO);
-    }
-
-    public static MessageBuilder getWarningMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.WARNING);
-    }
-
-    public static MessageBuilder getDangerMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.DANGER);
-    }
-
-    public static MessageBuilder getModerationMessageBuilder() {
-        return getMessageTypeMessageBuilder(MessageType.MODERATION);
     }
 
 
