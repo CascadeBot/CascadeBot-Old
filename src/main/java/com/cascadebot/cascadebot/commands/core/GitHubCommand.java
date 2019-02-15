d27336b818bdc1ca1aa8cf25edf5bcddb700344d
James Hawkins <jhawki2005@gmail.com>
2019-02-15 21:25:37 +0000

    Add GitHub command v2

--- src/main/java/com/cascadebot/cascadebot/commands/core/GitHubCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/core/GitHubCommand.java
@@ -1,4 +1,24 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
 package com.cascadebot.cascadebot.commands.core;
 
-public class GitHubCommand {
+import com.cascadebot.cascadebot.commandmeta.CommandContext;
+import com.cascadebot.cascadebot.commandmeta.ICommandCore;
+import net.dv8tion.jda.core.entities.Member;
+
+public class GitHubCommand implements ICommandCore {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        context.reply("https://github.com/CascadeBot/CascadeBot");
+    }
+
+    @Override
+    public String command() {
+        return "github";
+    }
+
 }
