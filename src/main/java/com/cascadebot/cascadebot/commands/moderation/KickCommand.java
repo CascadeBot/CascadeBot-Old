8aef219daf07081721aaed357d6b3db9e28edfa0
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-02-18 21:22:32 +0000

    Feature/kick command (#36)
    * Add spaces around strings
    
    * Add spaces around strings
    
    * Put some stuff here
    
    * Feature\Kick Command
    
    * Remove some stuff
    
    * Change to replyinfo
    
    * Try to use camelCase and change permission
    
    * Check if member is found
    
    * Change if to else
    
    * Get all args
    
    * Make sure Args are filled
    
    * Format code
    
    * default permissions
    
    * remove unnecessary returns
    
    * Make sure we catch those exceptions
    
    * Add Permission
    
    * Fix permission...
    Add Hierarchy info
    
    * Add insufficient permissions info
    
    * Remove this import

---
+++ src/main/java/com/cascadebot/cascadebot/commands/moderation/KickCommand.java
@@ -0,0 +1,54 @@
+package com.cascadebot.cascadebot.commands.moderation;
+
+import com.cascadebot.cascadebot.commandmeta.CommandContext;
+import com.cascadebot.cascadebot.commandmeta.ICommandMain;
+import com.cascadebot.cascadebot.commandmeta.Module;
+import com.cascadebot.cascadebot.permissions.CascadePermission;
+import com.cascadebot.cascadebot.utils.DiscordUtils;
+import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.exceptions.HierarchyException;
+import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
+
+public class KickCommand implements ICommandMain {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        if (context.getArgs().length == 0) {
+            context.replyDanger("Not enough Args (No specified member)");
+            return;
+        }
+        Member targetMember = DiscordUtils.getMember(context.getMessage(0), context.getGuild());
+        if (targetMember == null) {
+            context.replyDanger("Could not find that user");
+        } else {
+            try {
+                context.getGuild().getController().kick(targetMember).queue();
+                context.replyInfo("User: " + targetMember.getUser().getAsTag() + " has been kicked");
+            } catch (InsufficientPermissionException e) {
+                context.replyWarning("Cannot kick user " + targetMember.getUser().getAsTag() +
+                        ", missing Kick Members permission");
+            } catch (HierarchyException e) {
+                context.replyWarning("Cannot kick user " + targetMember.getUser().getAsTag() +
+                        ", the top role they have is higher than mine");
+            }
+        }
+    }
+
+    @Override
+    public String command() {
+        return "kick";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Kick Command", "kick",
+                false, Permission.KICK_MEMBERS);
+    }
+
+
+    @Override
+    public Module getModule() {
+        return Module.MODERATION;
+    }
+}
