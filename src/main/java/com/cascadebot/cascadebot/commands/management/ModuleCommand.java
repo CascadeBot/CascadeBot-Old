786c1496f2b16191c02997e0ecd777550c5f21de
William Oldham <wegg7250@gmail.com>
2019-02-18 09:05:47 +0000

    Add management module

--- src/main/java/com/cascadebot/cascadebot/commands/management/ModuleCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/management/ModuleCommand.java
@@ -1,17 +1,25 @@
-package com.cascadebot.cascadebot.commands.core;
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package com.cascadebot.cascadebot.commands.management;
 
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.ICommandCore;
 import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
+import com.cascadebot.cascadebot.commandmeta.ICommandMain;
+import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.permissions.CascadePermission;
-import com.cascadebot.cascadebot.subcommands.module.ModuleDisableSubCommand;
-import com.cascadebot.cascadebot.subcommands.module.ModuleEnableSubCommand;
-import com.cascadebot.cascadebot.subcommands.module.ModuleListSubCommand;
+import com.cascadebot.cascadebot.commands.subcommands.module.ModuleDisableSubCommand;
+import com.cascadebot.cascadebot.commands.subcommands.module.ModuleEnableSubCommand;
+import com.cascadebot.cascadebot.commands.subcommands.module.ModuleListSubCommand;
+import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 
 import java.util.Set;
 
-public class ModuleCommand implements ICommandCore {
+public class ModuleCommand implements ICommandMain {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
@@ -24,13 +32,18 @@ public class ModuleCommand implements ICommandCore {
     }
 
     @Override
+    public Module getModule() {
+        return Module.MANAGEMENT;
+    }
+
+    @Override
     public Set<ICommandExecutable> getSubCommands() {
         return Set.of(new ModuleDisableSubCommand(), new ModuleEnableSubCommand(), new ModuleListSubCommand());
     }
 
     @Override
     public CascadePermission getPermission() {
-        return CascadePermission.of("core.module");
+        return CascadePermission.of("Module command", "module", false, Permission.MANAGE_SERVER);
     }
 
 }
