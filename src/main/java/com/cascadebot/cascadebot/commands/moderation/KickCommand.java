208e574c52b55ef53c75de607b72ed612e5491de
Ikinon <joshuabarker3233@hotmail.com>
2019-02-18 13:18:07 +0000

    Add spaces around strings

--- src/main/java/com/cascadebot/cascadebot/commands/moderation/KickCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/moderation/KickCommand.java
@@ -1,4 +1,10 @@
 package com.cascadebot.cascadebot.commands.moderation;
 
+import com.cascadebot.cascadebot.commandmeta.CommandContext;
+import com.cascadebot.cascadebot.commandmeta.Module;
+import com.cascadebot.cascadebot.commandmeta.ICommandMain;
+
 public class KickCommand {
+
+
 }
