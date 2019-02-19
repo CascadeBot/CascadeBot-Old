6697e0fad525d953ba7f212a4686cc669e938951
William Oldham <wegg7250@gmail.com>
2019-02-19 19:54:37 +0000

    Ooo documentation

--- src/main/java/com/cascadebot/cascadebot/commandmeta/Module.java
+++ src/main/java/com/cascadebot/cascadebot/commandmeta/Module.java
@@ -8,12 +8,13 @@ package com.cascadebot.cascadebot.commandmeta;
 import java.util.Set;
 
 public enum Module {
-    MODERATION,
-    CORE,
-    FUN,
-    INFORMATIONAL,
-    MANAGEMENT,
-    DEVELOPER(false);
+
+    CORE, // A module that contains the bot's meta commands. These cannot be disabled!
+    MANAGEMENT, // This module contains commands that are used to manage the bot settings for the guild.
+    INFORMATIONAL, // The commands in this module display various pieces of information about discord entities.
+    MODERATION, // This module speaks for itself, it contains commands that allow the admins of a guild to moderate said guild
+    FUN, // This is a bit of a random module containing smaller commands.
+    DEVELOPER(false); // All our special commands :D
 
     // A set of modules that are always enabled and cannot be disabled
     public static final Set<Module> CORE_MODULES = Set.of(Module.CORE, Module.MANAGEMENT);
