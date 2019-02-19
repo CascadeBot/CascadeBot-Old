c678a47867e94f98cfbf2056849adcbc12ab90b9
William Oldham <wegg7250@gmail.com>
2019-02-19 19:46:26 +0000

    Only show public modules on this list

--- src/main/java/com/cascadebot/cascadebot/commands/subcommands/module/ModuleListSubCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/subcommands/module/ModuleListSubCommand.java
@@ -20,6 +20,7 @@ public class ModuleListSubCommand implements ICommandExecutable {
     @Override
     public void onCommand(Member sender, CommandContext context) {
         context.reply("**Modules**\n" + Arrays.stream(Module.values())
+                .filter(Module::isPublicModule)
                 .map(module -> module.toString().toLowerCase() +
                         " - " +
                         (context.getData().isModuleEnabled(module) ? "Enabled" : "Disabled"))
