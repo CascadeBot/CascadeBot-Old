594a68d147f4f2df42e8be5590cf4570b1da238b
William Oldham <wegg7250@gmail.com>
2019-02-19 10:06:23 +0000

    Add a reply exception with formatting and change exception to throwable

--- src/main/java/com/cascadebot/cascadebot/commandmeta/CommandException.java
+++ src/main/java/com/cascadebot/cascadebot/commandmeta/CommandException.java
@@ -9,8 +9,8 @@ import net.dv8tion.jda.core.entities.Guild;
 
 public class CommandException extends RuntimeException {
 
-    public CommandException(Exception e, Guild guild, String trigger) {
-        super("Error while processing command!\nGuild ID: " + guild.getId() + (trigger.isBlank() ? "" : " Trigger: " + trigger), e);
+    public CommandException(Throwable cause, Guild guild, String trigger) {
+        super("Error while processing command!\nGuild ID: " + guild.getId() + (trigger.isBlank() ? "" : " Trigger: " + trigger), cause);
     }
 
 }
