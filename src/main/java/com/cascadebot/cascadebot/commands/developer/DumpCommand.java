4909933922ba0b06232cff5a39223801a31988ca
William Oldham <wegg7250@gmail.com>
2019-02-20 09:26:01 +0000

    Use table object and builder

--- src/main/java/com/cascadebot/cascadebot/commands/developer/DumpCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/developer/DumpCommand.java
@@ -14,6 +14,7 @@ import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.permissions.CascadePermission;
 import com.cascadebot.cascadebot.utils.FormatUtils;
 import com.cascadebot.cascadebot.utils.PasteUtils;
+import com.cascadebot.cascadebot.utils.Table;
 import com.cascadebot.cascadebot.utils.pagination.PageObjects;
 import com.cascadebot.shared.SecurityLevel;
 import net.dv8tion.jda.core.entities.Member;
@@ -34,23 +35,22 @@ public class DumpCommand implements ICommandRestricted {
             String threads = "```\n" + Thread.getAllStackTraces().keySet().stream().map(Thread::getName).sorted().collect(Collectors.joining("\n")) + "```";
             PasteUtils.pasteIfLong(threads, 2048, context::reply);
         } else if (context.getArg(0).equalsIgnoreCase("commands")) {
-            List<String> header = List.of("Command", "Module", "Permission", "Subcommands");
-            List<List<String>> body = new ArrayList<>();
+            Table.TableBuilder builder = new Table.TableBuilder("Command", "Module", "Permission", "Subcommands");
             for (ICommandMain command : CascadeBot.INS.getCommandManager().getCommands()) {
-                body.add(List.of(
-                        command.command(),
-                        command.getModule().toString(),
-                        command.getPermission() == null ? "No permission" : command.getPermission().toString(),
-                        command.getSubCommands().stream().map(ICommandExecutable::command).collect(Collectors.toSet()).toString()));
+                builder.addRow(
+                    command.command(),
+                    command.getModule().toString(),
+                    command.getPermission() == null ? "No permission" : command.getPermission().toString(),
+                    command.getSubCommands().stream().map(ICommandExecutable::command).collect(Collectors.toSet()).toString()
+                );
             }
-            PasteUtils.pasteIfLong(FormatUtils.makeAsciiTable(header, body, null), 2048, context::reply);
+            PasteUtils.pasteIfLong(builder.build().toString(), 2048, context::reply);
         } else if (context.getArg(0).equalsIgnoreCase("permissions")) {
-            List<String> header = List.of("Permission", "Discord Perms");
-            List<List<String>> body = new ArrayList<>();
+            Table.TableBuilder builder = new Table.TableBuilder("Permission", "Discord Perms");
             for (CascadePermission permission : CascadeBot.INS.getPermissionsManager().getPermissions()) {
-                body.add(List.of(permission.getPermissionNode(), permission.getDiscordPerm().toString()));
+                builder.addRow(permission.getPermissionNode(), permission.getDiscordPerm().toString());
             }
-            PasteUtils.pasteIfLong(FormatUtils.makeAsciiTable(header, body, null), 2048, context::reply);
+            PasteUtils.pasteIfLong(builder.build().toString(), 2048, context::reply);
         } else {
             context.replyDanger("I can't seem to find that argument \uD83E\uDD14" /* Thinking emoji 🤔 */);
         }
