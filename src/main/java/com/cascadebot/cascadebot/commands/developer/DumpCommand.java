0a1389be643b6b008cfd2bd6541331ce10f71571
William Oldham <wegg7250@gmail.com>
2019-02-20 09:05:08 +0000

    Add tables into dump command

--- src/main/java/com/cascadebot/cascadebot/commands/developer/DumpCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/developer/DumpCommand.java
@@ -11,10 +11,15 @@ import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
 import com.cascadebot.cascadebot.commandmeta.ICommandMain;
 import com.cascadebot.cascadebot.commandmeta.ICommandRestricted;
 import com.cascadebot.cascadebot.commandmeta.Module;
+import com.cascadebot.cascadebot.permissions.CascadePermission;
+import com.cascadebot.cascadebot.utils.FormatUtils;
 import com.cascadebot.cascadebot.utils.PasteUtils;
+import com.cascadebot.cascadebot.utils.pagination.PageObjects;
 import com.cascadebot.shared.SecurityLevel;
 import net.dv8tion.jda.core.entities.Member;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class DumpCommand implements ICommandRestricted {
@@ -26,18 +31,28 @@ public class DumpCommand implements ICommandRestricted {
             return;
         }
         if (context.getArg(0).equalsIgnoreCase("threads")) {
-            String threads = Thread.getAllStackTraces().keySet().stream().map(Thread::getName).sorted().collect(Collectors.joining("\n"));
+            String threads = "```\n" + Thread.getAllStackTraces().keySet().stream().map(Thread::getName).sorted().collect(Collectors.joining("\n")) + "```";
             PasteUtils.pasteIfLong(threads, 2048, context::reply);
         } else if (context.getArg(0).equalsIgnoreCase("commands")) {
-            StringBuilder builder = new StringBuilder();
+            List<String> header = List.of("Command", "Module", "Permission", "Subcommands");
+            List<List<String>> body = new ArrayList<>();
             for (ICommandMain command : CascadeBot.INS.getCommandManager().getCommands()) {
-                builder.append(command.command()).append(" `").append(command.getModule().toString().toLowerCase()).append("`");
-                for (ICommandExecutable subcommand : command.getSubCommands()) {
-                    builder.append("\n - ").append(subcommand.command());
-                }
-                builder.append("\n");
+                body.add(List.of(
+                        command.command(),
+                        command.getModule().toString(),
+                        command.getPermission() == null ? "No permission" : command.getPermission().toString(),
+                        command.getSubCommands().stream().map(ICommandExecutable::command).collect(Collectors.toSet()).toString()));
             }
-            PasteUtils.pasteIfLong(builder.toString(), 2048, context::reply);
+            PasteUtils.pasteIfLong(FormatUtils.makeAsciiTable(header, body, null), 2048, context::reply);
+        } else if (context.getArg(0).equalsIgnoreCase("permissions")) {
+            List<String> header = List.of("Permission", "Discord Perms");
+            List<List<String>> body = new ArrayList<>();
+            for (CascadePermission permission : CascadeBot.INS.getPermissionsManager().getPermissions()) {
+                body.add(List.of(permission.getPermissionNode(), permission.getDiscordPerm().toString()));
+            }
+            PasteUtils.pasteIfLong(FormatUtils.makeAsciiTable(header, body, null), 2048, context::reply);
+        } else {
+            context.replyDanger("I can't seem to find that argument \uD83E\uDD14" /* Thinking emoji 🤔 */);
         }
     }
 
