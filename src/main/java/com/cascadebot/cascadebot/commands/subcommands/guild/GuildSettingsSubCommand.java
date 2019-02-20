b721551008f5277f98c9fa32bea50ecbd92a4efe
William Oldham <wegg7250@gmail.com>
2019-02-20 16:14:48 +0000

    Neatness changes :P

--- src/main/java/com/cascadebot/cascadebot/commands/subcommands/guild/GuildSettingsSubCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/subcommands/guild/GuildSettingsSubCommand.java
@@ -16,41 +16,41 @@ import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Set;
-import java.util.stream.Collectors;
 
 public class GuildSettingsSubCommand implements ICommandExecutable {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        for (Field field : GuildData.class.getDeclaredFields()) {
-            if (field.getName().equalsIgnoreCase(context.getArg(0))) {
-                if (field.getType() == boolean.class) {
-                    field.setAccessible(true);
-                    try {
-                        field.setBoolean(context.getData(), Boolean.valueOf(context.getArg(1)));
-                        context.replySuccess("Set field %s with a value of %s", field.getName(), context.getArg(1));
-                    } catch (IllegalAccessException e) {
-                        context.replyException("Could not access that setting!", e);
-                    }
-                    return;
-                }
+        Field field = Arrays.stream(GuildData.class.getDeclaredFields())
+                .filter(f -> f.getName().equalsIgnoreCase(context.getArg(0)))
+                .filter(f -> f.getType() == boolean.class)
+                .findFirst()
+                .orElse(null);
+
+        if (field != null) {
+            field.setAccessible(true);
+            try {
+                field.setBoolean(context.getData(), Boolean.valueOf(context.getArg(1)));
+                context.replySuccess("Set field `%s` to a value of `%s`", field.getName(), context.getArg(1));
+            } catch (IllegalAccessException e) {
+                context.replyException("Could not access that setting!", e);
             }
-        }
-        if (context.getArg(0).equalsIgnoreCase("list")) {
+        } else if (context.getArg(0).equalsIgnoreCase("list")) {
             List<String> headings = List.of("Field name", "Current value");
             List<List<String>> body = new ArrayList<>();
             Arrays.stream(GuildData.class.getDeclaredFields())
-                    .filter(field -> field.getType() == boolean.class)
-                    .forEach(field -> {
+                    .filter(f -> f.getType() == boolean.class)
+                    .forEach(f -> {
                         try {
-                            field.setAccessible(true);
-                            body.add(List.of(field.getName(), String.valueOf(field.getBoolean(context.getData()))));
+                            f.setAccessible(true);
+                            body.add(List.of(f.getName(), String.valueOf(f.getBoolean(context.getData()))));
                         } catch (IllegalAccessException e) {
                             e.printStackTrace();
                         }
                     });
             context.reply(FormatUtils.makeAsciiTable(headings, body, null));
+        } else {
+            context.replyDanger("Cannot find that field!");
         }
     }
 
