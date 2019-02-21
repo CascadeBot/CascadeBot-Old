096492376cf7ce6adff59b3ff5dccb419bdefafe
William Oldham <wegg7250@gmail.com>
2019-02-21 09:28:31 +0000

    All dis good unbanning

--- src/main/java/com/cascadebot/cascadebot/commands/moderation/UnbanCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/moderation/UnbanCommand.java
@@ -5,15 +5,22 @@
 
 package com.cascadebot.cascadebot.commands.moderation;
 
+import com.cascadebot.cascadebot.CascadeBot;
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.ICommandMain;
 import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.permissions.CascadePermission;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
 
+import java.util.regex.Pattern;
+
 public class UnbanCommand implements ICommandMain {
+
+    private Pattern userId = Pattern.compile("^[0-9]{17,}$");
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
@@ -21,14 +28,37 @@ public class UnbanCommand implements ICommandMain {
             context.replyDanger("Not enough arguments (No specified member)");
             return;
         }
-        String targetUser = context.getMessage(0);
-        try {
-            context.getGuild().getController().unban(targetUser).queue();
-            context.replyInfo("User <@" + targetUser + "> has been Unbanned!");
-        } catch (InsufficientPermissionException e) {
-            context.replyWarning("Cannot unban user <@" + targetUser + ">, missing Ban Members permission");
+        String target = context.getMessage(0);
+        if (userId.matcher(target).matches()) {
+            CascadeBot.INS.getShardManager().retrieveUserById(target).queue(user -> {
+                banUser(context, user);
+            }, failure -> {
+                context.replyDanger("Could not find user with ID %s!", target);
+            });
+        } else {
+            try {
+                context.getGuild().getBanList().queue(list -> {
+                    User bannedUser = list.stream()
+                            .map(Guild.Ban::getUser)
+                            .filter(user -> user.getName().equalsIgnoreCase(target) || user.getAsTag().equalsIgnoreCase(target))
+                            .findFirst()
+                            .orElse(null);
+                    banUser(context, bannedUser);
+                });
+            } catch (InsufficientPermissionException e) {
+                context.replyDanger("Cannot get banned user list, missing %s permission!", e.getPermission().getName());
+            }
         }
+    }
 
+    private void banUser(CommandContext context, User user) {
+        try {
+            context.getGuild().getController().unban(user).queue(success -> {
+                context.replySuccess("User %s has been unbanned!", user.getAsTag());
+            }, error -> context.replyException("Could not ban user %s!", error, user.getAsTag()));
+        } catch (InsufficientPermissionException e) {
+            context.replyDanger("Cannot unban user %s, missing %s permission!", user.getAsTag(), e.getPermission().getName());
+        }
     }
 
     @Override
