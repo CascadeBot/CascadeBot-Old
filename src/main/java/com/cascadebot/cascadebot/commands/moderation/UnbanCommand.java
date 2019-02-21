f3c604a5a5f60dbfd4f2062701b9870351d3a09e
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-02-21 15:30:25 +0000

    Maybe this fixed it

--- src/main/java/com/cascadebot/cascadebot/commands/moderation/UnbanCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/moderation/UnbanCommand.java
@@ -5,13 +5,19 @@
 
 package com.cascadebot.cascadebot.commands.moderation;
 
+import com.cascadebot.cascadebot.CascadeBot;
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.ICommandMain;
 import com.cascadebot.cascadebot.commandmeta.Module;
 import com.cascadebot.cascadebot.permissions.CascadePermission;
+import com.cascadebot.shared.Regex;
 import net.dv8tion.jda.core.Permission;
+import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.User;
 import net.dv8tion.jda.core.exceptions.InsufficientPermissionException;
+
+import java.util.regex.Pattern;
 
 public class UnbanCommand implements ICommandMain {
 
@@ -21,14 +27,38 @@ public class UnbanCommand implements ICommandMain {
             context.replyDanger("Not enough arguments (No specified member)");
             return;
         }
-        String targetUser = context.getMessage(0);
-        try {
-            context.getGuild().getController().unban(targetUser).queue();
-            context.replyInfo("User <@" + targetUser + "> has been Unbanned!");
-        } catch (InsufficientPermissionException e) {
-            context.replyWarning("Cannot unban user <@" + targetUser + ">, missing Ban Members permission");
+        String target = context.getMessage(0);
+        if (Regex.ID.matcher(target).matches()) {
+            CascadeBot.INS.getShardManager().retrieveUserById(target).queue(user -> {
+                unbanUser(context, user);
+            }, failure -> {
+                context.replyDanger("Could not find user with ID %s!", target);
+            });
+        } else {
+            try {
+                context.getGuild().getBanList().queue(list -> {
+                    User bannedUser = list.stream()
+                            .map(Guild.Ban::getUser)
+                            .filter(user -> user.getName().equalsIgnoreCase(target) || user.getAsTag().equalsIgnoreCase(target))
+                            .findFirst()
+                            .orElse(null);
+                    unbanUser(context, bannedUser);
+                });
+            } catch (InsufficientPermissionException e) {
+                context.replyDanger("Cannot get banned user list, missing %s permission!", e.getPermission().getName());
+            }
         }
+    }
 
+    // TODO: Make a moderation handler that does the action, handling perms etc.
+    private void unbanUser(CommandContext context, User user) {
+        try {
+            context.getGuild().getController().unban(user).queue(success -> {
+                context.replySuccess("User %s has been unbanned!", user.getAsTag());
+            }, error -> context.replyException("Could not ban user %s!", error, user.getAsTag()));
+        } catch (InsufficientPermissionException e) {
+            context.replyDanger("Cannot unban user %s, missing %s permission!", user.getAsTag(), e.getPermission().getName());
+        }
     }
 
     @Override
