bd36a04dbf5085cab52fd489874fbc1354b8992f
James Hawkins <jhawki2005@gmail.com>
2019-02-24 12:36:05 +0000

    yaya

--- src/main/java/com/cascadebot/cascadebot/commands/moderation/BanCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/moderation/BanCommand.java
@@ -31,6 +31,8 @@ public class BanCommand implements ICommandMain {
         try {
             context.getGuild().getController().ban(targetMember.getUser(), 7).queue(success -> {
                 context.replyInfo("**%s** has been banned!", targetMember.getUser().getAsTag());
+            }, throwable -> {
+                context.replyException("Could not ban the user %s!", throwable, targetMember.getUser().getAsTag());
             });
 
         }  catch (InsufficientPermissionException e) {
