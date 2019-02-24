dcca995b8ac99c077e6cac8be9114598ae32b01b
William Oldham <wegg7250@gmail.com>
2019-02-24 17:23:31 +0000

    Actually save :eyes:

--- src/main/java/com/cascadebot/cascadebot/commands/subcommands/guild/GuildSaveSubCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/subcommands/guild/GuildSaveSubCommand.java
@@ -8,6 +8,8 @@ package com.cascadebot.cascadebot.commands.subcommands.guild;
 import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
 import com.cascadebot.cascadebot.data.mapping.GuildDataMapper;
+import com.cascadebot.cascadebot.data.objects.GuildData;
+import com.cascadebot.cascadebot.events.GuildSaveListener;
 import com.cascadebot.cascadebot.permissions.CascadePermission;
 import net.dv8tion.jda.core.entities.Member;
 
@@ -16,13 +18,20 @@ public class GuildSaveSubCommand implements ICommandExecutable {
     @Override
     public void onCommand(Member sender, CommandContext context) {
         if (context.getArgs().length == 0) {
-            GuildDataMapper.getGuilds().invalidate(context.getGuild().getIdLong());
+            GuildDataMapper.replace(context.getGuild().getIdLong(), context.getData());
             context.replySuccess("Saved **this guild's** information successfully!");
         } else if (context.getArg(0).equals("all")) {
-            GuildDataMapper.getGuilds().invalidateAll();
+            for (var entry : GuildDataMapper.getGuilds().asMap().entrySet()) {
+                GuildDataMapper.replace(entry.getKey(), entry.getValue());
+            }
             context.replySuccess("Saved **all** guild information successfully!");
         } else {
-            GuildDataMapper.getGuilds().invalidate(context.getArg(0));
+            GuildData guildData = GuildDataMapper.getGuilds().asMap().get(Long.parseLong(context.getArg(0)));
+            if (guildData == null) {
+                context.replyDanger("Cannot find guild to save!");
+                return;
+            }
+            GuildDataMapper.replace(guildData.getGuildID(), guildData);
             context.replySuccess("Saved guild information for guild **" + context.getArg(0) + "**!");
         }
     }
