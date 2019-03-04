6b1bbe5b56709060b5a195fafce42e47f79aa76d
William Oldham <wegg7250@gmail.com>
2019-03-04 07:56:25 +0000

    Add uniformity with sendSuccess

--- src/main/java/com/cascadebot/cascadebot/moderation/ModerationManager.java
+++ src/main/java/com/cascadebot/cascadebot/moderation/ModerationManager.java
@@ -20,22 +20,14 @@ public class ModerationManager {
         context.replyException("Could not %s the user %s due to an exception!", throwable, action, target.getAsTag());
     });
 
+    // This is keeping the mod-action parameter as it is used for force-ban, soft-ban and normal ban.
     public void ban(CommandContext context, ModAction action, User target, Member submitter, String reason, int messagesToDelete) {
         if (runChecks(action, target, submitter, context)) {
             runWithCheckedExceptions(() -> {
                 context.getGuild().getController()
                         .ban(target, messagesToDelete, reason)
                         .queue(success -> {
-                            EmbedBuilder builder = MessagingObjects.getStandardMessageEmbed(String.format(
-                                    "%s has been %s!", target.getAsTag(), action.getVerb()
-                            ), submitter.getUser());
-
-                            if (!StringUtils.isBlank(reason)) {
-                                builder.addField("Reason", reason, false);
-                            }
-
-                            builder.setTitle(StringUtils.capitalize(action.getVerb()) + " user");
-                            context.replySuccess(builder);
+                            sendSuccess(context, target, submitter, action, reason);
                         }, throwable -> FAILURE_CONSUMER.accept(context, throwable, target, action));
             }, context, action, target);
         }
@@ -45,7 +37,7 @@ public class ModerationManager {
         if (runChecks(ModAction.UNBAN, target, submitter, context)) {
             runWithCheckedExceptions(() -> {
                 context.getGuild().getController().unban(target).reason(reason).queue(success -> {
-                    context.replySuccess("User %s has been unbanned!", target.getAsTag());
+                    sendSuccess(context, target, submitter, ModAction.UNBAN, reason);
                 }, throwable -> FAILURE_CONSUMER.accept(context, throwable, target, ModAction.UNBAN));
             }, context, ModAction.UNBAN, target);
         }
@@ -69,7 +61,7 @@ public class ModerationManager {
                 context.getGuild().getController()
                         .kick(target, reason)
                         .queue(success -> {
-                            context.replySuccess("Use %s has been kicked!", target.getUser().getAsTag());
+                            sendSuccess(context, target.getUser(), submitter, ModAction.KICK, reason);
                         }, throwable -> FAILURE_CONSUMER.accept(context, throwable, target.getUser(), ModAction.KICK));
             }, context, ModAction.KICK, target.getUser());
         }
@@ -97,6 +89,19 @@ public class ModerationManager {
         } catch (HierarchyException e) {
             context.replyDanger("Cannot %s user %s the top role they have is higher than mine!", action, target.getAsTag());
         }
+    }
+
+    private void sendSuccess(CommandContext context, User target, Member submitter, ModAction action, String reason) {
+        EmbedBuilder builder = MessagingObjects.getStandardMessageEmbed(String.format(
+                "%s has been %s!", target.getAsTag(), action.getVerb()
+        ), submitter.getUser());
+
+        if (!StringUtils.isBlank(reason)) {
+            builder.addField("Reason", reason, false);
+        }
+
+        builder.setTitle(StringUtils.capitalize(action.getVerb()) + " user");
+        context.replySuccess(builder);
     }
 
     @FunctionalInterface
