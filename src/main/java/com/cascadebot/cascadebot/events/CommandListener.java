cdf22a0bf091944c206f0d804a136b48c728eb51
William Oldham <wegg7250@gmail.com>
2019-03-05 21:39:46 +0000

    Allow "argument with spaces" to be seen as one string rather than 3

--- src/main/java/com/cascadebot/cascadebot/events/CommandListener.java
+++ src/main/java/com/cascadebot/cascadebot/events/CommandListener.java
@@ -95,6 +95,41 @@ public class CommandListener extends ListenerAdapter {
         }
     }
 
+    private String[] splitArgs(String input) {
+        boolean inQuotes = false; // Whether the current position is surrounded by quotes or not
+        int splitFrom = 0; // We initially start the first split from 0 to the first space
+        var args = new ArrayList<String>();
+        for (int pos = 0; pos < input.length(); pos++) {
+            char charAtPos = input.charAt(pos);
+            if (charAtPos == ' ') {
+                int splitTo = pos;
+                // If there is a quote to close this
+                if (inQuotes && (input.substring(pos).contains("\""))) {
+                    continue;
+                }
+                if (input.charAt(pos - 1) == '"') {
+                    splitTo = pos - 1; // If we are splitting after a quote, don't include the quote in the split
+                }
+                args.add(input.substring(splitFrom, splitTo));
+                splitFrom = pos + 1; // Set the next split start to be after
+            } else if (pos == input.length() - 1) {
+                int splitTo = input.length();
+                // If the end character is a quote, we want to "split" before the quote to no include it.
+                if (input.charAt(pos) == '"') {
+                    splitTo = pos;
+                }
+                args.add(input.substring(splitFrom, splitTo));
+                // End of string so do nothing else
+            } else if (charAtPos == '"') {
+                if (!inQuotes) {
+                    splitFrom += 1; // Start the split after the first quote
+                }
+                inQuotes = !inQuotes;
+            }
+        }
+        return args.toArray(String[]::new);
+    }
+
     private void processCommands(GuildMessageReceivedEvent event, GuildData guildData, String trigger, String[] args, boolean isMention) {
         ICommandMain cmd = CascadeBot.INS.getCommandManager().getCommand(trigger, event.getAuthor(), guildData);
         if (cmd != null) {
