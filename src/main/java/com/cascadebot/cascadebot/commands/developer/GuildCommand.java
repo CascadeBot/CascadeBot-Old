86d664c24fc61a441c02db2e9668de701e000cc8
William Oldham <wegg7250@gmail.com>
2019-03-21 21:44:43 +0000

    Remove flag toggling from guild command

--- src/main/java/com/cascadebot/cascadebot/commands/developer/GuildCommand.java
+++ src/main/java/com/cascadebot/cascadebot/commands/developer/GuildCommand.java
@@ -12,6 +12,7 @@ import com.cascadebot.cascadebot.commandmeta.CommandContext;
 import com.cascadebot.cascadebot.commandmeta.ICommandExecutable;
 import com.cascadebot.cascadebot.commandmeta.ICommandRestricted;
 import com.cascadebot.cascadebot.commandmeta.Module;
+import com.cascadebot.cascadebot.commands.subcommands.guild.GuildFlagSubCommand;
 import com.cascadebot.cascadebot.commands.subcommands.guild.GuildLeaveSubCommand;
 import com.cascadebot.cascadebot.commands.subcommands.guild.GuildSaveSubCommand;
 import com.cascadebot.cascadebot.data.mapping.GuildDataMapper;
@@ -32,42 +33,6 @@ public class GuildCommand implements ICommandRestricted {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        if (context.getArgs().length > 0) {
-            Flag flag;
-            if (!EnumUtils.isValidEnumIgnoreCase(Flag.class, context.getArg(0))) {
-                context.replyDanger("Invalid flag! Possible values: %s", Arrays.toString(Flag.values()));
-                return;
-            } else {
-                flag = EnumUtils.getEnum(Flag.class, context.getArg(0).toUpperCase());
-            }
-
-            Guild guild = context.getGuild();
-            if (context.getArgs().length == 2) {
-                if (Regex.ID.matcher(context.getArg(1)).matches()) {
-                    guild = CascadeBot.INS.getShardManager().getGuildById(context.getArg(1));
-                    if (guild == null) {
-                        context.replyDanger("Cannot find that guild!");
-                        return;
-                    }
-                } else {
-                    context.replyDanger("Invalid guild ID!");
-                    return;
-                }
-            }
-
-            GuildData guildData = GuildDataMapper.getGuildData(guild.getIdLong());
-
-            if (guildData.isFlagEnabled(flag)) {
-                guildData.disableFlag(flag);
-                context.replySuccess("Disabled flag `%s` for guild `%s (%s)`", flag.name(), guild.getName(), guild.getId());
-                return;
-            } else {
-                guildData.enableFlag(flag);
-                context.replySuccess("Enabled flag `%s` for guild `%s (%s)`", flag.name(), guild.getName(), guild.getId());
-                return;
-            }
-
-        }
         context.replyUsage(this);
     }
 
@@ -83,16 +48,7 @@ public class GuildCommand implements ICommandRestricted {
 
     @Override
     public Set<ICommandExecutable> getSubCommands() {
-        return Set.of(new GuildSaveSubCommand(), new GuildLeaveSubCommand());
-    }
-
-    @Override
-    public Set<Argument> getUndefinedArguments() {
-        return Set.of(Argument.of("flag", "", ArgumentType.OPTIONAL,
-                Set.of(
-                        Argument.of("guild_id", "Toggles a flag for this guild [or a different guild].", ArgumentType.OPTIONAL)
-                )
-        ));
+        return Set.of(new GuildSaveSubCommand(), new GuildLeaveSubCommand(), new GuildFlagSubCommand());
     }
 
     @Override
