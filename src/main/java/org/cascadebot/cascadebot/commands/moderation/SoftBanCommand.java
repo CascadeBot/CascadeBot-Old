8a437d1ae24f1b2b34acc58925de56a020d6ac2e
William Oldham <wegg7250@gmail.com>
2019-03-25 21:05:50 +0000

    Update src/main/java/org/cascadebot/cascadebot/commands/moderation/SoftBanCommand.java
    Co-Authored-By: JamesHawkinss <jhawki2005@gmail.com>

--- src/main/java/org/cascadebot/cascadebot/commands/moderation/SoftBanCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/moderation/SoftBanCommand.java
@@ -73,7 +73,7 @@ public class SoftBanCommand implements ICommandMain {
 
     @Override
     public String description() {
-        return "Ban and then unban (to remove messages) of a specified user";
+        return "Bans and then unbans a specified user to remove messages";
     }
 
 
