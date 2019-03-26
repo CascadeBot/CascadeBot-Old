2919f401c0837ae5be7abe3b12ad9ee3ef0f28a0
William Oldham <wegg7250@gmail.com>
2019-03-26 17:49:44 +0000

    Update src/main/java/org/cascadebot/cascadebot/commands/developer/GuildCommand.java
    Co-Authored-By: JamesHawkinss <jhawki2005@gmail.com>

--- src/main/java/org/cascadebot/cascadebot/commands/developer/GuildCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/developer/GuildCommand.java
@@ -46,7 +46,7 @@ public class GuildCommand implements ICommandRestricted {
 
     @Override
     public String description() {
-        return "Interact with guilds";
+        return "Allows administrative actions to be run on guilds";
     }
 
 }
