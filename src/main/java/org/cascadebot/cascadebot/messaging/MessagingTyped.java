fe53b39855465a9127d9a8ae55e3df86fae3cefb
weeryan17 <theweeryan17@gmail.com>
2019-03-28 12:09:57 -0500

    Documentation

--- src/main/java/org/cascadebot/cascadebot/messaging/MessagingTyped.java
+++ src/main/java/org/cascadebot/cascadebot/messaging/MessagingTyped.java
@@ -5,6 +5,7 @@
 
 package org.cascadebot.cascadebot.messaging;
 
+import net.dv8tion.jda.core.entities.MessageEmbed;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.CommandException;
 import net.dv8tion.jda.core.EmbedBuilder;
@@ -18,80 +19,219 @@ public class MessagingTyped {
         this.context = context;
     }
 
+    /**
+     * Replies to the channel in the command context as an info message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#INFO},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#INFO} at the top of the message
+     *
+     * @param message The String message to send.
+     */
     public void replyInfo(String message) {
         Checks.notBlank(message, "message");
         Messaging.sendInfoMessage(context.getChannel(), MessagingObjects.getStandardMessageEmbed(message, context.getUser()), context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as an info message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#INFO},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#INFO} at the top of the message
+     *
+     * @param message The String message to send.
+     * @param objects The objects to use with the formatting.
+     */
     public void replyInfo(String message, Object... objects) {
         replyInfo(String.format(message, objects));
     }
 
+    /**
+     * Replies to the channel in the command context as an info message using the passed in {@link EmbedBuilder}.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#INFO}
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#INFO} in the title and the embed formatted using {@link org.cascadebot.cascadebot.utils.FormatUtils#formatEmbed(MessageEmbed)}
+     *
+     * @param builder The embed builder to use as the message.
+     */
     public void replyInfo(EmbedBuilder builder) {
         Checks.notNull(builder, "build");
         Messaging.sendInfoMessage(context.getChannel(), builder, context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a warning message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#WARNING},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#WARNING} at the top of the message
+     *
+     * @param message The String message to send.
+     */
     public void replyWarning(String message) {
         Checks.notBlank(message, "message");
         Messaging.sendWarningMessage(context.getChannel(), MessagingObjects.getStandardMessageEmbed(message, context.getUser()), context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a warning message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#INFO},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#INFO} at the top of the message
+     *
+     * @param message The String message to send.
+     * @param objects The objects to use with the formatting.
+     */
     public void replyWarning(String message, Object... objects) {
         replyWarning(String.format(message, objects));
     }
 
+    /**
+     * Replies to the channel in the command context as a warning message using the passed in {@link EmbedBuilder}.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#INFO}
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#INFO} in the title and the embed formatted using {@link org.cascadebot.cascadebot.utils.FormatUtils#formatEmbed(MessageEmbed)}
+     *
+     * @param builder The embed builder to use as the message.
+     */
     public void replyWarning(EmbedBuilder builder) {
         Checks.notNull(builder, "build");
         Messaging.sendWarningMessage(context.getChannel(), builder, context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a success message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#WARNING},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#WARNING} at the top of the message
+     *
+     * @param message The String message to send.
+     */
     public void replySuccess(String message) {
         Checks.notBlank(message, "message");
         Messaging.sendSuccessMessage(context.getChannel(), MessagingObjects.getStandardMessageEmbed(message, context.getUser()), context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a success message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#WARNING},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#WARNING} at the top of the message
+     *
+     * @param message The String message to send.
+     * @param objects The objects to use with the formatting.
+     */
     public void replySuccess(String message, Object... objects) {
         replySuccess(String.format(message, objects));
     }
 
+    /**
+     * Replies to the channel in the command context as a success message using the passed in {@link EmbedBuilder}.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#WARNING}
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#WARNING} in the title and the embed formatted using {@link org.cascadebot.cascadebot.utils.FormatUtils#formatEmbed(MessageEmbed)}
+     *
+     * @param builder The embed builder to use as the message.
+     */
     public void replySuccess(EmbedBuilder builder) {
         Checks.notNull(builder, "build");
         Messaging.sendSuccessMessage(context.getChannel(), builder, context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a moderation message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#MODERATION,
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#MODERATION} at the top of the message
+     *
+     * @param message The String message to send.
+     */
     public void replyModeration(String message) {
         Checks.notBlank(message, "message");
         Messaging.sendModerationMessage(context.getChannel(), MessagingObjects.getStandardMessageEmbed(message, context.getUser()), context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a moderation message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#MODERATION},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#MODERATION} at the top of the message
+     *
+     * @param message The String message to send.
+     * @param objects The objects to use with the formatting.
+     */
     public void replyModeration(String message, Object... objects) {
         replyModeration(String.format(message, objects));
     }
 
+    /**
+     * Replies to the channel in the command context as a moderation message using the passed in {@link EmbedBuilder}.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#MODERATION}
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#MODERATION} in the title and the embed formatted using {@link org.cascadebot.cascadebot.utils.FormatUtils#formatEmbed(MessageEmbed)}
+     *
+     * @param builder The embed builder to use as the message.
+     */
     public void replyModeration(EmbedBuilder builder) {
         Checks.notNull(builder, "build");
         Messaging.sendModerationMessage(context.getChannel(), builder, context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a danger message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#DANGER,
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#DANGER} at the top of the message
+     *
+     * @param message The String message to send.
+     */
     public void replyDanger(String message) {
         Checks.notBlank(message, "message");
         Messaging.sendDangerMessage(context.getChannel(), MessagingObjects.getStandardMessageEmbed(message, context.getUser()), context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a danger message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#DANGER},
+     *     and with the message being in the description of the embed
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#DANGER} at the top of the message
+     *
+     * @param message The String message to send.
+     * @param objects The objects to use with the formatting.
+     */
     public void replyDanger(String message, Object... objects) {
         replyDanger(String.format(message, objects));
     }
 
+    /**
+     * Replies to the channel in the command context as a danger message using the passed in {@link EmbedBuilder}.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#DANGER}
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#DANGER} in the title and the embed formatted using {@link org.cascadebot.cascadebot.utils.FormatUtils#formatEmbed(MessageEmbed)}
+     *
+     * @param builder The embed builder to use as the message.
+     */
     public void replyDanger(EmbedBuilder builder) {
         Checks.notNull(builder, "build");
         Messaging.sendDangerMessage(context.getChannel(), builder, context.getSettings().useEmbedForMessages());
     }
 
+    /**
+     * Replies to the channel in the command context as a error message.
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#DANGER,
+     *     and with the message being in the description of the embed, and the stack trance as a link below it.
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#DANGER} at the top of the message, and the stack trance as a link below it.
+     *
+     * @param message   The String message to send.
+     * @param throwable The Exception that your sending.
+     */
     public void replyException(String message, Throwable throwable) {
         Messaging.sendExceptionMessage(context.getChannel(), message, new CommandException(throwable, context.getGuild(), context.getTrigger()));
     }
 
+    /**
+     * Replies to the channel in the command context as a danger message. After formatting the string using javas {@link String#format(String, Object...)}
+     * When embeds for guild are own it replies with an embed with the bar color being the color defined in {@link MessageType#DANGER},
+     *     and with the message being in the description of the embed, and the stack trance as a link below it.
+     * If embeds are turned off it replies with a normal message with the unicode character defined in {@link MessageType#DANGER} at the top of the message, and the stack trance as a link below it.
+     *
+     * @param message   The String message to send.
+     * @param throwable The Exception that your sending
+     * @param objects   The objects to use with the formatting.
+     */
     public void replyException(String message, Throwable throwable, Object... objects) {
         Messaging.sendExceptionMessage(context.getChannel(), String.format(message, objects), new CommandException(throwable, context.getGuild(), context.getTrigger()));
     }
--- src/main/java/org/cascadebot/cascadebot/messaging/MessagingUI.java
+++ src/main/java/org/cascadebot/cascadebot/messaging/MessagingUI.java
@@ -14,6 +14,7 @@ import net.dv8tion.jda.core.entities.Message;
 import net.dv8tion.jda.core.entities.MessageEmbed;
 import net.dv8tion.jda.core.requests.RequestFuture;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
@@ -26,6 +27,14 @@ public class MessagingUI {
         this.context = context;
     }
 
+    /**
+     * Sends an image to the channel in the context
+     * When embeds are on it embeds the image into the embed
+     * When embeds are off it downloads the image and sends it via the {@link net.dv8tion.jda.core.entities.TextChannel#sendFile(File)} method
+     *
+     * @param url The url of the image.
+     * @return A {@link RequestFuture<Message>} so you can interact with the message after it sends.
+     */
     public RequestFuture<Message> replyImage(String url) {
         if (context.getSettings().useEmbedForMessages()) {
             EmbedBuilder embedBuilder = MessagingObjects.getClearThreadLocalEmbedBuilder();
@@ -41,18 +50,51 @@ public class MessagingUI {
         }
     }
 
+    /**
+     * Sends a message with reactions to function as "buttons".
+     *
+     * @see ButtonGroup
+     *
+     * @param message The String message to send.
+     * @param group   The {@link ButtonGroup} to use for buttons.
+     */
     public void sendButtonedMessage(String message, ButtonGroup group) {
         Messaging.sendButtonedMessage(context.getChannel(), message, group);
     }
 
+    /**
+     * Sends a message with reactions to function as "buttons".
+     *
+     * @see ButtonGroup
+     *
+     * @param embed The {@link MessageEmbed} to use as the message.
+     * @param group The {@link ButtonGroup} to use for buttons.
+     */
     public void sendButtonedMessage(MessageEmbed embed, ButtonGroup group) {
         Messaging.sendButtonedMessage(context.getChannel(), embed, group);
     }
 
+    /**
+     * Sends a message with reactions to function as "buttons".
+     *
+     * @see ButtonGroup
+     *
+     * @param message The message to send.
+     * @param group   The {@link ButtonGroup} to use for buttons.
+     */
     public void sendButtonedMessage(Message message, ButtonGroup group) {
         Messaging.sendButtonedMessage(context.getChannel(), message, group);
     }
 
+    /**
+     * Sends a pages message with buttons for page navigation.
+     *
+     * @see org.cascadebot.cascadebot.utils.pagination.PageObjects.EmbedPage
+     * @see org.cascadebot.cascadebot.utils.pagination.PageObjects.StringPage
+     * @see org.cascadebot.cascadebot.utils.pagination.PageObjects.TablePage
+     *
+     * @param pages The list of pages to use. see {@link org.cascadebot.cascadebot.utils.pagination.PageObjects.EmbedPage}, {@link org.cascadebot.cascadebot.utils.pagination.PageObjects.StringPage}, and {@link org.cascadebot.cascadebot.utils.pagination.PageObjects.TablePage}.
+     */
     public void sendPagedMessage(List<Page> pages) {
         Messaging.sendPagedMessage(context.getChannel(), context.getMember(), pages);
     }
