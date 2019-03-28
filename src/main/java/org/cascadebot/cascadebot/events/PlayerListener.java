b2b8936bbe5264c2a2ab957f6fa5502acad494db
William Oldham <wegg7250@gmail.com>
2019-03-28 16:33:54 +0000

    Move playerlistener into event package

--- src/main/java/org/cascadebot/cascadebot/events/PlayerListener.java
+++ src/main/java/org/cascadebot/cascadebot/events/PlayerListener.java
@@ -3,16 +3,17 @@
  * Licensed under the MIT license.
  */
 
-package org.cascadebot.cascadebot.music;
+package org.cascadebot.cascadebot.events;
 
 import com.sedmelluq.discord.lavaplayer.track.AudioTrack;
 import lavalink.client.player.event.IPlayerEventListener;
 import lavalink.client.player.event.PlayerEvent;
 import lavalink.client.player.event.TrackEndEvent;
+import org.cascadebot.cascadebot.music.CascadePlayer;
 
 public class PlayerListener implements IPlayerEventListener {
 
-    CascadePlayer player;
+    private CascadePlayer player;
 
     public PlayerListener(CascadePlayer player) {
         this.player = player;
@@ -22,14 +23,14 @@ public class PlayerListener implements IPlayerEventListener {
     public void onEvent(PlayerEvent playerEvent) {
         if (playerEvent instanceof TrackEndEvent) {
             try {
-                if(player.loop.equals(CascadePlayer.LoopType.DISABLED) || player.loop.equals(CascadePlayer.LoopType.PLAYLIST)) {
-                    if (player.loop.equals(CascadePlayer.LoopType.PLAYLIST)) {
+                if(player.getLoopMode().equals(CascadePlayer.LoopMode.DISABLED) || player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
+                    if (player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
                         TrackEndEvent endEvent = (TrackEndEvent) playerEvent;
                         player.getTracks().add(endEvent.getTrack());
                     }
                     AudioTrack audioTrack = player.getTracks().remove();
                     player.getPlayer().playTrack(audioTrack);
-                } else if (player.loop.equals(CascadePlayer.LoopType.SONG)) {
+                } else if (player.getLoopMode().equals(CascadePlayer.LoopMode.SONG)) {
                     TrackEndEvent endEvent = (TrackEndEvent) playerEvent;
                     player.getPlayer().playTrack(endEvent.getTrack());
                 }
