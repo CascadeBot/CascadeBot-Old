69bbee45383dfb334b6839d509e0b09f315ac652
William Oldham <wegg7250@gmail.com>
2019-03-29 08:01:58 +0000

    Abstract into own method

--- src/main/java/org/cascadebot/cascadebot/events/PlayerListener.java
+++ src/main/java/org/cascadebot/cascadebot/events/PlayerListener.java
@@ -19,69 +19,51 @@ public class PlayerListener implements IPlayerEventListener, AudioEventListener 
 
     private CascadePlayer player;
 
+    private int songPlayCount = 0;
+
     public PlayerListener(CascadePlayer player) {
         this.player = player;
     }
 
-    private int songPlayCount = 0;
-
     @Override
     public void onEvent(PlayerEvent playerEvent) {
         if (playerEvent instanceof TrackEndEvent) {
-            TrackEndEvent endEvent = (TrackEndEvent) playerEvent;
-            try {
-                if (player.getLoopMode().equals(CascadePlayer.LoopMode.DISABLED) || player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
-                    if (player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
-                        // Add the track to the end of the queue to be repeated
-                        player.getTracks().add(endEvent.getTrack());
-                        if (player.isShuffleEnabled()) {
-                            if (++songPlayCount % player.getTracks().size() == 0) {
-                                player.shuffle(); //Shuffle when the tracks start over.
-                            }
-                        }
-                    }
-                    // Take the next track in the queue, remove it from the queue and play it
-                    AudioTrack audioTrack = player.getTracks().remove();
-                    player.getPlayer().playTrack(audioTrack);
-                } else if (player.getLoopMode().equals(CascadePlayer.LoopMode.SONG)) {
-                    // Take the song that just finished and repeat it
-                    player.getPlayer().playTrack(endEvent.getTrack());
-                }
-            } catch (NoSuchElementException e) {
-                // No more songs left in the queue
-                songPlayCount = 0;
-                // TODO: Anything more to this?
-            }
+            onEnd(((TrackEndEvent) playerEvent).getTrack());
         }
     }
 
     @Override
     public void onEvent(AudioEvent audioEvent) {
-        if(audioEvent instanceof com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent) {
-            com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent endEvent = (com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent) audioEvent;
-            try {
-                if (player.getLoopMode().equals(CascadePlayer.LoopMode.DISABLED) || player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
-                    if (player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
-                        // Add the track to the end of the queue to be repeated
-                        player.getTracks().add(endEvent.track);
-                        if (player.isShuffleEnabled()) {
-                            if (++songPlayCount % player.getTracks().size() == 0) {
-                                player.shuffle(); //Shuffle when the tracks start over.
-                            }
-                        }
-                    }
-                    // Take the next track in the queue, remove it from the queue and play it
-                    AudioTrack audioTrack = player.getTracks().remove();
-                    player.getPlayer().playTrack(audioTrack);
-                } else if (player.getLoopMode().equals(CascadePlayer.LoopMode.SONG)) {
-                    // Take the song that just finished and repeat it
-                    player.getPlayer().playTrack(endEvent.track);
-                }
-            } catch (NoSuchElementException e) {
-                // No more songs left in the queue
-                songPlayCount = 0;
-                // TODO: Anything more to this?
-            }
+        if (audioEvent instanceof com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent) {
+            onEnd(((com.sedmelluq.discord.lavaplayer.player.event.TrackEndEvent) audioEvent).track);
         }
     }
+
+    private void onEnd(AudioTrack track) {
+        songPlayCount++;
+        try {
+            if (player.getLoopMode().equals(CascadePlayer.LoopMode.DISABLED) || player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
+                if (player.getLoopMode().equals(CascadePlayer.LoopMode.PLAYLIST)) {
+                    // Add the track to the end of the queue to be repeated
+                    player.getTracks().add(track);
+                    if (player.isShuffleEnabled()) {
+                        if (songPlayCount % player.getTracks().size() == 0) {
+                            player.shuffle(); //Shuffle when the tracks start over.
+                        }
+                    }
+                }
+                // Take the next track in the queue, remove it from the queue and play it
+                AudioTrack audioTrack = player.getTracks().remove();
+                player.getPlayer().playTrack(audioTrack);
+            } else if (player.getLoopMode().equals(CascadePlayer.LoopMode.SONG)) {
+                // Take the song that just finished and repeat it
+                player.getPlayer().playTrack(track);
+            }
+        } catch (NoSuchElementException e) {
+            // No more songs left in the queue
+            songPlayCount = 0;
+            // TODO: Anything more to this?
+        }
+    }
+
 }
