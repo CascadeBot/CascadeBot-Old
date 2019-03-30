8da091ad36841a44727099887b5ae8cc474b0014
William Oldham <wegg7250@gmail.com>
2019-03-30 12:32:30 +0000

    Add join command

---
+++ src/main/java/org/cascadebot/cascadebot/commands/music/JoinCommand.java
@@ -0,0 +1,61 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.commands.music;
+
+import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.VoiceChannel;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandMain;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+import java.util.Set;
+
+public class JoinCommand implements ICommandMain {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        VoiceChannel voiceChannel = sender.getVoiceState().getChannel();
+        if (voiceChannel == null) {
+            context.replyDanger("You are not connected to a voice channel!");
+            return;
+        }
+
+        if (context.getData().getMusicPlayer().getConnectedChannel() != null) {
+            if (context.getData().getMusicPlayer().getConnectedChannel().equals(voiceChannel)) {
+                context.replyWarning("I am already connected to your channel!");
+                return;
+            }
+            
+        }
+    }
+
+    @Override
+    public Module getModule() {
+        return Module.MUSIC;
+    }
+
+    @Override
+    public Set<String> getGlobalAliases() {
+        return Set.of("summon");
+    }
+
+    @Override
+    public String command() {
+        return "join";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Join command", "join", true);
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+}
