84c9e19e7473506124d7e5fb9fd580450636745b
William Oldham <wegg7250@gmail.com>
2019-03-30 20:18:33 +0000

    Add ability to join other channels

--- src/main/java/org/cascadebot/cascadebot/commands/music/JoinCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/music/JoinCommand.java
@@ -5,6 +5,7 @@
 
 package org.cascadebot.cascadebot.commands.music;
 
+import com.jagrosh.jdautilities.commons.utils.FinderUtil;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Member;
 import net.dv8tion.jda.core.entities.VoiceChannel;
@@ -12,15 +13,31 @@ import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
+import org.cascadebot.shared.Regex;
 
+import java.util.List;
 import java.util.Set;
+import java.util.regex.Matcher;
 
 public class JoinCommand implements ICommandMain {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
         VoiceChannel voiceChannel = sender.getVoiceState().getChannel();
-        if (voiceChannel == null) {
+
+        if (context.getArgs().length == 1) {
+            Matcher matcher = Regex.ID.matcher(context.getArg(0));
+            if (matcher.matches()) {
+                voiceChannel = context.getGuild().getVoiceChannelById(matcher.group(0));
+            } else {
+                List<VoiceChannel> voiceChannels = FinderUtil.findVoiceChannels(context.getArg(0), context.getGuild());
+                voiceChannel = voiceChannels.isEmpty() ? null : voiceChannels.get(0);
+            }
+            if (voiceChannel == null) {
+                context.getTypedMessaging().replyDanger("Could not find that voice channel to connect to!");
+                return;
+            }
+        } else if (voiceChannel == null) {
             context.getTypedMessaging().replyDanger("You are not connected to a voice channel!");
             return;
         }
@@ -43,7 +60,6 @@ public class JoinCommand implements ICommandMain {
                     return;
                 }
             }
-            return;
         }
         context.getData().getMusicPlayer().join(voiceChannel);
         context.getTypedMessaging().replySuccess("I have successfully joined the voice channel `#%s`", voiceChannel.getName());
