bf12347de8df34b011aab5ad18a473b525c0477f
James Hawkins <jhawki2005@gmail.com>
2019-04-01 20:49:09 +0100

    ShuffleCommand

---
+++ src/main/java/org/cascadebot/cascadebot/commands/music/ShuffleCommand.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.commands.music;
+
+import net.dv8tion.jda.core.entities.Member;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandMain;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+public class ShuffleCommand implements ICommandMain {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        if (context.getData().getMusicPlayer().toggleShuffleOnRepeat()) {
+            context.getData().getMusicPlayer().shuffle();
+            if (context.getData().getMusicPlayer().isShuffleEnabled()) {
+                context.getTypedMessaging().replySuccess("Shuffling has been enabled!");
+            }
+        } else {
+            if (!context.getData().getMusicPlayer().isShuffleEnabled()){
+                context.getTypedMessaging().replySuccess("Shuffling has been disabled!");
+            }
+        }
+
+    }
+
+    @Override
+    public Module getModule() {
+        return Module.MUSIC;
+    }
+
+    @Override
+    public String command() {
+        return "shuffle";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Shuffle command", "shuffle", true);
+    }
+
+    @Override
+    public String description() {
+        return "Shuffles music in a queue";
+    }
+
+}
