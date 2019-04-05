2a653b49ec2e3e514ccdf3763a7c69daf1e7e3c5
Ikinon <36565735+Ikinon@users.noreply.github.com>
2019-04-05 15:54:35 +0100

    Volume Command

---
+++ src/main/java/org/cascadebot/cascadebot/commands/music/VolumeCommand.java
@@ -0,0 +1,55 @@
+package org.cascadebot.cascadebot.commands.music;
+
+import net.dv8tion.jda.core.entities.Member;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandMain;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.music.CascadePlayer;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+public class VolumeCommand implements ICommandMain {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        CascadePlayer player = context.getData().getMusicPlayer();
+        if (context.getArgs().length == 0) {
+            context.getTypedMessaging().replyInfo("Current volume is " + player.getPlayer().getVolume());
+            return;
+        }
+        Integer Arguments = Integer.valueOf(context.getMessage(0));
+        if (Arguments > 100 || Arguments < 0) {
+            context.getTypedMessaging().replyWarning("Volume needs to be between 100 and 0");
+            return;
+        }
+
+        if (Arguments == context.getData().getMusicPlayer().getPlayer().getVolume()) {
+            context.getTypedMessaging().replyInfo("Volume is already " + player.getPlayer().getVolume());
+        } else {
+            player.getPlayer().setVolume(Arguments);
+            context.getTypedMessaging().replyInfo("Volume set to " + player.getPlayer().getVolume());
+        }
+
+    }
+
+
+    @Override
+    public Module getModule() {
+        return Module.MUSIC;
+    }
+
+    @Override
+    public String command() {
+        return "volume";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Volume", "search", getModule());
+    }
+
+    @Override
+    public String description() {
+        return "Changes the players volume";
+    }
+
+}
