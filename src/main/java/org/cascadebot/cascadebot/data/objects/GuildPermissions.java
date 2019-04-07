3ed949deda86a9731f56fb6ddac04042a2d445d5
William Oldham <wegg7250@gmail.com>
2019-04-07 18:07:38 +0100

    Evaluate permissions

--- src/main/java/org/cascadebot/cascadebot/data/objects/GuildPermissions.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/GuildPermissions.java
@@ -5,8 +5,6 @@
 
 package org.cascadebot.cascadebot.data.objects;
 
-import com.google.common.collect.Lists;
-import com.google.common.collect.Sets;
 import net.dv8tion.jda.core.Permission;
 import net.dv8tion.jda.core.entities.Channel;
 import net.dv8tion.jda.core.entities.Member;
@@ -14,6 +12,7 @@ import org.cascadebot.cascadebot.CascadeBot;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 import org.cascadebot.cascadebot.permissions.Security;
 import org.cascadebot.cascadebot.permissions.objects.Group;
+import org.cascadebot.cascadebot.permissions.objects.PermissionAction;
 import org.cascadebot.cascadebot.permissions.objects.PermissionMode;
 import org.cascadebot.cascadebot.permissions.objects.User;
 import org.cascadebot.shared.SecurityLevel;
@@ -42,12 +41,57 @@ public class GuildPermissions {
         if (member.isOwner()) return true;
         if (member.hasPermission(Permission.ADMINISTRATOR)) return true;
 
+        User user = users.computeIfAbsent(member.getUser().getIdLong(), id -> new User());
+        List<Group> userGroups = groups.stream().filter(group -> user.getGroupIds().contains(group.getId())).collect(Collectors.toList());
 
+        PermissionAction action = getDefaultAction(permission);
+        PermissionAction evaluatedAction = PermissionAction.NEUTRAL;
 
+        if (mode == PermissionMode.MOST_RESTRICTIVE) {
+            evaluatedAction = evaluateMostRestrictiveMode(user, userGroups, permission);
+        } else if (mode == PermissionMode.HIERARCHICAL) {
+            evaluatedAction = evaluateHierarchicalMode(user, userGroups, permission);
+        }
 
-        return false;
+        if (evaluatedAction != PermissionAction.NEUTRAL) {
+            action = evaluatedAction;
+        }
+
+        return action != PermissionAction.DENY;
     }
 
+    private PermissionAction evaluateMostRestrictiveMode(User user, List<Group> userGroups, CascadePermission permission) {
+        PermissionAction action = user.getPermissionAction(permission);
+        if (action == PermissionAction.DENY) return action;
+
+        for (Group group : userGroups) {
+            PermissionAction groupAction = group.getPermissionAction(permission);
+            if (groupAction == PermissionAction.NEUTRAL) continue;
+            if (groupAction == PermissionAction.DENY) return PermissionAction.DENY;
+            action = groupAction;
+        }
+        return action;
+    }
+
+    private PermissionAction evaluateHierarchicalMode(User user, List<Group> userGroups, CascadePermission permission) {
+        PermissionAction action = PermissionAction.NEUTRAL;
+        for (int i = userGroups.size() - 1; i >= 0; i--) {
+            PermissionAction groupAction = userGroups.get(i).getPermissionAction(permission);
+            if (groupAction == PermissionAction.NEUTRAL) continue;
+            action = groupAction;
+        }
+
+        PermissionAction userAction = user.getPermissionAction(permission);
+        if (userAction != PermissionAction.NEUTRAL) {
+            action = userAction;
+        }
+
+        return action;
+    }
+
+    private PermissionAction getDefaultAction(CascadePermission permission) {
+        return permission.isDefaultPerm() ? PermissionAction.ALLOW : PermissionAction.NEUTRAL;
+    }
 
     public Group createGroup(String name) {
         Set<String> ids = groups.stream().map(Group::getId).collect(Collectors.toSet());
@@ -60,10 +104,9 @@ public class GuildPermissions {
                 throw new IllegalStateException("Could not create a group with a unique ID!");
             }
         } while (ids.contains(group.getId()));
+        groups.add(group);
         return group;
     }
-
-
 
 
 }
