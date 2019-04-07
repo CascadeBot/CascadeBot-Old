71dfd86fd2d63e92e8ed6b001aca7cd1325061e1
William Oldham <wegg7250@gmail.com>
2019-04-07 11:06:00 +0100

    Create enums

---
+++ src/main/java/org/cascadebot/cascadebot/permissions/objects/PermissionMode.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.permissions.objects;
+
+public enum PermissionMode {
+
+    HIERARCHICAL,
+    MOST_RESTRICTIVE
+
+}
