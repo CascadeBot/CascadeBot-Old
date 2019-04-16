ea8d5c8ff423d9747ae19215a5c1de5ac8cf3106
William Oldham <wegg7250@gmail.com>
2019-04-16 21:53:33 +0100

    Implement caching for commonly used results

--- src/main/java/org/cascadebot/cascadebot/permissions/objects/Result.java
+++ src/main/java/org/cascadebot/cascadebot/permissions/objects/Result.java
@@ -1,26 +1,47 @@
 package org.cascadebot.cascadebot.permissions.objects;
 
+import org.apache.commons.lang3.tuple.Pair;
+
+import java.util.HashMap;
+import java.util.Map;
+
 public class Result {
 
     private PermissionAction action;
     private PermissionHolder container;
     private ResultCause cause;
 
-    public Result(PermissionAction action) {
+    private static Map<Pair<PermissionAction, ResultCause>, Result> cache = new HashMap<>();
+
+    private Result(PermissionAction action) {
         this.action = action;
     }
 
-    public Result(PermissionAction action, ResultCause cause) {
+    private Result(PermissionAction action, ResultCause cause) {
         this.action = action;
         this.cause = cause;
     }
 
-    public Result(PermissionAction action, PermissionHolder container) {
+    private Result(PermissionAction action, PermissionHolder container) {
         this.action = action;
         if (container != null) {
             this.container = container;
             this.cause = ResultCause.valueOf(container.getType().name());
         }
+    }
+
+    public static Result of(PermissionAction action) {
+        var key = Pair.of(action, (ResultCause) null);
+        return cache.computeIfAbsent(key, pairKey -> new Result(pairKey.getKey()));
+    }
+
+    public static Result of(PermissionAction action, ResultCause cause) {
+        var key = Pair.of(action, cause);
+        return cache.computeIfAbsent(key, pairKey -> new Result(pairKey.getKey(), pairKey.getValue()));
+    }
+
+    public static Result of(PermissionAction action, PermissionHolder container) {
+        return new Result(action, container);
     }
 
     public PermissionAction getAction() {
