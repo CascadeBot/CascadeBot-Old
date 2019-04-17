042b6461ce65f601a67fe5dc73e87c018c9c945b
William Oldham <wegg7250@gmail.com>
2019-04-17 19:35:13 +0100

    Document!

--- src/main/java/org/cascadebot/cascadebot/permissions/objects/ResultCause.java
+++ src/main/java/org/cascadebot/cascadebot/permissions/objects/ResultCause.java
@@ -1,5 +1,31 @@
 package org.cascadebot.cascadebot.permissions.objects;
 
 public enum ResultCause {
-    GROUP, USER, DISCORD, OFFICIAL, GUILD, DEFAULT
+
+    /**
+     * Indicates that this result originated from a permission in a group.
+     */
+    GROUP,
+    /**
+     * Indicates that this result originated from a user permission.
+     */
+    USER,
+    /**
+     * Indicates that this result was allowed because of a discord permission.
+     */
+    DISCORD,
+    /**
+     * Indicates that this result was allowed because of official authorisation.
+     * i.e. a developer or owner where all permissions are allowed.
+     */
+    OFFICIAL,
+    /**
+     * Indicates that this result is directly from the guild. At the time of writing
+     * this is only used for permissions being checked against an administrator/owner
+     */
+    GUILD,
+    /**
+     * Indicates that this result is generated from default permissions
+     */
+    DEFAULT
 }
