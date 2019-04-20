08ccd23c0178481e7b8e265ffce1d1b78aa01a9f
weeryan17 <theweeryan17@gmail.com>
2019-04-20 15:39:05 -0500

    more documentation.

--- src/main/java/org/cascadebot/cascadebot/utils/votes/VoteMessageType.java
+++ src/main/java/org/cascadebot/cascadebot/utils/votes/VoteMessageType.java
@@ -7,9 +7,24 @@ package org.cascadebot.cascadebot.utils.votes;
 
 public enum VoteMessageType {
 
+    /**
+     * This vote type will use ✅ (\u2705) and ❌ (\u274C) for votes
+     */
     YES_NO,
+
+    /**
+     * This vote type will use discord regional indicator numbers up to the amount specified in the builder.
+     */
     NUMBERS,
+
+    /**
+     * This vote type will use discord regional indicator letters up to the amount specified in the builder.
+     */
     LETTERS,
+
+    /**
+     * This vote type will give you complete control over what reactions you want to use .
+     */
     CUSTOM
 
 }
