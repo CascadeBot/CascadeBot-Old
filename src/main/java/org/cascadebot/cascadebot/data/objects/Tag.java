bc251ae34064e99401023a98d746077b5704c2cd
James Hawkins <jhawki2005@gmail.com>
2019-04-22 21:34:52 +0100

    swanky placeholder system lol :D

--- src/main/java/org/cascadebot/cascadebot/data/objects/Tag.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/Tag.java
@@ -5,37 +5,59 @@
 
 package org.cascadebot.cascadebot.data.objects;
 
+import org.apache.commons.lang.math.NumberUtils;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.utils.FormatUtils;
 
 import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.function.BiFunction;
 import java.util.function.Function;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Tag {
 
-    private static final Map<String, Function<CommandContext, String>> placeholders = new HashMap<>();
+    private static final Map<String, BiFunction<CommandContext, String[], String>> placeholders = new HashMap<>();
+    // https://regex101.com/r/hlsgVW/1
+    private static final Pattern TAG_PATTERN = Pattern.compile("\\{([A-z]+)(?::((?:,?\\w+)+))?}");
+
     private String content;
     private String category;
 
     static {
         // Guild
-        placeholders.put("{server_region}", (context -> context.getGuild().getRegion().getName()));
-        placeholders.put("{server_name}", (context -> context.getGuild().getName()));
-        placeholders.put("{server_owner}", (context -> context.getGuild().getOwner().getEffectiveName()));
-        placeholders.put("{member_count}", (context -> String.valueOf(context.getGuild().getMembers().size())));
+        addPlaceholder("server_region", (context -> context.getGuild().getRegion().getName()));
+        addPlaceholder("server_name", (context -> context.getGuild().getName()));
+        addPlaceholder("server_owner", (context -> context.getGuild().getOwner().getEffectiveName()));
+        addPlaceholder("member_count", (context -> String.valueOf(context.getGuild().getMembers().size())));
 
         // Sender
-        placeholders.put("{sender}", (context -> context.getMember().getEffectiveName()));
-        placeholders.put("{sender_id}", (context -> context.getUser().getId()));
-        placeholders.put("{sender_mention}", (context -> context.getMember().getAsMention()));
-
+        addPlaceholder("sender", (context -> context.getMember().getEffectiveName()));
+        addPlaceholder("sender_id", (context -> context.getUser().getId()));
+        addPlaceholder("sender_mention", (context -> context.getMember().getAsMention()));
+        
         // Channel
-        placeholders.put("{channel_name}", (context -> context.getChannel().getName()));
+        addPlaceholder("channel_name", (context -> context.getChannel().getName()));
 
         // Other
-        placeholders.put("{time}", (context -> FormatUtils.formatDateTime(OffsetDateTime.now())));
+        addPlaceholder("time", (context -> FormatUtils.formatDateTime(OffsetDateTime.now())));
+
+        addPlaceholder("args", (context, placeholderArgs) -> {
+            if (placeholderArgs.length == 0) return "";
+            int arg = NumberUtils.toInt(placeholderArgs[0], -1);
+            if (arg == -1 || arg > context.getArgs().length - 1) return "";
+            else return context.getArg(arg);
+        });
+    }
+    
+    private static void addPlaceholder(String key, Function<CommandContext, String> function) {
+        placeholders.put(key, (context, strings) -> function.apply(context));
+    }
+
+    private static void addPlaceholder(String key, BiFunction<CommandContext, String[], String> function) {
+        placeholders.put(key, function);
     }
 
     public Tag(String content, String category) {
@@ -59,6 +81,19 @@ public class Tag {
 
     public void setCategory(String category) {
         this.category = category;
+    }
+
+    public String formatTag(CommandContext commandContext) {
+        String message = content;
+        for (String key : placeholders.keySet()) {
+            Matcher matcher = TAG_PATTERN.matcher(content);
+            if (matcher.find()) {
+                if (key.equalsIgnoreCase(matcher.group(1))) {
+                    message = message.replace(matcher.group(), placeholders.get(key).apply(commandContext, matcher.group(2).split(",")));
+                }
+            }
+        }
+        return message;
     }
 
 }
