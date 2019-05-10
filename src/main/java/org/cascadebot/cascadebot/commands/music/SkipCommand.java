0094d5841443e8ad08b4fcfdde88a71ea509b517
William Oldham <wegg7250@gmail.com>
2019-05-10 19:47:07 +0100

    The people have spoken!

--- src/main/java/org/cascadebot/cascadebot/commands/music/SkipCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/music/SkipCommand.java
@@ -39,11 +39,6 @@ public class SkipCommand implements ICommandMain {
             return;
         }
 
-        if (context.getMusicPlayer().getTracks().peek() == null) {
-            context.getTypedMessaging().replyDanger("No song to skip to!");
-            return;
-        }
-
         if (context.getArgs().length > 0) {
             if (context.getArg(0).equalsIgnoreCase("force")) {
                 if (context.hasPermission("skip.force")) {
