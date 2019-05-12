a383e55188f8ab2c9aa116044d9bc50c031816ca
weeryan17 <theweeryan17@gmail.com>
2019-05-12 13:34:02 -0500

    move tags over to enum

--- src/main/java/org/cascadebot/cascadebot/data/objects/Tag.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/Tag.java
@@ -1,11 +1,12 @@
 /*
-  * Copyright (c) 2019 CascadeBot. All rights reserved.
-  * Licensed under the MIT license.
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
  */
 
 package org.cascadebot.cascadebot.data.objects;
 
 import org.apache.commons.lang.math.NumberUtils;
+import org.apache.commons.lang3.EnumUtils;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.utils.FormatUtils;
 
@@ -19,46 +20,11 @@ import java.util.regex.Pattern;
 
 public class Tag {
 
-    private static final Map<String, BiFunction<CommandContext, String[], String>> placeholders = new HashMap<>();
     // https://regex101.com/r/hlsgVW/1
     private static final Pattern TAG_PATTERN = Pattern.compile("\\{([A-z]+)(?::((?:,?\\w+)+))?}");
 
     private String content;
     private String category;
-
-    static {
-        // Guild
-        addPlaceholder("server_region", (context -> context.getGuild().getRegion().getName()));
-        addPlaceholder("server_name", (context -> context.getGuild().getName()));
-        addPlaceholder("server_owner", (context -> context.getGuild().getOwner().getEffectiveName()));
-        addPlaceholder("member_count", (context -> String.valueOf(context.getGuild().getMembers().size())));
-
-        // Sender
-        addPlaceholder("sender", (context -> context.getMember().getEffectiveName()));
-        addPlaceholder("sender_id", (context -> context.getUser().getId()));
-        addPlaceholder("sender_mention", (context -> context.getMember().getAsMention()));
-        
-        // Channel
-        addPlaceholder("channel_name", (context -> context.getChannel().getName()));
-
-        // Other
-        addPlaceholder("time", (context -> FormatUtils.formatDateTime(OffsetDateTime.now())));
-
-        addPlaceholder("args", (context, placeholderArgs) -> {
-            if (placeholderArgs.length == 0) return "";
-            int arg = NumberUtils.toInt(placeholderArgs[0], -1);
-            if (arg == -1 || arg > context.getArgs().length - 1) return "";
-            else return context.getArg(arg);
-        });
-    }
-    
-    private static void addPlaceholder(String key, Function<CommandContext, String> function) {
-        placeholders.put(key, (context, strings) -> function.apply(context));
-    }
-
-    private static void addPlaceholder(String key, BiFunction<CommandContext, String[], String> function) {
-        placeholders.put(key, function);
-    }
 
     public Tag(String content, String category) {
         this.content = content;
@@ -85,12 +51,12 @@ public class Tag {
 
     public String formatTag(CommandContext commandContext) {
         String message = content;
-        for (String key : placeholders.keySet()) {
-            Matcher matcher = TAG_PATTERN.matcher(content);
-            if (matcher.find()) {
-                if (key.equalsIgnoreCase(matcher.group(1))) {
-                    message = message.replace(matcher.group(), placeholders.get(key).apply(commandContext, matcher.group(2).split(",")));
-                }
+        Tags.class.getEnumConstants();
+        Matcher matcher = TAG_PATTERN.matcher(content);
+        while (matcher.find()) {
+            Tags tag = EnumUtils.getEnum(Tags.class, matcher.group(1).toUpperCase());
+            if (tag != null) {
+                message = message.replace(matcher.group(), tag.getFunction().apply(commandContext, matcher.group(2).split(",")));
             }
         }
         return message;
---
+++ src/main/java/org/cascadebot/cascadebot/data/objects/Tags.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.data.objects;
+
+import org.apache.commons.lang.math.NumberUtils;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.utils.FormatUtils;
+
+import java.time.OffsetDateTime;
+import java.util.function.BiFunction;
+
+public enum Tags {
+
+    //region Server
+    SERVER_REGION(((context, args) -> context.getGuild().getRegion().getName())),
+    SERVER_NAME((((context, args) -> context.getGuild().getName()))),
+    SERVER_OWNER((((context, args) -> context.getGuild().getOwner().getEffectiveName()))),
+    MEMBER_COUNT((((context, args) -> String.valueOf(context.getGuild().getMembers().size())))),
+    //endregion
+
+    //region Sender
+    SENDER((((context, args) -> context.getMember().getEffectiveName()))),
+    SENDER_ID((((context, args) -> context.getUser().getId()))),
+    SENDER_MENTION((((context, args) -> context.getMember().getAsMention()))),
+    //endregion
+
+    //region channel
+    CHANNEL_NAME((((context, args) -> context.getChannel().getName()))),
+    //endregion
+
+    //region Other
+    TIME((((context, args) -> FormatUtils.formatDateTime(OffsetDateTime.now())))),
+    ARGS((((context, args) -> {
+        if (args.length == 0) return "";
+        int arg = NumberUtils.toInt(args[0], -1);
+        if (arg == -1 || arg > context.getArgs().length - 1) return "";
+        else return context.getArg(arg);
+    })));
+    //endregion
+
+    private BiFunction<CommandContext, String[], String> function;
+
+    Tags(BiFunction<CommandContext, String[], String> function) {
+        this.function = function;
+    }
+
+    public BiFunction<CommandContext, String[], String> getFunction() {
+        return function;
+    }
+
+}
