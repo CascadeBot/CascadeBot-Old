eb9da2b928e9c355422ed9f2f9ec9559c177ff43
William Oldham <wegg7250@gmail.com>
2019-05-21 17:36:54 +0100

    Document setting annotation

--- src/main/java/org/cascadebot/cascadebot/data/objects/Setting.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/Setting.java
@@ -16,14 +16,35 @@ import java.lang.annotation.Target;
 @Retention(RetentionPolicy.RUNTIME)
 public @interface Setting {
 
+
+    /**
+     * This is the name that will be used on the web dashboard instead of the
+     * field name. TODO: This will need to be localised
+     */
     String niceName();
 
+
+    /**
+     * The description of this setting to be displayed on the web dashboard.
+     * TODO: This will also need to be localised
+     */
     String description() default "";
 
+
+    /**
+     * The flag(s) required to have access to change this setting.
+     */
     Flag[] flagRequired() default {};
 
+
+    /**
+     * The module(s) that this setting belongs to. Used to organise the web panel.
+     */
     Module[] module() default {};
 
+    /**
+     * Whether the user can directly edit this setting through the ;settings command.
+     */
     boolean directlyEditable() default true;
 
 }
