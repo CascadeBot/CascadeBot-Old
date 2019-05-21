34f5dd5ebc340452f9a2b344c9fdf1b2ede03a59
William Oldham <wegg7250@gmail.com>
2019-05-21 17:19:09 +0100

    Create a setting interface

--- src/main/java/org/cascadebot/cascadebot/data/objects/Setting.java
+++ src/main/java/org/cascadebot/cascadebot/data/objects/Setting.java
@@ -5,6 +5,8 @@
 
 package org.cascadebot.cascadebot.data.objects;
 
+import org.cascadebot.cascadebot.commandmeta.Module;
+
 import java.lang.annotation.ElementType;
 import java.lang.annotation.Retention;
 import java.lang.annotation.RetentionPolicy;
@@ -12,8 +14,16 @@ import java.lang.annotation.Target;
 
 @Target({ElementType.FIELD})
 @Retention(RetentionPolicy.RUNTIME)
-public @interface FlagRequired {
+public @interface Setting {
 
-    Flag value();
+    String niceName();
+
+    String description() default "";
+
+    Flag[] flagRequired() default {};
+
+    Module[] module() default {};
+
+    boolean directlyEditable() default true;
 
 }
