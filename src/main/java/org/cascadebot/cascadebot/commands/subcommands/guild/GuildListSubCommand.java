e39185d71688db37b1b6fbb7045563ae46fdfa1f
James Hawkins <jhawki2005@gmail.com>
2019-05-25 07:14:04 +0100

    I GOT IT!

--- src/main/java/org/cascadebot/cascadebot/commands/subcommands/guild/GuildListSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/subcommands/guild/GuildListSubCommand.java
@@ -1,41 +1,33 @@
 package org.cascadebot.cascadebot.commands.subcommands.guild;
 
+import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Guild;
 import net.dv8tion.jda.core.entities.Member;
 import org.cascadebot.cascadebot.CascadeBot;
+import org.cascadebot.cascadebot.UnicodeConstants;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandExecutable;
 import org.cascadebot.cascadebot.data.objects.Flag;
+import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
-import org.cascadebot.cascadebot.utils.Table;
-import org.cascadebot.cascadebot.utils.pagination.PageUtils;
 
 public class GuildListSubCommand implements ICommandExecutable {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        Guild guildForInfo = context.getGuild();
-
-        if (context.getArgs().length > 0) {
-            guildForInfo = CascadeBot.INS.getShardManager().getGuildById(context.getArg(0));
-        }
-        if (guildForInfo == null) {
-            context.getTypedMessaging().replyDanger("We couldn't find that guild!");
-            return;
-        }
-
-        Table.TableBuilder builder = new Table.TableBuilder("Flag Name");
+        EmbedBuilder builder = MessagingObjects.getClearThreadLocalEmbedBuilder();
+        builder.setTitle(context.getGuild().getName());
+        builder.setThumbnail(context.getGuild().getIconUrl());
 
         for (Flag flag : Flag.values()) {
-            builder.addRow(flag.name());
             if (context.getData().isFlagEnabled(flag)) {
-                builder.addRow("Enabled");
-            } else {
-                builder.addRow("Disabled");
+                builder.addField(flag.name(), "Enabled " + UnicodeConstants.TICK, false);
+            } else if (!context.getData().isFlagEnabled(flag)) {
+                builder.addField(flag.name(), "Disabled " + UnicodeConstants.RED_CROSS, false);
             }
         }
 
-        context.getUIMessaging().sendPagedMessage(PageUtils.splitTableDataToPages(builder.build(), 20));
+        context.getTypedMessaging().replyInfo(builder);
     }
 
     @Override
