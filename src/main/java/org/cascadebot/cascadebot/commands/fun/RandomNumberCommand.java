c7cc53d6db7b56d897a9570631e25fbcdb857007
Deadly <31647160+DeadlyFirex@users.noreply.github.com>
2019-06-12 23:38:37 +0200

    boo

--- src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
@@ -13,11 +13,37 @@ import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
-import org.cascadebot.cascadebot.utils.WebUtils;
-import org.cascadebot.cascadebot.utils.buttons.Button;
-import org.cascadebot.cascadebot.utils.buttons.ButtonGroup;
 import java.io.IOException;mport java.util.Random;
 
-Random random_obj = new Random();
-int random_number = random_obj.nextInt(999);
 
+public class RandCommand extends ICommandMain
+{
+    @Override
+    public void onCommand(Member sender, CommandContext context)
+        Message message = event.getMessage();
+        String content = message.getContentRaw(); 
+        Random random_obj = new Random();
+        int random_number = random_obj.nextInt(999);
+        MessageChannel channel = event.getChannel();
+        channel.sendMessage(random_number).queue();
+    @Override
+    public String command() {
+        return "rand";
+    }
+ 
+    @Override
+    public Module getModule() {
+        return Module.FUN;
+    }
+ 
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Rand command", "rand", true);
+    }
+ 
+    @Override
+    public String description() {
+        return "Returns a random number";
+    }
+ 
+}
