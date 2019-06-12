57aa234403a98cb22dc42e1c2c4d458e0c619568
weeryan17 <theweeryan17@gmail.com>
2019-06-12 14:05:50 -0500

    Basic group commands

---
+++ src/main/java/org/cascadebot/cascadebot/commands/management/permission/GroupPermissionCommand.java
@@ -0,0 +1,47 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.commands.management.permission;
+
+import java.util.Set;
+import net.dv8tion.jda.core.entities.Member;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandExecutable;
+import org.cascadebot.cascadebot.commandmeta.ICommandMain;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+public class GroupPermissionCommand implements ICommandMain {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        context.getUIMessaging().replyUsage(this);
+    }
+
+    @Override
+    public Module getModule() {
+        return Module.MANAGEMENT;
+    }
+
+    @Override
+    public String command() {
+        return "groupperms";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Group permissions command", "permissions.group", false, Module.MANAGEMENT);
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+
+    @Override
+    public Set<ICommandExecutable> getSubCommands() {
+        return Set.of(new GroupPermissionCreateSubCommand(), new GroupPermissionAddPermissionSubCommand());
+    }
+}
---
+++ src/main/java/org/cascadebot/cascadebot/commands/management/permission/GroupPermissionRemovePermissionSubCommand.java
@@ -0,0 +1,52 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.commands.management.permission;
+
+import net.dv8tion.jda.core.entities.Member;
+import org.cascadebot.cascadebot.CascadeBot;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandExecutable;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+public class GroupPermissionRemovePermissionSubCommand implements ICommandExecutable {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        if(context.getArgs().length < 2) {
+            context.getUIMessaging().replyUsage(this, "groupperms");
+            return;
+        }
+
+        if(!CascadeBot.INS.getPermissionsManager().isValidPermission(context.getGuild(), context.getArg(1))) {
+            context.getTypedMessaging().replyDanger("`%s` isn't a valid permission", context.getArg(1));
+            return;
+        }
+
+        PermissionCommandUtils.tryGetGroupFromString(context, context.getArg(0), group -> {
+            if(group.removePermission(context.getArg(1))) {
+                context.getTypedMessaging().replySuccess("Successfully removed permission `%s` from group `%s`", context.getArg(1), group.getName() + "(" + group.getId() + ")");
+            } else {
+                context.getTypedMessaging().replyWarning("Couldn't removed permission `%s` from group `%s` as they already have the permission", context.getArg(1), group.getName() + "(" + group.getId() + ")");
+            }
+        }, sender.getUser().getIdLong());
+    }
+
+    @Override
+    public String command() {
+        return "removepermission";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return CascadePermission.of("Group permissions add sub command", "permissions.group.removepermission", false, Module.MANAGEMENT);
+    }
+
+    @Override
+    public String description() {
+        return null;
+    }
+}
