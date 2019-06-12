103faed2449376f5aaefbe0fbaba971815ac5e92
Deadly <31647160+DeadlyFirex@users.noreply.github.com>
2019-06-12 22:41:22 +0200

    import log

--- src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
@@ -1,5 +1,15 @@
-
-import java.util.Random;
+import net.dv8tion.jda.core.EmbedBuilder;
+import net.dv8tion.jda.core.entities.Member;
+import org.cascadebot.cascadebot.UnicodeConstants;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ICommandMain;
+import org.cascadebot.cascadebot.commandmeta.Module;
+import org.cascadebot.cascadebot.messaging.MessagingObjects;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+import org.cascadebot.cascadebot.utils.WebUtils;
+import org.cascadebot.cascadebot.utils.buttons.Button;
+import org.cascadebot.cascadebot.utils.buttons.ButtonGroup;
+import java.io.IOException;mport java.util.Random;
 
 Random random_obj = new Random();
 int random_number = random_obj.nextInt(999);
