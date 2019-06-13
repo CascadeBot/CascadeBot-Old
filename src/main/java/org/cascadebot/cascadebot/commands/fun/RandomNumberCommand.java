5aa1d0d014b8393478ca23dc0cfd8dd040403137
Deadly <31647160+DeadlyFirex@users.noreply.github.com>
2019-06-13 21:19:53 +0200

    yes it's not 4AM

--- src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/RandomNumberCommand.java
@@ -2,50 +2,63 @@
  * Copyright (c) 2019 CascadeBot. All rights reserved.
  * Licensed under the MIT license.
  */
- 
+
 package org.cascadebot.cascadebot.commands.fun;
 
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.entities.Member;
+import net.dv8tion.jda.core.entities.Message;
+
 import org.cascadebot.cascadebot.UnicodeConstants;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
-import java.io.IOException;mport java.util.Random;
+import java.io.IOException;
+import java.util.Random;
 
+public class RandomNumberCommand implements ICommandMain {
 
-public class RandCommand extends ICommandMain
-{
+	@Override
+	public void onCommand(Member sender, CommandContext context) {
+	    Random randomObj = new Random();
+		try {
+		    String argRaw = context.getArg(0);   
+		    if (argRaw.matches("[0-9]+") == false) {	
+		        context.getTypedMessaging().replyInfo("Please provide numbers only");
+		    }
+		    
+		    else {
+		        int argOne = Integer.parseInt(argRaw);
+		        int randomNumberInt = randomObj.nextInt(argOne);
+		        String randomNumber = Integer.toString(randomNumberInt);
+		        context.getTypedMessaging().replyInfo("Random number is " + randomNumber);
+		    }
+		    
+		} catch(Exception ArrayIndexOutOfBoundsException) {
+		      context.getTypedMessaging().replyInfo("No arguments given");
+	      }
+	}
+	      
+	@Override
+	public String command() {
+        return "randnum";
+	}
 
-    @Override
-    public void onCommand(Member sender, CommandContext context)
-        Message message = event.getMessage();
-        String content = message.getContentRaw(); 
-        Random random_obj = new Random();
-        int random_number = random_obj.nextInt(999);
-        MessageChannel channel = event.getChannel();
-        channel.sendMessage(random_number).queue();
+	@Override
+	public Module getModule() {
+	    return Module.FUN;
+	}
 
-    @Override
-    public String command() {
-        return "rand";
-    }
- 
-    @Override
-    public Module getModule() {
-        return Module.FUN;
-    }
- 
-    @Override
-    public CascadePermission getPermission() {
-        return CascadePermission.of("Rand command", "rand", true);
-    }
- 
-    @Override
-    public String description() {
-        return "Returns a random number";
-    }
- 
+	@Override
+	public CascadePermission getPermission() {
+	    return CascadePermission.of("Random number command", "randnum", true);
+	}
+
+	@Override
+	public String description() {
+	    return "Returns a random number";
+	}
+
 }
