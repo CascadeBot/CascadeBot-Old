0bef02b6604bf21e9fc28544f3b22a528833b34c
mrjvs <jellevs@gmail.com>
2019-06-14 12:52:47 +0200

    Added semantic colors as constants

--- src/main/java/org/cascadebot/cascadebot/Constants.java
+++ src/main/java/org/cascadebot/cascadebot/Constants.java
@@ -6,6 +6,9 @@
 package org.cascadebot.cascadebot;
 
 import lombok.experimental.UtilityClass;
+import org.cascadebot.shared.SharedConstants;
+
+import java.awt.*;
 
 @UtilityClass
 public class Constants {
@@ -13,5 +16,12 @@ public class Constants {
     // Changeable constants if needed
     public static final String serverInvite = "https://discord.gg/P23GZFB";
 
+    // Semantic colors used for embeds
+    public static final Color COLOR_SUCCESS = new Color(132, 214, 162);
+    public static final Color COLOR_INFO = new Color(152, 159, 255);
+    public static final Color COLOR_WARNING = new Color(255, 187, 95);
+    public static final Color COLOR_DANGER = new Color(16741492);
+    public static final Color COLOR_MODERATION = Color.WHITE;
+    public static final Color COLOR_NEUTRAL = SharedConstants.CASCADE_COLOR;
 
 }
