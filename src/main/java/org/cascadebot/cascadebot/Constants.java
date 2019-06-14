31b0face987a3857a0c2afe2dd667d5c87ba58ca
mrjvs <jellevs@gmail.com>
2019-06-14 13:07:38 +0200

    Added hex color comments to color constants

--- src/main/java/org/cascadebot/cascadebot/Constants.java
+++ src/main/java/org/cascadebot/cascadebot/Constants.java
@@ -17,10 +17,10 @@ public class Constants {
     public static String serverInvite = "https://discord.gg/P23GZFB";
 
     // Semantic colors used for embeds
-    public static final Color COLOR_SUCCESS = new Color(132, 214, 162);
-    public static final Color COLOR_INFO = new Color(152, 159, 255);
-    public static final Color COLOR_WARNING = new Color(255, 187, 95);
-    public static final Color COLOR_DANGER = new Color(16741492);
+    public static final Color COLOR_SUCCESS = new Color(132, 214, 162); // #84D6A2
+    public static final Color COLOR_INFO = new Color(152, 159, 255); // #989FFF
+    public static final Color COLOR_WARNING = new Color(255, 187, 95); // #FFBB5F
+    public static final Color COLOR_DANGER = new Color(16741492); // #FF7474
     public static final Color COLOR_MODERATION = Color.WHITE;
     public static final Color COLOR_NEUTRAL = SharedConstants.CASCADE_COLOR;
 
