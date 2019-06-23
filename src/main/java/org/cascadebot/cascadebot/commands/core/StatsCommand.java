98c6e41c63cf3b10ad1e52e261c705db8ea55bdd
James Hawkins <jhawki2005@gmail.com>
2019-06-23 19:00:26 +0100

    oo

--- src/main/java/org/cascadebot/cascadebot/commands/core/StatsCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/core/StatsCommand.java
@@ -1,31 +1,23 @@
 package org.cascadebot.cascadebot.commands.core;
 
-import ch.qos.logback.core.pattern.ConverterUtil;
-import lavalink.client.player.LavalinkPlayer;
 import net.dv8tion.jda.core.EmbedBuilder;
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDAInfo;
 import net.dv8tion.jda.core.entities.Member;
 import org.apache.commons.io.FileUtils;
 import org.cascadebot.cascadebot.CascadeBot;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandCore;
-import org.cascadebot.cascadebot.commandmeta.ICommandMain;
-import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.messaging.MessagingObjects;
-import org.cascadebot.cascadebot.music.LavaPlayerAudioSendHandler;
-import org.cascadebot.cascadebot.music.MusicHandler;
-import org.cascadebot.cascadebot.permissions.CascadePermission;
 
-import javax.management.ObjectName;
+import com.sun.management.OperatingSystemMXBean;
+
 import java.lang.management.ManagementFactory;
-import java.lang.management.OperatingSystemMXBean;
 
 public class StatsCommand implements ICommandCore {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
         EmbedBuilder builder = MessagingObjects.getClearThreadLocalEmbedBuilder();
+        OperatingSystemMXBean osBean = ManagementFactory.getPlatformMXBean(OperatingSystemMXBean.class);
 
         builder.setTitle(context.getSelfUser().getName());
         builder.setThumbnail(context.getSelfUser().getAvatarUrl());
@@ -34,7 +26,7 @@ public class StatsCommand implements ICommandCore {
         builder.addField("Active Guilds", String.valueOf(CascadeBot.INS.getShardManager().getGuildCache().size()), true);
         builder.addField("Active Voice Channels", String.valueOf(CascadeBot.INS.getShardManager().getVoiceChannels().size()), true);
         builder.addField("RAM Usage", FileUtils.byteCountToDisplaySize(Runtime.getRuntime().totalMemory()), true);
-        builder.addField("CPU Load", String.valueOf(ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage()), true);
+        builder.addField("CPU Load", String.valueOf(osBean.getSystemCpuLoad()), true);
         builder.addField("Total Shards", String.valueOf(CascadeBot.INS.getShardManager().getShardsTotal()), true);
         builder.addField("Online Shards", String.valueOf(CascadeBot.INS.getShardManager().getShardsRunning()), true);
         builder.addField("Ping", String.valueOf(context.getChannel().getJDA().getPing()), true);
