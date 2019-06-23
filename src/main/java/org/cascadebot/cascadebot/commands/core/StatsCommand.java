2e5c2a1c78d48b0962da262dc28e06a9540ab367
James Hawkins <jhawki2005@gmail.com>
2019-06-23 18:25:44 +0100

    ooo

--- src/main/java/org/cascadebot/cascadebot/commands/core/StatsCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/core/StatsCommand.java
@@ -1,10 +1,12 @@
 package org.cascadebot.cascadebot.commands.core;
 
+import ch.qos.logback.core.pattern.ConverterUtil;
 import lavalink.client.player.LavalinkPlayer;
 import net.dv8tion.jda.core.EmbedBuilder;
 import net.dv8tion.jda.core.JDA;
 import net.dv8tion.jda.core.JDAInfo;
 import net.dv8tion.jda.core.entities.Member;
+import org.apache.commons.io.FileUtils;
 import org.cascadebot.cascadebot.CascadeBot;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandCore;
@@ -15,7 +17,11 @@ import org.cascadebot.cascadebot.music.LavaPlayerAudioSendHandler;
 import org.cascadebot.cascadebot.music.MusicHandler;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 
-public class StatusCommand implements ICommandCore {
+import javax.management.ObjectName;
+import java.lang.management.ManagementFactory;
+import java.lang.management.OperatingSystemMXBean;
+
+public class StatsCommand implements ICommandCore {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
@@ -24,8 +30,11 @@ public class StatusCommand implements ICommandCore {
         builder.setTitle(context.getSelfUser().getName());
         builder.setThumbnail(context.getSelfUser().getAvatarUrl());
 
-        builder.addField("Cascade Version", CascadeBot.getVersion().toString(), true);
-        builder.addField("JDA Version", JDAInfo.VERSION, true);
+        builder.addField("Total Guilds", String.valueOf(CascadeBot.INS.getShardManager().getGuilds().size()), true);
+        builder.addField("Active Guilds", String.valueOf(CascadeBot.INS.getShardManager().getGuildCache().size()), true);
+        builder.addField("Active Voice Channels", String.valueOf(CascadeBot.INS.getShardManager().getVoiceChannels().size()), true);
+        builder.addField("RAM Usage", FileUtils.byteCountToDisplaySize(Runtime.getRuntime().totalMemory()), true);
+        builder.addField("CPU Load", String.valueOf(ManagementFactory.getOperatingSystemMXBean().getSystemLoadAverage()), true);
         builder.addField("Total Shards", String.valueOf(CascadeBot.INS.getShardManager().getShardsTotal()), true);
         builder.addField("Online Shards", String.valueOf(CascadeBot.INS.getShardManager().getShardsRunning()), true);
         builder.addField("Ping", String.valueOf(context.getChannel().getJDA().getPing()), true);
@@ -37,7 +46,7 @@ public class StatusCommand implements ICommandCore {
 
     @Override
     public String command() {
-        return "status";
+        return "stats";
     }
 
     @Override
