2e8621fe81e6f8f95b724f8616c41b5752829c84
William Oldham <wegg7250@gmail.com>
2019-07-24 21:11:30 +0100

    Authorisation (Not Authorization)

--- src/main/java/org/cascadebot/cascadebot/data/graphql/GraphQLRoute.java
+++ src/main/java/org/cascadebot/cascadebot/data/graphql/GraphQLRoute.java
@@ -1,17 +1,15 @@
 package org.cascadebot.cascadebot.data.graphql;
 
-import com.google.gson.JsonObject;
 import graphql.ExecutionInput;
 import graphql.ExecutionResult;
+import org.apache.commons.lang3.StringUtils;
 import org.cascadebot.cascadebot.CascadeBot;
+import org.cascadebot.cascadebot.data.Config;
 import org.cascadebot.cascadebot.data.graphql.objects.GraphQLRequest;
 import org.cascadebot.cascadebot.data.graphql.objects.QLContext;
-import org.cascadebot.cascadebot.data.language.Locale;
 import spark.Request;
 import spark.Response;
 import spark.Route;
-
-import java.util.Arrays;
 
 public class GraphQLRoute implements Route {
 
@@ -25,22 +23,43 @@ public class GraphQLRoute implements Route {
     public Object handle(Request request, Response response) throws Exception {
         GraphQLRequest graphQLRequest = CascadeBot.getGSON().fromJson(request.body(), GraphQLRequest.class);
 
-        response.type("application/json");
+//        Locale locale = Arrays.stream(Locale.values())
+//                .filter(locale1 -> locale1.getLanguageCode().equalsIgnoreCase(request.headers("Cascade-Locale")))
+//                .findFirst()
+//                .orElse(Locale.getDefaultLocale());
 
-        Locale locale = Arrays.stream(Locale.values())
-                .filter(locale1 -> locale1.getLanguageCode().equalsIgnoreCase(request.headers("Cascade-Locale")))
-                .findFirst()
-                .orElse(Locale.getDefaultLocale());
+        long userId;
+        long guildId = -1;
+        try {
+            userId = Long.parseLong(request.headers("Cascade-UserID"));
+            if (!StringUtils.isEmpty(request.headers("Cascade-GuildID"))) {
+                guildId = Long.parseLong(request.headers("Cascade-GuildID"));
+            }
+        } catch (NumberFormatException e) {
+            response.status(400);
+            return "The ID's provided are invalid! " + e;
+        }
+
+        if (StringUtils.isEmpty(request.headers("Authorization"))) {
+            response.status(401);
+            return "You are not authorized!";
+        }
+
+        if (!Config.INS.getAuth().verifyEncrypt(Long.toString(userId), request.headers("Authorization"))) {
+            response.status(401);
+            return "You are not authorized!";
+        }
 
         ExecutionInput input = ExecutionInput.newExecutionInput()
                 .query(graphQLRequest.getQuery())
-                .context(new QLContext(request, locale))
+                .context(new QLContext(request, userId, guildId))
                 .operationName(graphQLRequest.getOperationName())
                 .variables(graphQLRequest.getVariables())
                 .build();
 
         ExecutionResult executionResult = manager.getGraphQL().execute(input);
 
+        response.type("application/json");
         return CascadeBot.getGSON().toJson(executionResult.toSpecification());
     }
 
