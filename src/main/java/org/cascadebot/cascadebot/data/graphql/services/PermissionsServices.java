cf08d4501ccd22eb8b9db1102811be20a7958340
William Oldham <wegg7250@gmail.com>
2019-08-02 16:30:36 +0100

    Add a mutation to check if user has permissions

--- src/main/java/org/cascadebot/cascadebot/data/graphql/services/PermissionsServices.java
+++ src/main/java/org/cascadebot/cascadebot/data/graphql/services/PermissionsServices.java
@@ -1,12 +1,18 @@
 package org.cascadebot.cascadebot.data.graphql.services;
 
+import io.leangen.graphql.annotations.GraphQLContext;
+import io.leangen.graphql.annotations.GraphQLMutation;
 import io.leangen.graphql.annotations.GraphQLNonNull;
 import io.leangen.graphql.annotations.GraphQLQuery;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import net.dv8tion.jda.core.entities.Member;
 import org.cascadebot.cascadebot.CascadeBot;
+import org.cascadebot.cascadebot.data.graphql.objects.QLContext;
+import org.cascadebot.cascadebot.data.objects.GuildData;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
+import org.cascadebot.cascadebot.permissions.objects.Result;
 
 import java.util.Set;
 
@@ -24,6 +30,21 @@ public class PermissionsServices {
     @GraphQLQuery
     public CascadePermission permission(@GraphQLNonNull String permission) {
         return CascadeBot.INS.getPermissionsManager().getPermission(permission);
+    }
+
+    @GraphQLMutation
+    public Result userHasPermission(@GraphQLContext QLContext context, long userId, String permission) {
+        return context.runIfAuthenticated(QLContext.AuthenticationLevel.GUILD, () -> {
+            GuildData guildData = context.getGuildData();
+            if (guildData == null) throw new IllegalStateException("GuildData is null!");
+
+            Member member = context.getGuild().getMemberById(userId);
+
+            CascadePermission cascadePermission = CascadeBot.INS.getPermissionsManager().getPermission(permission);
+            if (cascadePermission == null) throw new IllegalArgumentException("The permission provided does not exist!");
+
+            return guildData.getGuildPermissions().evalPermission(member, cascadePermission, guildData.getCoreSettings());
+        });
     }
 
 }
