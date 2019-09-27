845b4054420b91f648d567b9e26c1538a59ec1d6
James Hawkins <jhawki2005@gmail.com>
2019-09-27 21:43:06 +0100

    woop woop

--- src/main/java/org/cascadebot/cascadebot/commands/music/SeekCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/music/SeekCommand.java
@@ -1,10 +1,14 @@
 package org.cascadebot.cascadebot.commands.music;
 
+import com.ibm.icu.text.MeasureFormat;
+import com.ibm.icu.util.Measure;
+import com.ibm.icu.util.ULocale;
 import net.dv8tion.jda.api.entities.Member;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
+import org.cascadebot.cascadebot.utils.ParserUtils;
 import org.cascadebot.shared.Regex;
 
 import java.util.Arrays;
@@ -14,18 +18,35 @@ public class SeekCommand implements ICommandMain {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        String time = context.getArg(0);
+        String time = context.getMessage(0);
 
-        Pattern pattern = Pattern.compile("(\\d+)(?: ?hours|h)|(\\d+)(?: ?minutes|m)|(\\d+)(?: ?seconds|s)");
-        time = Arrays.toString(pattern.split(time));
-        System.out.println(time);
-
-        if (Long.parseLong(time) < 0) {
-            context.getTypedMessaging().replyDanger("You cannot seek to a negative value!");
+        if (context.getArgs().length == 0) {
+            context.getUIMessaging().replyUsage();
             return;
         }
 
-        context.getMusicPlayer().getPlayer().seekTo(Long.parseLong(time));
+        long millis;
+
+        if (context.getMusicPlayer().getPlayer().getPlayingTrack() == null) {
+            context.getTypedMessaging().replyDanger(context.i18n("commands.seek.no_music_playing"));
+            return;
+        }
+
+        if (context.isArgInteger(0)) {
+            millis = Long.parseLong(time);
+        } else {
+            millis = ParserUtils.parseTime(time, true);
+        }
+
+        if (millis < 0) {
+            context.getTypedMessaging().replyDanger(context.i18n("commands.seek.negative_value"));
+            return;
+        } else if (millis > context.getMusicPlayer().getPlayer().getPlayingTrack().getDuration()) {
+            context.getTypedMessaging().replyDanger(context.i18n("commands.seek.duration_shorter"));
+            return;
+        }
+
+        context.getMusicPlayer().getPlayer().seekTo(millis);
     }
 
     @Override
@@ -40,7 +61,7 @@ public class SeekCommand implements ICommandMain {
 
     @Override
     public CascadePermission getPermission() {
-        return null;
+        return CascadePermission.of("seek", true);
     }
 
 }
--- src/main/java/org/cascadebot/cascadebot/utils/ParserUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/ParserUtils.java
@@ -1,5 +1,45 @@
 package org.cascadebot.cascadebot.utils;
 
+import java.util.concurrent.TimeUnit;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ParserUtils {
+
+    private static final Pattern TIME_PARSER = Pattern.compile("(?<hours>\\d+)h|(?<minutes>\\d+)m|(?<seconds>\\d+)s");
+
+    public static long parseTime(String input, boolean onlyAllowOnce) {
+        Matcher matcher = TIME_PARSER.matcher(input);
+
+        // These booleans represent whether the unit of time has been found yet
+        boolean hoursParsed = false;
+        boolean minutesParsed = false;
+        boolean secondsParsed = false;
+
+        // The result in milliseconds
+        long millis = 0;
+        while (matcher.find()) {
+            if (matcher.group("hours") != null) {
+                if (hoursParsed && onlyAllowOnce) {
+                    throw new IllegalArgumentException("Only one of each time unit from the list: h, m, s is allowed!");
+                }
+                hoursParsed = true;
+                millis += TimeUnit.HOURS.toMillis(Long.parseLong(matcher.group("hours")));
+            } else if (matcher.group("minutes") != null) {
+                if (minutesParsed && onlyAllowOnce) {
+                    throw new IllegalArgumentException("Only one of each time unit from the list: h, m, s is allowed!");
+                }
+                minutesParsed = true;
+                millis += TimeUnit.MINUTES.toMillis(Long.parseLong(matcher.group("minutes")));
+            } else if (matcher.group("seconds") != null) {
+                if (secondsParsed && onlyAllowOnce) {
+                    throw new IllegalArgumentException("Only one of each time unit from the list: h, m, s is allowed!");
+                }
+                secondsParsed = true;
+                millis += TimeUnit.SECONDS.toMillis(Long.parseLong(matcher.group("seconds")));
+            }
+        }
+        return millis;
+    }
 
 }
