0048f43d32682de06e75b9d1aff4168f9d0dca05
weeryan17 <theweeryan17@gmail.com>
2019-09-30 14:50:35 -0500

    Start of commands

--- src/main/java/org/cascadebot/cascadebot/commands/useful/TodoAddSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/useful/TodoAddSubCommand.java
@@ -2,37 +2,29 @@ package org.cascadebot.cascadebot.commands.useful;
 
 import net.dv8tion.jda.api.entities.Member;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
-import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.ISubCommand;
-import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 
-import java.util.Set;
-
-public class TodoCommand implements ICommandMain {
+public class TodoAddSubCommand implements ISubCommand {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
-        context.getUIMessaging().replyUsage();
+        
     }
 
     @Override
     public String command() {
-        return "todo";
-    }
-
-    @Override
-    public Module getModule() {
-        return Module.USEFUL;
+        return "add";
     }
 
     @Override
     public CascadePermission getPermission() {
-        return CascadePermission.of("todo", true);
+        return CascadePermission.of("todo.add", false);
     }
 
     @Override
-    public Set<ISubCommand> getSubCommands() {
-        return Set.of(new TodoCreateSubCommand());
+    public String parent() {
+        return "todo";
     }
+
 }
--- src/main/java/org/cascadebot/cascadebot/commands/useful/TodoCreateSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/useful/TodoCreateSubCommand.java
@@ -3,13 +3,26 @@ package org.cascadebot.cascadebot.commands.useful;
 import net.dv8tion.jda.api.entities.Member;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ISubCommand;
+import org.cascadebot.cascadebot.data.objects.TodoList;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 
 public class TodoCreateSubCommand implements ISubCommand {
 
     @Override
     public void onCommand(Member sender, CommandContext context) {
+        if (context.getArgs().length == 0) {
+            context.getUIMessaging().replyUsage();
+            return;
+        }
 
+        TodoList todoList = context.getData().getGuildSettingUseful().createTodoList(context.getArg(0));
+
+        if (todoList == null) {
+            context.getTypedMessaging().replyDanger("A todo list with that name already exists!");
+            return;
+        }
+
+        context.getTypedMessaging().replySuccess("Created todo list with name `" + todoList.getName() + "`");
     }
 
     @Override
