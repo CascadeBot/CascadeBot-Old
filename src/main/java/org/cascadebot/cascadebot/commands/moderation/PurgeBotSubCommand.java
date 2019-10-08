4a56a404f4de835a463297de2d5bd755cf3c7337
Deadly <info.saddiki@gmail.com>
2019-10-08 06:44:03 +0200

    almost identical to other!

---
+++ src/main/java/org/cascadebot/cascadebot/commands/moderation/PurgeBotSubCommand.java
@@ -0,0 +1,38 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.commands.moderation;
+
+import net.dv8tion.jda.core.entities.*;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ISubCommand;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+import org.cascadebot.cascadebot.utils.PurgeUtils;
+
+public class PurgeBotSubCommand implements ISubCommand {
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        if (context.getArgs().length == 0) {
+            context.getUIMessaging().replyUsage();
+            return;
+        }
+        PurgeUtils.Purge(context, PurgeUtils.Criteria.BOT, context.getArgAsInteger(0), null);
+    }
+
+    @Override
+    public String command() {
+        return "bot";
+    }
+
+    @Override
+    public String parent() { return "purge"; }
+
+    @Override
+    public String description() { return "Deletes messages made by bot accounts"; }
+
+    @Override
+    public CascadePermission getPermission() { return null; }
+
+}
