7eba10a52e5caf30c35213fda472cbeba1e7b06e
Deadly <info.saddiki@gmail.com>
2019-10-11 10:02:09 +0200

    more

--- src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
@@ -1,6 +1,7 @@
 package org.cascadebot.cascadebot.utils;
 
 import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.User;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 
 import java.util.ArrayList;
@@ -8,6 +9,21 @@ import java.util.List;
 
 public class PurgeUtils {
 
+    /**
+     * Criteria for the {@link PurgeUtils#purge} method that checks
+     * which filters you are trying to apply to the message searching
+     *
+     * <ul>
+     *     <li>{@code Attachment - Clears messages with } {@link net.dv8tion.jda.api.entities.Message.Attachment}</li>
+     *     <li>{@code Bot - Clears messages with } {@link User#isBot()} {@code as true}</li>
+     *     <li>{@code Link - Clears any message with a regex checking for links}</li>
+     *     <li>{@code Token - Clears any message that contains x}</li>
+     *     <li>{@code User - Clears any message from a specific } {@link User}</li>
+     *     <li>{@code All - Clears anything}</li>
+     * </ul>
+     * @author DeadlyFirex
+     * @see PurgeUtils#purge
+     */
     public enum Criteria {
         ATTACHMENT,
         BOT,
@@ -17,6 +33,16 @@ public class PurgeUtils {
         ALL,
     }
 
+    /**
+     * Purge method that cleans messages based on the criteria received,
+     * and the amount of messages to clean.
+     *
+     * @param context {@link CommandContext} of the command
+     * @param type {@link PurgeUtils.Criteria} to filter for
+     * @param amount Amount of messages to clear
+     * @param argument Optional argument, made for {@code TOKEN and USER}
+     * @return {@link CommandContext#getTypedMessaging}
+     */
     public static void purge(CommandContext context, Criteria type, int amount, String argument) {
 
         List<Message> messageList = new ArrayList<>();
@@ -25,13 +51,11 @@ public class PurgeUtils {
             if (messageList.size() == amount) {
                 break;
             }
-            
             if (!message.getTimeCreated().isBefore(java.time.OffsetDateTime.now().plusWeeks(2))) {
-                break;
+                context.getTypedMessaging().replyDanger(context.i18n("commands.purge.restriction_time"));
+                return;
             }
-            
             switch (type) {
-                    
                 case ATTACHMENT:
                     if (!message.getAttachments().isEmpty()) {
                         messageList.add(message);
@@ -62,13 +86,11 @@ public class PurgeUtils {
                     break;
             }
         }
-        
         if (messageList.size() <= 1) {
-            context.getTypedMessaging().replyWarning("No messages were purged with this criteria");
+            context.getTypedMessaging().replyWarning(context.i18n("commands.purge.failed_clear"));
             return;
         }
-        
         context.getChannel().deleteMessages(messageList).queue();
-        context.getTypedMessaging().replySuccess("Success! Purged " + messageList.size() + " messages.");
+        context.getTypedMessaging().replySuccess(context.i18n("commands.purge.successfully_done", messageList.size()));
         }
     }
