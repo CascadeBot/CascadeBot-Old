f1a45a2a29bd15d5c412b96fca4f83d60db9d225
Deadly <info.saddiki@gmail.com>
2019-10-12 23:52:11 +0200

    consume the consumers

--- src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
@@ -1,11 +1,13 @@
 package org.cascadebot.cascadebot.utils;
 
+import groovy.transform.Undefined;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.User;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.regex.Pattern;
 
 public class PurgeUtils {
 
@@ -33,6 +35,8 @@ public class PurgeUtils {
         ALL,
     }
 
+    private static final Pattern linkCheck = Pattern.compile("^(?:https?|ftp):\\/\\/[^\\s/$.?#].[^\\s]*$");
+
     /**
      * Purge method that cleans messages based on the criteria received,
      * and the amount of messages to clean.
@@ -43,18 +47,23 @@ public class PurgeUtils {
      * @param argument Optional argument, made for {@code TOKEN and USER}
      * @return {@link CommandContext#getTypedMessaging}
      */
-    public static void purge(CommandContext context, Criteria type, int amount, String argument) {
 
+    public static void purge(CommandContext context, Criteria type, int amount, String argument) {
+        
         List<Message> messageList = new ArrayList<>();
 
         for (Message message : context.getChannel().getIterableHistory()) {
             if (messageList.size() == amount) {
                 break;
             }
-            if (!message.getTimeCreated().isBefore(java.time.OffsetDateTime.now().plusWeeks(2))) {
-                context.getTypedMessaging().replyDanger(context.i18n("commands.purge.restriction_time"));
-                return;
+            if (!message.getTimeCreated().isBefore(java.time.OffsetDateTime.now().minusWeeks(2))) {
+                context.getTypedMessaging().replyWarning(context.i18n("commands.purge.restriction_time"));
+                break;
             }
+            if (!context.getCoreSettings().isPurgePinnedMessages() && message.isPinned()) {
+                    continue;
+            }
+
             switch (type) {
                 case ATTACHMENT:
                     if (!message.getAttachments().isEmpty()) {
@@ -67,7 +76,7 @@ public class PurgeUtils {
                     }
                     break;
                 case LINK:
-                    if (message.getContentRaw().toLowerCase().matches("^(?:https?|ftp):\\/\\/[^\\s/$.?#].[^\\s]*$")) {
+                    if (linkCheck.matcher(message.getContentRaw()).matches()) {
                         messageList.add(message);
                     }
                     break;
@@ -79,18 +88,20 @@ public class PurgeUtils {
                 case USER:
                     if (message.getAuthor().getId().equals(argument)) {
                         messageList.add(message);
-                    }
+                }
                     break;
                 case ALL:
                     messageList.add(message);
                     break;
             }
         }
+
         if (messageList.size() <= 1) {
             context.getTypedMessaging().replyWarning(context.i18n("commands.purge.failed_clear"));
             return;
         }
-        context.getChannel().deleteMessages(messageList).queue();
-        context.getTypedMessaging().replySuccess(context.i18n("commands.purge.successfully_done", messageList.size()));
+        context.getChannel().deleteMessages(messageList).queue($void -> {
+            context.getTypedMessaging().replySuccess(context.i18n("commands.purge.successfully_done", messageList.size()));
+        }, e -> context.getTypedMessaging().replyException(context.i18n("responses.failed_to_run_command"), e));
         }
     }
