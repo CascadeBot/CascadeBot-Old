64e270358e166893cb5b214ad8c440cff4a8fef4
Deadly <info.saddiki@gmail.com>
2019-10-13 22:02:01 +0200

    thy shalt bring thy fear

--- src/main/java/org/cascadebot/cascadebot/commands/moderation/PurgeUserSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/moderation/PurgeUserSubCommand.java
@@ -6,7 +6,8 @@
 package org.cascadebot.cascadebot.commands.moderation;
 
 import net.dv8tion.jda.api.entities.Member;
-import org.cascadebot.cascadebot.commandmeta.*;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ISubCommand;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 import org.cascadebot.cascadebot.utils.DiscordUtils;
 import org.cascadebot.cascadebot.utils.PurgeUtils;
