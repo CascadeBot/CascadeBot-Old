079848e0ae29e432045f808e62f250e74f9f741a
Deadly <info.saddiki@gmail.com>
2019-10-15 11:05:28 +0200

    smh again

--- src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/PurgeUtils.java
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+
 package org.cascadebot.cascadebot.utils;
 
 import net.dv8tion.jda.api.entities.Message;
