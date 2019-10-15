6866e19d1794d6bd4b12a6746a3cb4296232805b
Deadly <info.saddiki@gmail.com>
2019-10-15 11:22:51 +0200

    remove the imports

--- src/main/java/org/cascadebot/cascadebot/commands/fun/ColorCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/ColorCommand.java
@@ -6,22 +6,12 @@
 
 package org.cascadebot.cascadebot.commands.fun;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
-import org.cascadebot.cascadebot.commandmeta.Argument;
-import org.cascadebot.cascadebot.commandmeta.ArgumentType;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
-import org.cascadebot.cascadebot.messaging.MessageType;
-import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 import org.cascadebot.cascadebot.utils.ColorUtils;
-
-import java.awt.Color;
-import java.util.Set;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class ColorCommand implements ICommandMain {
 
