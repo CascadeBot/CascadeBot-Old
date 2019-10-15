cbad79b097bcdc6d300a0ebb4ed24b2bcb169f04
Deadly <info.saddiki@gmail.com>
2019-10-15 11:23:44 +0200

    TAKE DOWN JENKINS

--- src/main/java/org/cascadebot/cascadebot/commands/fun/RandomColorSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/RandomColorSubCommand.java
@@ -6,21 +6,13 @@
 
 package org.cascadebot.cascadebot.commands.fun;
 
-import net.dv8tion.jda.api.EmbedBuilder;
 import net.dv8tion.jda.api.entities.Member;
 
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
-import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.ISubCommand;
-import org.cascadebot.cascadebot.commandmeta.Module;
-import org.cascadebot.cascadebot.messaging.MessageType;
-import org.cascadebot.cascadebot.messaging.MessagingObjects;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
 import org.cascadebot.cascadebot.utils.ColorUtils;
 import org.cascadebot.cascadebot.utils.RandomUtils;
-
-import java.awt.*;
-import java.util.Set;
 
 public class RandomColorSubCommand implements ISubCommand {
 
