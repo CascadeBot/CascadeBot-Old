815cc0f29c8fb373d8348b9be806d61b647ae11a
William Oldham <wegg7250@gmail.com>
2019-11-09 12:27:46 +0000

    Add more time parsing
    Add the ability to parse:
     - Single number as seconds
     - Digital time in either `mm:ss` or `hh:mm:ss`
     - Time in the format `_h _m _s`

--- src/main/java/org/cascadebot/cascadebot/utils/ParserUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/ParserUtils.java
@@ -6,10 +6,23 @@ import java.util.regex.Pattern;
 
 public class ParserUtils {
 
-    private static final Pattern TIME_PARSER = Pattern.compile("(?<hours>\\d+)h|(?<minutes>\\d+)m|(?<seconds>\\d+)s");
+    private static final Pattern TEXT_TIME_PARSER = Pattern.compile("(?<hours>\\d+)h|(?<minutes>\\d+)m|(?<seconds>\\d+)s");
+    private static final Pattern DIGITAL_TIME_REGEX = Pattern.compile("(\\d+):(\\d+)(?::(\\d+))?");
 
-    public static long parseTime(String input, boolean onlyAllowOnce) {
-        Matcher matcher = TIME_PARSER.matcher(input);
+    public static long parseTime(String input) {
+        try {
+            return Long.parseLong(input) * 1000;
+        } catch (NumberFormatException e) {
+            if (DIGITAL_TIME_REGEX.matcher(input).matches()) {
+                return parseDigitalTime(input);
+            } else {
+                return parseTextTime(input, true);
+            }
+        }
+    }
+
+    public static long parseTextTime(String input, boolean onlyAllowOnce) {
+        Matcher matcher = TEXT_TIME_PARSER.matcher(input);
 
         // These booleans represent whether the unit of time has been found yet
         boolean hoursParsed = false;
@@ -41,5 +54,22 @@ public class ParserUtils {
         }
         return millis;
     }
+
+    public static long parseDigitalTime(String input) {
+        Matcher matcher = DIGITAL_TIME_REGEX.matcher(input);
+        if (matcher.matches()) {
+            if (matcher.group(3) == null) {
+                return TimeUnit.MINUTES.toMillis(Long.parseLong(matcher.group(1))) +
+                        TimeUnit.SECONDS.toMillis(Long.parseLong(matcher.group(2)));
+            } else {
+                return TimeUnit.HOURS.toMillis(Long.parseLong(matcher.group(1))) +
+                        TimeUnit.MINUTES.toMillis(Long.parseLong(matcher.group(2))) +
+                        TimeUnit.SECONDS.toMillis(Long.parseLong(matcher.group(3)));
+            }
+        } else {
+            return 0;
+        }
+    }
+
 
 }
