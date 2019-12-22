072f4263eb405f874ecaaf94680b23a1d701b90b
James Hawkins <jhawki2005@gmail.com>
2019-12-22 15:37:08 +0000

    Review

--- src/main/java/org/cascadebot/cascadebot/commands/fun/JokeCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/fun/JokeCommand.java
@@ -1,12 +1,16 @@
+/*
+ * Copyright (c) 2019 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
 package org.cascadebot.cascadebot.commands.fun;
 
-import com.google.gson.JsonElement;
 import net.dv8tion.jda.api.entities.Member;
 import org.cascadebot.cascadebot.commandmeta.CommandContext;
 import org.cascadebot.cascadebot.commandmeta.ICommandMain;
 import org.cascadebot.cascadebot.commandmeta.Module;
 import org.cascadebot.cascadebot.permissions.CascadePermission;
-import org.cascadebot.cascadebot.utils.WebUtils;
+import org.cascadebot.cascadebot.utils.RandomUtils;
 
 import java.io.IOException;
 
@@ -15,15 +19,10 @@ public class JokeCommand implements ICommandMain {
     @Override
     public void onCommand(Member sender, CommandContext context) {
         try {
-            context.getTypedMessaging().replyInfo(getJoke());
+            context.getTypedMessaging().replyInfo(RandomUtils.randomJoke());
         } catch (IOException e) {
             context.getTypedMessaging().replyDanger(context.i18n("commands.joke.error_loading"));
-        };
-    }
-
-    private String getJoke() throws IOException {
-        JsonElement jsonElement = WebUtils.getJsonFromURL("https://icanhazdadjoke.com/");
-        return jsonElement.getAsJsonObject().get("joke").getAsString();
+        }
     }
 
     @Override
