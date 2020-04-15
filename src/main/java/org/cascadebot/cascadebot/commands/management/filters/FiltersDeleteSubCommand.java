f8c11d27988a2fda26dcfbf7ad422abe33d2d2b7
William Oldham <wegg7250@gmail.com>
2020-04-15 23:02:17 +0100

    Filter delete command initial

--- src/main/java/org/cascadebot/cascadebot/commands/management/filters/FiltersDeleteSubCommand.java
+++ src/main/java/org/cascadebot/cascadebot/commands/management/filters/FiltersDeleteSubCommand.java
@@ -1,5 +1,33 @@
 package org.cascadebot.cascadebot.commands.management.filters;
 
-public class FiltersDeleteCommand {
+import net.dv8tion.jda.api.entities.Member;
+import org.cascadebot.cascadebot.commandmeta.CommandContext;
+import org.cascadebot.cascadebot.commandmeta.ISubCommand;
+import org.cascadebot.cascadebot.permissions.CascadePermission;
+
+public class FiltersDeleteSubCommand implements ISubCommand {
+
+    @Override
+    public void onCommand(Member sender, CommandContext context) {
+        if (context.getArgs().length != 1) {
+            context.getUIMessaging().replyUsage();
+            return;
+        }
+    }
+
+    @Override
+    public String command() {
+        return "delete";
+    }
+
+    @Override
+    public String parent() {
+        return "filters";
+    }
+
+    @Override
+    public CascadePermission getPermission() {
+        return null;
+    }
 
 }
