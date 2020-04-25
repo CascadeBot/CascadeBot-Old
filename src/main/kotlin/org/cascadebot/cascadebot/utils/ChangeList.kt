a6f33f72399dc395c547cd26e7f32526e1d04eb9
William Oldham <wegg7250@gmail.com>
2020-04-25 15:20:43 +0100

    Add a change list to maintain changes without modifying base

---
+++ src/main/kotlin/org/cascadebot/cascadebot/utils/ChangeList.kt
@@ -0,0 +1,36 @@
+package org.cascadebot.cascadebot.utils
+
+import java.util.Collections
+
+class ChangeList<T>(val baseCollection: Set<T>) {
+
+    val addedItems
+        get() = added.toList()
+    val removedItems
+        get() = removed.toList()
+
+    private val added = Collections.synchronizedSet<T>(HashSet())
+    private val removed = Collections.synchronizedSet<T>(HashSet())
+
+    fun add(element: T): Boolean {
+        if (!added.remove(element)) {
+            removed.add(element)
+        }
+        return !baseCollection.contains(element)
+    }
+
+    fun remove(element: T): Boolean {
+        if (!removed.remove(element)) {
+            added.add(element)
+        }
+        return baseCollection.contains(element)
+    }
+
+    fun applyChanges(): Set<T> {
+        val baseElements = baseCollection.toMutableSet()
+        baseElements.addAll(added)
+        baseElements.removeAll(removed)
+        return baseElements
+    }
+
+}
