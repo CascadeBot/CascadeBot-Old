f2d1fb12f0a94e9112712649c39c64515a1b0a98
William Oldham <wegg7250@gmail.com>
2020-04-28 15:20:19 +0100

    Add unit tests for change list

---
+++ src/test/kotlin/org/cascadebot/cascadebot/utils/ChangeListTest.kt
@@ -0,0 +1,48 @@
+package org.cascadebot.cascadebot.utils
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Assertions.assertFalse
+import org.junit.jupiter.api.Assertions.assertTrue
+import org.junit.jupiter.api.Test
+
+
+class ChangeListTest {
+
+    @Test
+    fun `Remove removes from added list`() {
+        val changeList = ChangeList<String>(mutableSetOf())
+        changeList.add("element")
+        changeList.remove("element")
+
+        assertTrue(changeList.addedItems.isEmpty())
+        assertTrue(changeList.removedItems.isEmpty())
+    }
+
+    @Test
+    fun `Add adds to removed list`() {
+        val changeList = ChangeList<String>(mutableSetOf())
+        changeList.remove("element")
+        changeList.add("element")
+
+        assertTrue(changeList.addedItems.isEmpty())
+        assertTrue(changeList.removedItems.isEmpty())
+    }
+
+    @Test
+    fun `Remove from base collection`() {
+        val changeList = ChangeList(mutableSetOf("element", "different"))
+        changeList.remove("element")
+        assertTrue(changeList.removedItems.contains("element"))
+        assertEquals(changeList.applyChanges(), setOf("different"))
+    }
+
+    @Test
+    fun `Add to base collection`() {
+        val changeList = ChangeList(mutableSetOf("different"))
+        changeList.add("element")
+        assertTrue(changeList.addedItems.contains("element"))
+        assertEquals(changeList.applyChanges(), setOf("element", "different"))
+    }
+
+
+}
