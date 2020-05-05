f67e8f1158405af4b776dbccbc0b8ad0f0dda3d4
William Oldham <wegg7250@gmail.com>
2020-05-05 18:02:43 +0100

    Change general event listener to kotlin and add join and leave events

---
+++ src/main/kotlin/org/cascadebot/cascadebot/events/GeneralEventListener.kt
@@ -0,0 +1,98 @@
+package org.cascadebot.cascadebot.events
+
+import net.dv8tion.jda.api.JDA
+import net.dv8tion.jda.api.events.ReadyEvent
+import net.dv8tion.jda.api.events.StatusChangeEvent
+import net.dv8tion.jda.api.events.guild.GuildJoinEvent
+import net.dv8tion.jda.api.events.guild.GuildLeaveEvent
+import net.dv8tion.jda.api.events.guild.member.GuildMemberJoinEvent
+import net.dv8tion.jda.api.events.guild.member.GuildMemberLeaveEvent
+import net.dv8tion.jda.api.events.role.RoleDeleteEvent
+import net.dv8tion.jda.api.hooks.ListenerAdapter
+import org.apache.commons.lang3.StringUtils
+import org.cascadebot.cascadebot.CascadeBot
+import org.cascadebot.cascadebot.UnicodeConstants
+import org.cascadebot.cascadebot.data.Config
+import org.cascadebot.cascadebot.data.managers.GuildDataManager
+import org.cascadebot.cascadebot.messaging.MessageType
+import org.cascadebot.cascadebot.utils.FormatUtils
+
+class GeneralEventListener : ListenerAdapter() {
+
+    override fun onReady(event: ReadyEvent) {
+        val shardManager = CascadeBot.INS.shardManager
+        if (shardManager.shards.size == shardManager.shardsTotal) {
+            CascadeBot.INS.run()
+            Config.INS.eventWebhook.send(
+                    MessageType.SUCCESS.emoji + " All shards ready!"
+            )
+        }
+    }
+
+    override fun onStatusChange(event: StatusChangeEvent) {
+        @Suppress("NON_EXHAUSTIVE_WHEN")
+        when (event.newStatus) {
+            JDA.Status.CONNECTED,
+            JDA.Status.DISCONNECTED,
+            JDA.Status.RECONNECT_QUEUED,
+            JDA.Status.ATTEMPTING_TO_RECONNECT,
+            JDA.Status.SHUTTING_DOWN,
+            JDA.Status.SHUTDOWN,
+            JDA.Status.FAILED_TO_LOGIN -> Config.INS.eventWebhook.send(String.format(
+                    UnicodeConstants.ROBOT + " Status Update: `%s` to `%s` on shard: `%d`",
+                    FormatUtils.formatEnum(event.oldStatus),
+                    FormatUtils.formatEnum(event.newStatus),
+                    event.jda.shardInfo.shardId
+            ))
+        }
+    }
+
+    override fun onGuildJoin(event: GuildJoinEvent) {
+        if (!StringUtils.isBlank(Config.INS.guildWelcomeMessage)) {
+            event.guild.owner!!
+                    .user
+                    .openPrivateChannel()
+                    .queue({ it.sendMessage(Config.INS.guildWelcomeMessage).queue() }, { /* Do nothing on error! */ })
+        }
+    }
+
+    override fun onGuildLeave(event: GuildLeaveEvent) {
+        CascadeBot.INS.musicHandler.removePlayer(event.guild.idLong)
+        if (!StringUtils.isBlank(Config.INS.guildGoodbyeMessage)) {
+            event.guild.owner!!
+                    .user
+                    .openPrivateChannel()
+                    .queue({ it.sendMessage(Config.INS.guildGoodbyeMessage).queue() }, { /* Do nothing on error! */ })
+        }
+    }
+
+    override fun onGuildMemberJoin(event: GuildMemberJoinEvent) {
+        val guildData = GuildDataManager.getGuildData(event.guild.idLong)
+        val greetings = guildData.management.greetings
+        if (greetings.welcomeEnabled) {
+            greetings.welcomeChannel?.let {
+                // .randomItem should only return null if there are no messages so it can be ignored
+                // TODO Formatting
+                greetings.welcomeMessages.randomItem?.let { message -> it.sendMessage(message) }
+            } ?: run { greetings.welcomeChannel = null }
+        }
+    }
+
+    override fun onGuildMemberLeave(event: GuildMemberLeaveEvent) {
+        val guildData = GuildDataManager.getGuildData(event.guild.idLong)
+        val greetings = guildData.management.greetings
+        if (greetings.goodbyeEnabled) {
+            greetings.goodbyeChannel?.let {
+                // .randomItem should only return null if there are no messages so it can be ignored
+                // TODO Formatting
+                greetings.goodbyeMessages.randomItem?.let { message -> it.sendMessage(message) }
+            } ?: run { greetings.goodbyeChannel = null }
+        }
+    }
+
+    override fun onRoleDelete(event: RoleDeleteEvent) {
+        for (group in GuildDataManager.getGuildData(event.guild.idLong).management.permissions.groups) {
+            group.unlinkRole(event.role.idLong)
+        }
+    }
+}
