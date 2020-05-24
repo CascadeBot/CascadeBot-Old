9504a892ac934b28d77e78b21918cf78215e4134
William Oldham <wegg7250@gmail.com>
2020-05-24 12:30:17 +0100

    Welcome channel language

--- src/main/kotlin/org/cascadebot/cascadebot/commands/management/WelcomeChannelSubCommand.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/commands/management/WelcomeChannelSubCommand.kt
@@ -8,8 +8,6 @@ package org.cascadebot.cascadebot.commands.management
 import net.dv8tion.jda.api.entities.Member
 import org.cascadebot.cascadebot.commandmeta.CommandContext
 import org.cascadebot.cascadebot.commandmeta.SubCommand
-import org.cascadebot.cascadebot.messaging.MessageType
-import org.cascadebot.cascadebot.messaging.embed
 import org.cascadebot.cascadebot.permissions.CascadePermission
 import org.cascadebot.cascadebot.utils.DiscordUtils
 
@@ -22,32 +20,30 @@ class WelcomeChannelSubCommand : SubCommand() {
         }
 
         if (context.args.isEmpty()) {
-            context.typedMessaging.replyInfo(embed(MessageType.INFO, context.user) {
-                description = run {
-                    val greetings = context.data.management.greetings
-                    if (greetings.welcomeChannel != null) {
-                        "The current welcome channel is: ${greetings.welcomeChannel?.asMention}"
-                    } else {
-                        "There is no welcome channel set!"
-                    }
+            context.typedMessaging.replyInfo(run {
+                val greetings = context.data.management.greetings
+                if (greetings.welcomeChannel != null) {
+                    context.i18n("commands.welcome.current_channel", greetings.welcomeChannel?.asMention!!)
+                } else {
+                    context.i18n("commands.welcome.no_channel_set")
                 }
             })
         } else {
-            if (context.getArg(0).equals("clear", true)) {
+            if (context.getArg(0).equals(context.i18n("arguments.welcome#channel#clear.name"), true)) {
                 context.data.management.greetings.welcomeChannel = null
-                context.typedMessaging.replySuccess("Successfully cleared the welcome channel!")
+                context.typedMessaging.replySuccess(context.i18n("commands.welcome.channel.clear_success"))
                 return
             }
 
             val channel = DiscordUtils.getTextChannel(context.guild, context.getArg(0))
             if (channel == null) {
-                context.typedMessaging.replyDanger("Please specify a valid channel!")
+                context.typedMessaging.replyDanger(context.i18n("commands.welcome.channel.invalid_channel"))
                 return
             }
 
             context.data.management.greetings.welcomeChannel = channel
 
-            context.typedMessaging.replySuccess("Successfully set the welcome channel to ${channel.asMention}")
+            context.typedMessaging.replySuccess(context.i18n("commands.welcome.channel.set_success", channel.asMention))
         }
 
     }
