8146dc19a119a0292dd742472c8a6b514c64e1b0
William Oldham <wegg7250@gmail.com>
2020-06-13 00:09:22 +0100

    Make FiltersCreate work

--- src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersCreateSubCommand.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersCreateSubCommand.kt
@@ -13,21 +13,18 @@ import org.cascadebot.cascadebot.data.objects.CommandFilter
 import org.cascadebot.cascadebot.data.objects.CommandFilter.FilterType
 import org.cascadebot.cascadebot.permissions.CascadePermission
 import org.cascadebot.cascadebot.utils.FormatUtils
-import java.util.Arrays
-import java.util.function.Function
-import java.util.stream.Collectors
 
 class FiltersCreateSubCommand : SubCommand() {
 
     override fun onCommand(sender: Member, context: CommandContext) {
-        if (context.args.length == 0 || context.args.length > 2) {
+        if (context.args.isEmpty() || context.args.size > 2) {
             context.uiMessaging.replyUsage()
             return
         }
 
         val name = context.getArg(0)
 
-        if (context.data.getCoreSettings().hasCommandFilter(name)) {
+        if (context.data.management.filters.any { it.name == name }) {
             context.typedMessaging.replyDanger(context.i18n(
                     "commands.filters.create.already_exists",
                     name
@@ -38,9 +35,9 @@ class FiltersCreateSubCommand : SubCommand() {
         // Args in the format "<name> [type]"
 
         // Args in the format "<name> [type]"
-        if (context.args.length == 1) {
+        if (context.args.size == 1) {
             val filter = CommandFilter(name)
-            context.data.getCoreSettings().addCommandFilter(filter)
+            context.data.management.filters.add(filter)
             context.typedMessaging.replySuccess(context.i18n(
                     "commands.filters.create.created_filter",
                     FormatUtils.formatEnum(filter.type, context.locale),
@@ -51,7 +48,7 @@ class FiltersCreateSubCommand : SubCommand() {
             if (EnumUtils.isValidEnumIgnoreCase(FilterType::class.java, type)) {
                 val filter = CommandFilter(name)
                 filter.type = EnumUtils.getEnumIgnoreCase(FilterType::class.java, type)
-                context.data.core.addCommandFilter(filter)
+                context.data.management.filters.add(filter)
                 context.typedMessaging.replySuccess(context.i18n(
                         "commands.filters.create.created_filter",
                         FormatUtils.formatEnum(filter.type, context.locale),
@@ -61,9 +58,7 @@ class FiltersCreateSubCommand : SubCommand() {
                 context.typedMessaging.replyDanger(context.i18n(
                         "commands.filters.create.type_invalid",
                         type,
-                        Arrays.stream(FilterType.values())
-                                .map<String>(Function { filterType: FilterType -> "`" + FormatUtils.formatEnum(filterType, context.locale) + "`" })
-                                .collect(Collectors.joining(", "))
+                        FilterType.values().joinToString(", ") { filterType -> "`" + FormatUtils.formatEnum(filterType, context.locale) + "`" }
                 ))
             }
         }
