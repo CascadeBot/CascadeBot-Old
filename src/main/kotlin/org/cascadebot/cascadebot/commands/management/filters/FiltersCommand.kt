0ca22bcdbe9998a2ffcd87875e0eb6c02750a9a2
William Oldham <wegg7250@gmail.com>
2020-06-14 16:16:57 +0100

    Add subcommands to filters

--- src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersCommand.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersCommand.kt
@@ -9,6 +9,7 @@ import net.dv8tion.jda.api.entities.Member
 import org.cascadebot.cascadebot.commandmeta.CommandContext
 import org.cascadebot.cascadebot.commandmeta.MainCommand
 import org.cascadebot.cascadebot.commandmeta.Module
+import org.cascadebot.cascadebot.commandmeta.SubCommand
 import org.cascadebot.cascadebot.permissions.CascadePermission
 
 class FiltersCommand : MainCommand() {
@@ -20,6 +21,13 @@ class FiltersCommand : MainCommand() {
     override fun command(): String = "filters"
 
     override fun module(): Module = Module.MANAGEMENT
+
+    override fun subCommands(): Set<SubCommand> = setOf(
+            FiltersCreateSubCommand(),
+            FiltersDeleteSubCommand(),
+            FiltersEnableSubCommand(),
+            FiltersDisableSubCommand()
+    )
 
     override fun permission(): CascadePermission? = null
 
