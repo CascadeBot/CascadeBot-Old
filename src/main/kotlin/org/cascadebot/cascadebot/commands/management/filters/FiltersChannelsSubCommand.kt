63c71d62b1296167829b233a94ed1dcef896c0e4
William Oldham <wegg7250@gmail.com>
2020-06-21 20:41:05 +0100

    Add link, unlink and list to filter channels

--- src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersChannelsSubCommand.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/commands/management/filters/FiltersChannelsSubCommand.kt
@@ -9,6 +9,8 @@ import net.dv8tion.jda.api.entities.Member
 import net.dv8tion.jda.api.entities.TextChannel
 import org.cascadebot.cascadebot.commandmeta.CommandContext
 import org.cascadebot.cascadebot.commandmeta.SubCommand
+import org.cascadebot.cascadebot.messaging.MessageType
+import org.cascadebot.cascadebot.messaging.embed
 import org.cascadebot.cascadebot.permissions.CascadePermission
 import org.cascadebot.cascadebot.utils.DiscordUtils
 
@@ -21,7 +23,7 @@ class FiltersChannelsSubCommand : SubCommand() {
         }
 
         when {
-            context.testForArg("add") -> {
+            context.testForArg("link") -> {
                 val args = context.args.copyOfRange(1, context.args.size)
 
                 // <filter> <channel>
@@ -45,14 +47,68 @@ class FiltersChannelsSubCommand : SubCommand() {
                     return
                 }
 
-                filter.channelIds.add(channel.idLong)
-                context.typedMessaging.replySuccess(context.i18n("commands.filters.channels.add.success", channel.asMention))
+                if (filter.channelIds.add(channel.idLong)) {
+                    context.typedMessaging.replySuccess(context.i18n("commands.filters.channels.link.success", channel.asMention, filterName))
+                } else {
+                    context.typedMessaging.replyInfo(context.i18n("commands.filters.channels.link.already_exists", channel.asMention, filterName))
+                }
             }
-            context.testForArg("remove") -> {
+            context.testForArg("unlink") -> {
+                val args = context.args.copyOfRange(1, context.args.size)
 
+                // <filter> <channel>
+                if (args.size < 2) {
+                    context.uiMessaging.replyUsage()
+                    return
+                }
+
+                val filterName = args[0]
+                val filter = context.data.management.filters.find { it.name == filterName }
+
+                if (filter == null) {
+                    context.typedMessaging.replyDanger(context.i18n("commands.filters.doesnt_exist", filterName))
+                    return
+                }
+
+                val channel: TextChannel? = DiscordUtils.getTextChannel(context.guild, args[1])
+
+                if (channel == null) {
+                    context.typedMessaging.replyDanger(context.i18n("commands.filters.channels.invalid_channel", args[1]))
+                    return
+                }
+
+                if (filter.channelIds.remove(channel.idLong)) {
+                    context.typedMessaging.replySuccess(context.i18n("commands.filters.channels.unlink.success", channel.asMention, filterName))
+                } else {
+                    context.typedMessaging.replyDanger(context.i18n("commands.filters.channels.unlink.didnt_exist", channel.asMention, filterName))
+                }
             }
+
             context.testForArg("list") -> {
+                val args = context.args.copyOfRange(1, context.args.size)
 
+                if (args.isEmpty()) {
+                    context.uiMessaging.replyUsage()
+                    return
+                }
+
+                val filterName = args[0]
+                val filter = context.data.management.filters.find { it.name == filterName }
+
+                if (filter == null) {
+                    context.typedMessaging.replyDanger(context.i18n("commands.filters.doesnt_exist", filterName))
+                    return
+                }
+
+                context.typedMessaging.replyInfo(embed(MessageType.INFO) {
+                    title {
+                        name = context.i18n("commands.filters.channels.list.embed_title", filterName)
+                    }
+                    description = filter.channelIds
+                            .mapNotNull { context.guild.getTextChannelById(it)?.asMention }
+                            .joinToString("\n")
+                            .ifBlank { context.i18n("commands.filters.channels.list.no_filters") }
+                })
             }
             else -> {
                 context.uiMessaging.replyUsage()
