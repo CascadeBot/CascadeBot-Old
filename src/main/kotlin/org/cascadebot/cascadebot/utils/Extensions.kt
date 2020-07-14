98b02f5d131f8ac82f5d80c83899ae8f2d447886
William Oldham <wegg7250@gmail.com>
2020-07-14 21:45:01 +0100

    Add muted role extension for muting

--- src/main/kotlin/org/cascadebot/cascadebot/utils/Extensions.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/utils/Extensions.kt
@@ -5,6 +5,9 @@
 
 package org.cascadebot.cascadebot.utils
 
+import net.dv8tion.jda.api.entities.Guild
+import net.dv8tion.jda.api.entities.Role
+import org.cascadebot.cascadebot.data.managers.GuildDataManager
 import kotlin.math.pow
 import kotlin.math.round
 import kotlin.math.roundToInt
@@ -19,4 +22,14 @@ fun String.truncate(length: Int, ellipses: Boolean = true): String =
 
 fun Double.toPercentage(dp: Int = 0): String {
     return (round((this * 100) * 10.0.pow(dp)) / 10.0.pow(dp)).roundToInt().toString() + "%"
+}
+
+fun Guild.getMutedRole(): Role {
+    val muteRoleName = GuildDataManager.getGuildData(this.idLong).moderation.muteRoleName
+    val roleByName = this.getRolesByName(muteRoleName, true)
+    return if (roleByName.isEmpty()) {
+        this.createRole().setName(muteRoleName).complete()
+    } else {
+        roleByName[0]
+    }
 }
