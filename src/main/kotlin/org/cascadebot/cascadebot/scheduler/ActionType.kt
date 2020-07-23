04d214d5aaf41d42febd5246561f65f9ba066158
William Oldham <wegg7250@gmail.com>
2020-07-23 23:21:21 +0100

    Revert "GOOD ENOUGH"
    This reverts commit c985c967f4d57296b1b5621ac65d77a5955bba56.

--- src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
@@ -6,9 +6,7 @@ import kotlin.reflect.full.isSubclassOf
 enum class ActionType(val expectedClass: KClass<*>, val dataConsumer: (ScheduledAction) -> Unit) {
     UNMUTE(ScheduledAction.ModerationActionData::class, { action -> TODO() }),
     UNBAN(ScheduledAction.ModerationActionData::class, { action -> TODO() }),
-    REMINDER(ScheduledAction.ReminderActionData::class, { action ->
-        kotlin.io.println("HIIII")
-    });
+    REMINDER(ScheduledAction.ReminderActionData::class, { action -> TODO() });
 
     fun verifyDataType(data: ScheduledAction.ActionData) = data::class.isSubclassOf(expectedClass)
 }
