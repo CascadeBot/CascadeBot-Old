f559a27cd927db758f90f7aab0e460366bab383d
William Oldham <wegg7250@gmail.com>
2020-07-23 23:22:05 +0100

    GOOD ENOUGH

--- src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
@@ -6,7 +6,9 @@ import kotlin.reflect.full.isSubclassOf
 enum class ActionType(val expectedClass: KClass<*>, val dataConsumer: (ScheduledAction) -> Unit) {
     UNMUTE(ScheduledAction.ModerationActionData::class, { action -> TODO() }),
     UNBAN(ScheduledAction.ModerationActionData::class, { action -> TODO() }),
-    REMINDER(ScheduledAction.ReminderActionData::class, { action -> TODO() });
+    REMINDER(ScheduledAction.ReminderActionData::class, { action ->
+        kotlin.io.println("HIIII")
+    });
 
     fun verifyDataType(data: ScheduledAction.ActionData) = data::class.isSubclassOf(expectedClass)
 }
