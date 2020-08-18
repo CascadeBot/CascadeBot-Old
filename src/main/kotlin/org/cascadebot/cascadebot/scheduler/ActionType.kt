b6777028924acffb3aacd9cc1882e3995f2af520
William Oldham <wegg7250@gmail.com>
2020-08-18 21:16:44 +0100

    Add reasoning to unmute and unban scheduled actions

--- src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/scheduler/ActionType.kt
@@ -1,6 +1,9 @@
 package org.cascadebot.cascadebot.scheduler
 
+import org.cascadebot.cascadebot.CascadeBot
+import org.cascadebot.cascadebot.data.language.Language
 import org.cascadebot.cascadebot.utils.getMutedRole
+import org.cascadebot.cascadebot.utils.toCapitalized
 import kotlin.reflect.KClass
 import kotlin.reflect.full.isSubclassOf
 
@@ -10,14 +13,25 @@ enum class ActionType(val expectedClass: KClass<*>, val dataConsumer: (Scheduled
             action.guild?.let { guild ->
                 val member = guild.getMemberById(action.data.targetId)
                 if (member != null) {
-                    guild.removeRoleFromMember(member, guild.getMutedRole()).reason("Temp mute: Unmuting").queue()
+                    val restAction = guild.removeRoleFromMember(member, guild.getMutedRole())
+                    val userName = guild.getMemberById(action.data.targetId)?.user?.asTag ?: Language.getGuildLocale(guild.idLong).i18n("words.unknown").toCapitalized()
+                    restAction.reason(Language.getGuildLocale(guild.idLong).i18n("mod_actions.temp_mute.unmute_reason", userName))
+                    // TODO handle errors
+                    restAction.queue()
                 }
             }
         }
     }),
     UNBAN(ScheduledAction.ModerationActionData::class, { action ->
         if (action.data is ScheduledAction.ModerationActionData) {
-            action.guild?.unban(action.data.targetId.toString())?.reason("Temp ban: Unbanning user")?.queue()
+            action.guild?.let { guild ->
+                val restAction = guild.unban(action.data.targetId.toString())
+                val userName = CascadeBot.INS.shardManager.getUserById(action.data.targetId)
+                        ?: Language.getGuildLocale(guild.idLong).i18n("words.unknown").toCapitalized()
+                restAction.reason(Language.getGuildLocale(guild.idLong).i18n("mod_actions.temp_mute.unmute_reason", userName))
+                // TODO handle errors
+                restAction.queue()
+            }
         }
     }),
     REMINDER(ScheduledAction.ReminderActionData::class, { action -> TODO() });
