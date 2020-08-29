b42cc9993f1e69ad58ac369e78bf8a8a87e096f9
Ikinon <36565735+Ikinon@users.noreply.github.com>
2020-08-29 14:55:53 +0100

    update slowmode reset

--- src/main/kotlin/org/cascadebot/cascadebot/commands/moderation/SlowmodeResetSubCommand.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/commands/moderation/SlowmodeResetSubCommand.kt
@@ -13,29 +13,35 @@ class SlowmodeResetSubCommand : SubCommand() {
 
     override fun onCommand(sender: Member, context: CommandContext) {
         var channel: TextChannel = context.channel
-        if (!context.args.isEmpty()) {
+        if (context.args.isNotEmpty()) {
             val tempChannel = DiscordUtils.getTextChannel(context.guild, context.getArg(0))
             if (tempChannel != null) {
                 channel = tempChannel
             } else {
                 context.typedMessaging.replyDanger(context.i18n("responses.cannot_find_channel_matching", context.getArg(0)))
-                return
             }
+        }
+        if (!context.member.hasPermission(channel, Permission.MANAGE_CHANNEL)) {
+            context.uiMessaging.sendUserDiscordPermError(Permission.MANAGE_CHANNEL)
+            return
         }
         if (!context.selfMember.hasPermission(channel, Permission.MANAGE_CHANNEL)) {
             context.uiMessaging.sendBotDiscordPermError(Permission.MANAGE_CHANNEL)
             return
         }
+
         // Slowmode 0 = off
-        channel.manager.setSlowmode(0).queue({
-            context.typedMessaging.replySuccess(context.i18n("commands.slowmode.reset.reset_success", channel.name))
-        }, {
-            if (it is PermissionException) {
-                context.uiMessaging.sendBotDiscordPermError(it.permission)
-            } else {
-                context.typedMessaging.replyException("Couldn't change channel permissions", it)
+        try {
+            channel.manager.setSlowmode(0).queue {
+                context.typedMessaging.replySuccess(context.i18n("commands.slowmode.reset.reset_success", channel.name))
             }
-        })
+        } catch (e: PermissionException) {
+            context.uiMessaging.sendBotDiscordPermError(e.permission)
+        } catch (e: Exception) {
+            context.typedMessaging.replyException("Couldn't change channel permissions", e)
+        }
+
+
     }
 
     override fun parent(): String = "slowmode"
