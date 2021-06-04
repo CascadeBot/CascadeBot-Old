f1dc9d6434f8c95378a8e607ad5a8570a67df9df
William Oldham <wegg7250@gmail.com>
2021-06-04 11:08:12 +0100

    Test change

--- src/main/java/org/cascadebot/cascadebot/utils/GuildDataUtils.java
+++ src/main/java/org/cascadebot/cascadebot/utils/GuildDataUtils.java
@@ -1,15 +1,12 @@
 package org.cascadebot.cascadebot.utils;
 
+import org.cascadebot.cascadebot.data.objects.GuildData;
 import org.slf4j.MDC;
 
 public class GuildDataUtils {
 
     public static void assertWriteMode() {
-        String mode = MDC.get("writeMode");
-        if (mode == null) {
-            throw new UnsupportedOperationException("Cannot write guild data if not in write mode!");
-        }
-        boolean writeMode = Boolean.parseBoolean(mode);
+        boolean writeMode = GuildData.Companion.getWriteMode().get();
         if (!writeMode) {
             throw new UnsupportedOperationException("Cannot write guild data if not in write mode!");
         }
