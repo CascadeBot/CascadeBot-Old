a0de430f4e989e1ef044f67989f8b2c3a83558d5
William Oldham <wegg7250@gmail.com>
2021-07-15 21:10:12 +0100

    CascadeLinkButton: Builder funcs

--- src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/CascadeLinkButton.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/CascadeLinkButton.kt
@@ -5,18 +5,20 @@ import net.dv8tion.jda.api.interactions.components.Button
 import net.dv8tion.jda.api.interactions.components.ButtonStyle
 import net.dv8tion.jda.api.interactions.components.Component
 
-class CascadeLinkButton private constructor (val link: String, val emoji: Emoji?, val label: String?) : CascadeComponent(link) {
-    override val discordComponent: Component
-        get() {
-            return Button.of(ButtonStyle.LINK, link, label, emoji)
-        }
+class CascadeLinkButton private constructor (val link: String, val label: String?, val emoji: Emoji?) : CascadeComponent(link) {
+    override val discordComponent: Component = Button.of(ButtonStyle.LINK, link, label, emoji)
 
     init {
         require(label != null || emoji != null) { "Label and emoji cannot both be null" }
     }
 
-    constructor(link: String, label: String): this(link, null, label)
-    constructor(link: String, emoji: Emoji): this(link, emoji, null)
-    constructor(link: String, label: String, emoji: Emoji): this(link, emoji, label)
+    companion object {
+        @JvmStatic
+        fun of(link: String, label: String) = CascadeLinkButton(link, label, null)
+        @JvmStatic
+        fun of(link: String, emoji: Emoji) = CascadeLinkButton(link, null, emoji)
+        @JvmStatic
+        fun of(link: String, label: String, emoji: Emoji) = CascadeLinkButton(link, label, emoji)
+    }
 
 }
