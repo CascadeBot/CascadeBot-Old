d1027b7598b81a710b275756cf0b71a96375ce4d
William Oldham <wegg7250@gmail.com>
2021-09-26 11:59:05 +0100

    First entity

---
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildEntity.kt
@@ -0,0 +1,29 @@
+/*
+ * Copyright (c) 2021 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.data.entities
+
+import org.hibernate.cfg.Environment
+import java.time.LocalDateTime
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "guilds")
+class GuildEntity(guildId: Long) {
+
+    @Id
+    @Column(name = "guild_id", nullable = false)
+    var guildId: Long = guildId
+
+    @Column(name = "created_at")
+    val createdAt: LocalDateTime? = LocalDateTime.now()
+
+    @Column(name = "removed_at")
+    var removedAt: LocalDateTime? = null
+
+}
