88852bd69a7d36185cb60c053506fb9e21e9927e
William Oldham <wegg7250@gmail.com>
2021-10-02 21:33:28 +0100

    Guild tag entity

---
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildTagEntity.kt
@@ -0,0 +1,36 @@
+/*
+ * Copyright (c) 2021 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.data.entities
+
+import java.io.Serializable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.IdClass
+import javax.persistence.Table
+
+@Entity
+@Table(name = "guild_tag")
+@IdClass(GuildTagId::class)
+class GuildTagEntity(guildId: Long, name: String, content: String) {
+
+    @Id
+    @Column(name = "guild_id", nullable = false)
+    val guildId: Long = guildId
+
+    @Id
+    @Column(name = "name", nullable = false)
+    val name: String = name
+
+    @Column(name = "content", nullable = false)
+    var content: String = content
+
+    @Column(name = "category")
+    var category: String? = null
+
+}
+
+data class GuildTagId(val guildId: Long, val name: String) : Serializable
