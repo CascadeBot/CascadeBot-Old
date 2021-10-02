3bfb6295408e6c689da401ec36501e28fe2ff37e
William Oldham <wegg7250@gmail.com>
2021-10-02 21:33:19 +0100

    Guild autorole entity

---
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildAutoRoleEntity.kt
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2021 CascadeBot. All rights reserved.
+ * Licensed under the MIT license.
+ */
+
+package org.cascadebot.cascadebot.data.entities
+
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Table(name = "guild_autorole")
+class GuildAutoRoleEntity(guildId: Long, roleId: Long) {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    val id: UUID = UUID.randomUUID()
+
+    @Column(name = "guild_id", nullable = false)
+    val guildId: Long = guildId
+
+    @Column(name = "role_id", nullable = false)
+    val roleId: Long = roleId
+
+}
