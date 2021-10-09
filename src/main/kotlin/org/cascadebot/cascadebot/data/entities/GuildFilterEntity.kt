d25665180a4b9d677da34f1078e876296d8f2f27
William Oldham <wegg7250@gmail.com>
2021-10-09 18:26:26 +0100

    Command Filter Entity

---
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildFilterEntity.kt
@@ -0,0 +1,46 @@
+package org.cascadebot.cascadebot.data.entities
+
+import org.cascadebot.cascadebot.data.objects.CommandFilter
+import org.hibernate.annotations.Type
+import java.io.Serializable
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Id
+import javax.persistence.IdClass
+import javax.persistence.Table
+
+@Entity
+@Table(name = "guild_filter")
+@IdClass(GuildFilterId::class)
+class GuildFilterEntity(name: String, guildId: Long, type: CommandFilter.FilterType, operator: CommandFilter.FilterOperator) {
+
+    @Id
+    @Column(name = "name", nullable = false)
+    val name: String = name
+
+    @Id
+    @Column(name = "guild_id", nullable = false)
+    val guildId: Long = guildId
+
+    @Column(name = "enabled", nullable = false)
+    var enabled: Boolean = true
+
+    @Column(name = "filter_type", nullable = false)
+    @Type(type = "psql-enum")
+    @Enumerated(EnumType.STRING)
+    var type: CommandFilter.FilterType = type
+
+    @Column(name = "filter_operator", nullable = false)
+    @Type(type = "psql-enum")
+    @Enumerated(EnumType.STRING)
+    var operator: CommandFilter.FilterOperator = operator
+
+    @Column(name = "commands", columnDefinition = "varchar(255)[]", nullable = false)
+    @Type(type = "list-array")
+    val commands: MutableList<String> = mutableListOf()
+
+}
+
+data class GuildFilterId(val name: String, val guildId: Long) : Serializable
