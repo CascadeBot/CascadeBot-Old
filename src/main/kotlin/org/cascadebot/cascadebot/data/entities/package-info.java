11321a902cb499b7dd37fe0609423e94ffb14285
weeryan17 <theweeryan17@gmail.com>
2022-02-12 17:41:14 -0500

    Add json typedef before we forget

--- src/main/kotlin/org/cascadebot/cascadebot/data/entities/package-info.java
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/package-info.java
@@ -1,10 +1,12 @@
 @TypeDefs({
         @TypeDef(name = "psql-enum", typeClass = EnumDBType.class),
-        @TypeDef(name = "list-array", typeClass = ListArrayType.class)
+        @TypeDef(name = "list-array", typeClass = ListArrayType.class),
+        @TypeDef(name = "json", typeClass = JsonType.class)
 })
 package org.cascadebot.cascadebot.data.entities;
 
 import com.vladmihalcea.hibernate.type.array.ListArrayType;
+import com.vladmihalcea.hibernate.type.json.JsonType;
 import org.cascadebot.cascadebot.data.EnumDBType;
 import org.hibernate.annotations.TypeDef;
 import org.hibernate.annotations.TypeDefs;
