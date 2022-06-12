6923a3086a50d4ffdbbf220e979b5d977416cf07
William Oldham <wegg7250@gmail.com>
2022-06-12 20:38:58 +0100

    Rename voteButton function and abstractify todoButtonToggle

--- src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/PersistentComponent.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/PersistentComponent.kt
@@ -7,19 +7,8 @@ import net.dv8tion.jda.api.entities.TextChannel
 import org.cascadebot.cascadebot.UnicodeConstants
 
 enum class PersistentComponent(@field:Transient val component: CascadeComponent) {
-    TODO_BUTTON_CHECK(CascadeButton.primary("Check", Emoji.fromUnicode(UnicodeConstants.TICK), ::todoButtonCheck)),
-    TODO_BUTTON_UNCHECK(CascadeButton.primary("Uncheck", Emoji.fromUnicode(UnicodeConstants.WHITE_HALLOW_SQUARE),
-        IButtonRunnable { runner: Member, channel: TextChannel, message: InteractionMessage ->
-            /*val todoList =
-                GuildDataManager.getGuildData(channel.guild.idLong).useful.getTodoListByMessage(message.idLong)
-            if (!todoList.canUserEdit(runner.idLong)) {
-                return@IButtonRunnable
-            }
-            val item = todoList.items[todoList.currentItem]
-            item.done = false
-            todoList.addCheckButton(message.message)
-            message.editMessage(todoList.todoListMessage).queue()*/
-        })),
+    TODO_BUTTON_CHECK(CascadeButton.primary("Check", Emoji.fromUnicode(UnicodeConstants.TICK), todoButtonToggle(true))),
+    TODO_BUTTON_UNCHECK(CascadeButton.primary("Uncheck", Emoji.fromUnicode(UnicodeConstants.WHITE_HALLOW_SQUARE), todoButtonToggle(false))),
     TODO_BUTTON_NAVIGATE_LEFT(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.BACKWARD_ARROW),
         IButtonRunnable { runner: Member, channel: TextChannel, message: InteractionMessage ->
             /*val todoList =
@@ -102,39 +91,39 @@ enum class PersistentComponent(@field:Transient val component: CascadeComponent)
             }
             voteButtonGroup.addVote(runner.user, UnicodeConstants.RED_CROSS)*/
         })),
-    VOTE_BUTTON_ONE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.ONE), voteButton(1))),
-    VOTE_BUTTON_TWO(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.TWO), voteButton(2))),
-    VOTE_BUTTON_THREE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.THREE), voteButton(3))),
-    VOTE_BUTTON_FOUR(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.FOUR), voteButton(4))),
-    VOTE_BUTTON_FIVE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.FIVE), voteButton(5))),
-    VOTE_BUTTON_SIX(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.SIX), voteButton(6))),
-    VOTE_BUTTON_SEVEN(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.SEVEN), voteButton(7))),
-    VOTE_BUTTON_EIGHT(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.EIGHT), voteButton(8))),
-    VOTE_BUTTON_NINE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.NINE), voteButton(9))),
-    VOTE_BUTTON_A(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.A), voteButton(0))),
-    VOTE_BUTTON_B(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.B), voteButton(1))),
-    VOTE_BUTTON_C(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.C), voteButton(2))),
-    VOTE_BUTTON_D(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.D), voteButton(3))),
-    VOTE_BUTTON_E(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.E), voteButton(4))),
-    VOTE_BUTTON_F(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.F), voteButton(5))),
-    VOTE_BUTTON_G(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.G), voteButton(6))),
-    VOTE_BUTTON_H(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.H), voteButton(7))),
-    VOTE_BUTTON_I(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.I), voteButton(8))),
-    VOTE_BUTTON_J(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.J), voteButton(9))),
-    VOTE_BUTTON_K(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.K), voteButton(10))),
-    VOTE_BUTTON_L(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.L), voteButton(11))),
-    VOTE_BUTTON_M(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.M), voteButton(12))),
-    VOTE_BUTTON_N(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.N), voteButton(13))),
-    VOTE_BUTTON_O(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.O), voteButton(14))),
-    VOTE_BUTTON_P(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.P), voteButton(15))),
-    VOTE_BUTTON_Q(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.Q), voteButton(16))),
-    VOTE_BUTTON_R(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.R), voteButton(17))),
-    VOTE_BUTTON_S(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.S), voteButton(18))),
-    VOTE_BUTTON_T(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.T), voteButton(19))),
-    VOTE_BUTTON_U(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.U), voteButton(20))),
-    VOTE_BUTTON_V(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.V), voteButton(21))),
-    VOTE_BUTTON_W(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.W), voteButton(22))),
-    VOTE_BUTTON_X(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.X), voteButton(23))),
-    VOTE_BUTTON_Y(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.Y), voteButton(24)));
+    VOTE_BUTTON_ONE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.ONE), voteButtonAlphaNumeric(1))),
+    VOTE_BUTTON_TWO(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.TWO), voteButtonAlphaNumeric(2))),
+    VOTE_BUTTON_THREE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.THREE), voteButtonAlphaNumeric(3))),
+    VOTE_BUTTON_FOUR(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.FOUR), voteButtonAlphaNumeric(4))),
+    VOTE_BUTTON_FIVE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.FIVE), voteButtonAlphaNumeric(5))),
+    VOTE_BUTTON_SIX(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.SIX), voteButtonAlphaNumeric(6))),
+    VOTE_BUTTON_SEVEN(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.SEVEN), voteButtonAlphaNumeric(7))),
+    VOTE_BUTTON_EIGHT(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.EIGHT), voteButtonAlphaNumeric(8))),
+    VOTE_BUTTON_NINE(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.NINE), voteButtonAlphaNumeric(9))),
+    VOTE_BUTTON_A(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.A), voteButtonAlphaNumeric(0))),
+    VOTE_BUTTON_B(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.B), voteButtonAlphaNumeric(1))),
+    VOTE_BUTTON_C(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.C), voteButtonAlphaNumeric(2))),
+    VOTE_BUTTON_D(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.D), voteButtonAlphaNumeric(3))),
+    VOTE_BUTTON_E(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.E), voteButtonAlphaNumeric(4))),
+    VOTE_BUTTON_F(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.F), voteButtonAlphaNumeric(5))),
+    VOTE_BUTTON_G(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.G), voteButtonAlphaNumeric(6))),
+    VOTE_BUTTON_H(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.H), voteButtonAlphaNumeric(7))),
+    VOTE_BUTTON_I(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.I), voteButtonAlphaNumeric(8))),
+    VOTE_BUTTON_J(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.J), voteButtonAlphaNumeric(9))),
+    VOTE_BUTTON_K(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.K), voteButtonAlphaNumeric(10))),
+    VOTE_BUTTON_L(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.L), voteButtonAlphaNumeric(11))),
+    VOTE_BUTTON_M(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.M), voteButtonAlphaNumeric(12))),
+    VOTE_BUTTON_N(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.N), voteButtonAlphaNumeric(13))),
+    VOTE_BUTTON_O(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.O), voteButtonAlphaNumeric(14))),
+    VOTE_BUTTON_P(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.P), voteButtonAlphaNumeric(15))),
+    VOTE_BUTTON_Q(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.Q), voteButtonAlphaNumeric(16))),
+    VOTE_BUTTON_R(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.R), voteButtonAlphaNumeric(17))),
+    VOTE_BUTTON_S(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.S), voteButtonAlphaNumeric(18))),
+    VOTE_BUTTON_T(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.T), voteButtonAlphaNumeric(19))),
+    VOTE_BUTTON_U(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.U), voteButtonAlphaNumeric(20))),
+    VOTE_BUTTON_V(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.V), voteButtonAlphaNumeric(21))),
+    VOTE_BUTTON_W(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.W), voteButtonAlphaNumeric(22))),
+    VOTE_BUTTON_X(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.X), voteButtonAlphaNumeric(23))),
+    VOTE_BUTTON_Y(CascadeButton.secondary(Emoji.fromUnicode(UnicodeConstants.Y), voteButtonAlphaNumeric(24)));
 
 }
--- src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/PersistentComponentFunctions.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/utils/interactions/PersistentComponentFunctions.kt
@@ -10,38 +10,40 @@ import net.dv8tion.jda.api.entities.TextChannel
 import org.cascadebot.cascadebot.CascadeBot
 import org.cascadebot.cascadebot.data.entities.GuildTodolistEntity
 
-fun todoButtonCheck(runner: Member, owner: Member, channel: TextChannel, message: InteractionMessage) {
-    val todoListOption = CascadeBot.INS.postgresManager.transaction {
-        val query = this.createQuery("FROM GuildTodolistEntity T where T.message_id = :message_id", GuildTodolistEntity::class.java)
-        query.setParameter("message_id", message.idLong)
-        query.uniqueResultOptional()
-    }!!
-    if (todoListOption.isEmpty) {
-        // The list doesn't exist, so no action to do
-        return
+fun todoButtonToggle(checked: Boolean): ButtonRunnable {
+    return fun(runner: Member, owner: Member, channel: TextChannel, message: InteractionMessage) {
+        val todoListOption = CascadeBot.INS.postgresManager.transaction {
+            val query = this.createQuery("FROM GuildTodolistEntity T where T.message_id = :message_id", GuildTodolistEntity::class.java)
+            query.setParameter("message_id", message.idLong)
+            query.uniqueResultOptional()
+        }!!
+        if (todoListOption.isEmpty) {
+            // The list doesn't exist, so no action to do
+            return
+        }
+
+        val todoList = todoListOption.get()
+
+        if (!todoList.canUserEdit(runner.idLong)) {
+            return
+        }
+
+        val item = todoList.items[todoList.currentItem]
+        item.done = checked
+        todoList.setChecked(message.message, checked)
+
+        message.editMessage(todoList.todoListMessage).queue()
     }
 
-    val todoList = todoListOption.get()
-
-    if (!todoList.canUserEdit(runner.idLong)) {
-        return
-    }
-
-    val item = todoList.items[todoList.currentItem]
-    item.done = true
-    todoList.addUncheckButton(message.message)
-    message.editMessage(todoList.todoListMessage).queue()
-
-    message.editMessage(todoList.todoListMessage)
 }
 
-fun voteButton(index: Int): ButtonRunnable {
+fun voteButtonAlphaNumeric(alphaNumericIndex: Int): ButtonRunnable {
     return fun(runner: Member, owner: Member, channel: TextChannel, message: InteractionMessage) {
         val voteButtonGroup =
             GuildDataManager.getGuildData(channel.guild.idLong).findVoteGroupByMessageAndChannel(channel.idLong, message.idLong)
         if (!voteButtonGroup!!.isUserAllowed(runner.idLong)) {
             return
         }
-        voteButtonGroup.addVote(runner.user, index)
+        voteButtonGroup.addVote(runner.user, alphaNumericIndex)
     }
 }
