1b0b3f369dda53c0a23f6f3ec6fa6af7740952c3
William Oldham <wegg7250@gmail.com>
2022-06-25 10:52:14 +0100

    Guild vote group entity

--- src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildVoteGroup.kt
+++ src/main/kotlin/org/cascadebot/cascadebot/data/entities/GuildVoteGroup.kt
@@ -5,5 +5,71 @@
 
 package org.cascadebot.cascadebot.data.entities
 
-class GuildVoteGroup {
+import org.cascadebot.cascadebot.utils.votes.VoteFinishFunction
+import org.cascadebot.cascadebot.utils.votes.VotePeriodicFunction
+import org.hibernate.annotations.Type
+import java.time.Instant
+import java.util.UUID
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.EnumType
+import javax.persistence.Enumerated
+import javax.persistence.Id
+import javax.persistence.JoinColumn
+import javax.persistence.JoinColumns
+import javax.persistence.OneToMany
+import javax.persistence.Table
+
+@Entity
+@Table(name = "guild_vote_group")
+class GuildVoteGroup(type: String, guildId: Long, channelId: Long, messageId: Long, ownerId: Long, finishFunction: VoteFinishFunction) {
+
+    @Id
+    @Column(name = "id")
+    val id: UUID = UUID.randomUUID()
+
+    @Column(name = "type", nullable = false)
+    var type: String = type
+
+    @Column(name = "guild_id", nullable = false)
+    var guildId: Long = guildId
+
+    @Column(name = "channel_id", nullable = false)
+    var channelId: Long = channelId
+
+    @Column(name = "message_id", nullable = false)
+    var messageId: Long = messageId
+
+    @Column(name = "owner_id", nullable = false)
+    var ownerId: Long = ownerId
+
+    @Column(name = "start_time", nullable = true)
+    var startTime: Instant? = null
+
+    @Column(name = "run_duration", nullable = false)
+    var runDuration: Int = 0
+
+    @Column(name = "max_duration", nullable = false)
+    var maxDuration: Int = 0
+
+    @Column(name = "duration_increment", nullable = false)
+    var durationIncrement: Int = TODO("Default value?")
+
+    @Column(name = "dynamic_timing", nullable = false)
+    var dynamicTiming: Boolean = false
+
+    @Column(name =  "periodic_function", nullable = true)
+    @Enumerated(EnumType.STRING)
+    @Type(type = "psql-enum")
+    var periodicFunction: VotePeriodicFunction? = null
+
+    @Column(name =  "finish_function", nullable = false)
+    @Enumerated(EnumType.STRING)
+    @Type(type = "psql-enum")
+    var finishFunction: VoteFinishFunction = finishFunction
+
+    @OneToMany
+    @JoinColumn(name = "vote_group_id", referencedColumnName = "id")
+    val allowedUsers: MutableList<GuildVoteAllowedUser> = mutableListOf()
+
 }
